5723f4a997abed4c66450c2239bbbfb46e5085e5
philzoio <philzoio@realsolve.co.uk>
2007-10-05 20:49:30 +0000

    Added equals method

--- impala/impala-core/src/net/java/impala/spring/plugin/SimplePlugin.java
+++ impala/impala-core/src/net/java/impala/spring/plugin/SimplePlugin.java
@@ -16,4 +16,30 @@ public class SimplePlugin implements Plugin {
 		return name;
 	}
 
+	@Override
+	public int hashCode() {
+		final int PRIME = 31;
+		int result = 1;
+		result = PRIME * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final SimplePlugin other = (SimplePlugin) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		}
+		else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
 }
