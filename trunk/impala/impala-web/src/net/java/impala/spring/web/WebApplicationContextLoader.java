d3d7ee4ec8bc6a5045d9b1dfac7e82621c404493
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2007-10-07 19:09:16 +0000

    Extracted interfaces ApplicationContextLoader and WebApplicationContextLoader

---
+++ trunk/impala/impala-web/src/net/java/impala/spring/web/WebApplicationContextLoader.java
@@ -0,0 +1,17 @@
+package net.java.impala.spring.web;
+
+import java.util.List;
+
+import javax.servlet.ServletContext;
+
+import net.java.impala.spring.util.ApplicationContextLoader;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.context.WebApplicationContext;
+
+public interface WebApplicationContextLoader extends ApplicationContextLoader {
+
+	WebApplicationContext loadWebContext(WebApplicationContext parent, String parentName, String servletName,
+			ServletContext context, List<Resource> resourceLocations);
+
+}
