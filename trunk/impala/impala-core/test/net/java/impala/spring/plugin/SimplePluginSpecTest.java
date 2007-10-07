e128b11fb7efc7ab9cff13ff5edb727918780b8c
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2007-10-07 19:27:44 +0000

    Renamed PluginSpec to SpringContextSpec

--- trunk/impala/impala-core/test/net/java/impala/spring/plugin/SimplePluginSpecTest.java
+++ trunk/impala/impala-core/test/net/java/impala/spring/plugin/SimplePluginSpecTest.java
@@ -5,7 +5,7 @@ import junit.framework.TestCase;
 public class SimplePluginSpecTest extends TestCase {
 
 	public void testHasPlugin() {
-		SimplePluginSpec spec = new SimplePluginSpec(new String[] { "l1", "l2" }, new String[] { "p1", "p2" });
+		SimpleSpringContextSpec spec = new SimpleSpringContextSpec(new String[] { "l1", "l2" }, new String[] { "p1", "p2" });
 		assertTrue(spec.hasPlugin("p1"));
 		assertTrue(spec.hasPlugin("p2"));
 		assertFalse(spec.hasPlugin("p3"));
