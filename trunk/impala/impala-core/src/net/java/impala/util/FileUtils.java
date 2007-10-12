d82c0bbcb49a0bf77520b4eef2a89f615ad6bf6c
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2007-10-12 15:33:37 +0000

    Using FileCopyUtils

--- trunk/impala/impala-core/src/net/java/impala/util/FileUtils.java
+++ trunk/impala/impala-core/src/net/java/impala/util/FileUtils.java
@@ -14,9 +14,7 @@
 
 package net.java.impala.util;
 
-import java.io.DataInputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -25,31 +23,14 @@ import org.springframework.util.FileCopyUtils;
 
 public class FileUtils {
 
-	//FIXME can we use FileCopyUtils here
 	public static byte[] getBytes(File f) throws IOException {
-
 		if (f == null)
 			throw new IllegalArgumentException("File is null");
 
 		if (!f.exists())
 			throw new IllegalArgumentException("File " + f + " does not exist");
 
-		int size = (int) f.length();
-		byte buffer[] = new byte[size];
-		InputStream inputStream = null;
-		try {
-			inputStream = new FileInputStream(f);
-			DataInputStream dis = new DataInputStream(inputStream);
-			dis.readFully(buffer);
-		}
-		finally {
-			try {
-				inputStream.close();
-			}
-			catch (Exception e) {
-			}
-		}
-		return buffer;
+		return FileCopyUtils.copyToByteArray(f);
 	}
 
 	public static byte[] getBytes(Resource resource) throws IOException {
