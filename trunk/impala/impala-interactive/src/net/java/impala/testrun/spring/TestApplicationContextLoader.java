970476dcff07ed0d94c7114ba91a76d7de1cf740
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2007-10-13 13:07:46 +0000

    Moved newParentClassLoader to TestApplicationContextLoader

--- trunk/impala/impala-interactive/src/net/java/impala/testrun/spring/TestApplicationContextLoader.java
+++ trunk/impala/impala-interactive/src/net/java/impala/testrun/spring/TestApplicationContextLoader.java
@@ -1,7 +1,10 @@
 package net.java.impala.testrun.spring;
 
+import org.springframework.util.ClassUtils;
+
 import net.java.impala.classloader.TestContextResourceHelper;
 import net.java.impala.spring.util.DefaultApplicationContextLoader;
+import net.java.impala.util.PathUtils;
 
 public class TestApplicationContextLoader extends DefaultApplicationContextLoader {
 
@@ -12,6 +15,11 @@ public class TestApplicationContextLoader extends DefaultApplicationContextLoade
 	public ClassLoader getTestClassLoader(ClassLoader parentClassLoader, Class testClass) {
 		TestContextResourceHelper contextResourceHelper = (TestContextResourceHelper) getContextResourceHelper();
 		return contextResourceHelper.getTestClassLoader(parentClassLoader, testClass.getName());
+	}
+	
+	public ClassLoader newParentClassLoader() {
+		ClassLoader contextClassLoader = ClassUtils.getDefaultClassLoader();
+		return getContextResourceHelper().getParentClassLoader(contextClassLoader, PathUtils.getCurrentDirectoryName());
 	}
 
 }
