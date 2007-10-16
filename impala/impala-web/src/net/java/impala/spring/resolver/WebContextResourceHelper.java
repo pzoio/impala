0656cb4847fb3756491eab7d7593818b53228b0d
philzoio <philzoio@realsolve.co.uk>
2007-10-16 19:44:47 +0000

    Removed @Deprecated files

--- impala/impala-web/src/net/java/impala/spring/resolver/WebContextResourceHelper.java
+++
@@ -1,8 +0,0 @@
-package net.java.impala.spring.resolver;
-
-import net.java.impala.classloader.ContextResourceHelper;
-
-@Deprecated
-public interface WebContextResourceHelper extends ContextResourceHelper {
-	ClassLoader getWebClassLoader(String pluginName);
-}
