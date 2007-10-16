434f9d259ee1afcb72578fbbbafc43ca00121e8b
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2007-10-16 07:43:18 +0000

    Implemented PluginLoader-based loading

--- trunk/impala/impala-web/src/net/java/impala/spring/web/WebServletSpec.java
+++ trunk/impala/impala-web/src/net/java/impala/spring/web/WebServletSpec.java
@@ -2,11 +2,16 @@ package net.java.impala.spring.web;
 
 import net.java.impala.spring.plugin.SimpleParentSpec;
 
-public class WebParentSpec extends SimpleParentSpec {
+public class WebServletSpec extends SimpleParentSpec {
 
 	private String name;
 
-	public WebParentSpec(String name, String[] parentContextLocations) {
+	@Override
+	public String getType() {
+		return "servlet";
+	}
+
+	public WebServletSpec(String name, String[] parentContextLocations) {
 		super(parentContextLocations);
 		this.name = name;
 	}
