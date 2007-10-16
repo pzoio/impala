1bb6923a95db54231409d5e88024e75d7813c543
philzoio <philzoio@realsolve.co.uk>
2007-10-16 12:50:36 +0000

    Added PluginMonitor implementation

---
+++ impala/impala-web/src/net/java/impala/spring/plugin/WebPluginModificationListener.java
@@ -0,0 +1,15 @@
+package net.java.impala.spring.plugin;
+
+import net.java.impala.spring.monitor.PluginModificationEvent;
+import net.java.impala.spring.monitor.PluginModificationListener;
+
+public class WebPluginModificationListener implements PluginModificationListener {
+
+	
+	
+	public void pluginModified(PluginModificationEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
