d5f090b7f0a0dd887b7eef5b8892732785e43164
philzoio <philzoio@realsolve.co.uk>
2007-10-23 20:05:51 +0000

    Added BeanSetMapReader

---
+++ impala/impala-core/test/net/java/impala/spring/beanset/BeanSetMapReaderTest.java
@@ -0,0 +1,55 @@
+package net.java.impala.spring.beanset;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import org.springframework.beans.FatalBeanException;
+
+import junit.framework.TestCase;
+
+public class BeanSetMapReaderTest extends TestCase {
+
+	public final void testReadBeanSetSpec() {
+		BeanSetMapReader reader = new BeanSetMapReader();
+		final Map<String, Set<String>> map = reader.readBeanSetSpec("null: set1, set2; mock: set3, ");
+		assertEquals(2, map.size());
+		
+		final Set<String> setNull = map.get("null");
+		assertEquals(2, setNull.size());
+		final Iterator<String> iteratorNull = setNull.iterator();
+		
+		assertEquals("set1", iteratorNull.next());
+		assertEquals("set2", iteratorNull.next());
+		
+		final Set<String> setMock = map.get("mock");
+		assertEquals(1, setMock.size());
+		final Iterator<String> iteratorMock = setMock.iterator();
+		
+		assertEquals("set3", iteratorMock.next());
+	}
+	
+	public final void testNothingAtEnd() {
+		BeanSetMapReader reader = new BeanSetMapReader();
+		final Map<String, Set<String>> map = reader.readBeanSetSpec("null:, set1; mock:");
+		assertEquals(1, map.size());
+		
+		final Set<String> setNull = map.get("null");
+		assertEquals(1, setNull.size());
+		final Iterator<String> iteratorNull = setNull.iterator();
+		assertEquals("set1", iteratorNull.next());
+	}
+	
+	public void testMissingColonIndex() {
+		try {
+			new BeanSetMapReader().readBeanSetSpec("null: set1, set2; mock set3");
+			fail("Expected missing colon index failure");
+		}
+		catch (FatalBeanException e) {
+			assertEquals(
+					"Invalid beanset specification. Missing ':' from string ' mock set3' in 'null: set1, set2; mock set3'",
+					e.getMessage());
+		}
+	}
+
+}
