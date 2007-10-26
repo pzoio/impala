3f813de8d87447613c7529f93766478914550d0e
philzoio <philzoio@realsolve.co.uk>
2007-10-26 19:20:59 +0000

    Using ParentClassLoader in ApplicationPluginLoader

---
+++ impala/wineorder-sample/wineorder-dao/test/InProjectWineDAOTest.java
@@ -0,0 +1,51 @@
+
+
+import interfaces.WineDAO;
+
+import java.util.Collection;
+
+import test.BaseDataTest;
+
+import net.java.impala.spring.plugin.SpringContextSpec;
+import net.java.impala.spring.plugin.SimpleSpringContextSpec;
+import net.java.impala.testrun.DynamicContextHolder;
+import net.java.impala.testrun.PluginTestRunner;
+
+import classes.Wine;
+
+public class InProjectWineDAOTest extends BaseDataTest {
+
+	public static void main(String[] args) {
+		System.setProperty("impala.parent.project", "wineorder");
+		PluginTestRunner.run(InProjectWineDAOTest.class);
+	}
+
+	public void testDAO() {
+
+		WineDAO dao = DynamicContextHolder.getBean(this, "wineDAO", WineDAO.class);
+
+		Wine wine = new Wine();
+		wine.setColor("red");
+		wine.setVineyard("Chateau X");
+		wine.setTitle("Cabernet");
+		wine.setVintage(1996);
+		dao.save(wine);
+
+		Collection<Wine> winesOfVintage = dao.getWinesOfVintage(1996);
+		System.out.println("Wines of vintage 1996: " + winesOfVintage.size());
+		assertEquals(1, winesOfVintage.size());
+
+		wine.setVintage(2000);
+		wine.setColor("rose");
+		dao.update(wine);
+
+		Wine updated = dao.findById(wine.getId());
+		assertEquals(2000, updated.getVintage());
+
+	}
+
+	public SpringContextSpec getPluginSpec() {
+		return new SimpleSpringContextSpec("parent-context.xml", new String[] { "wineorder-dao", "wineorder-hibernate" });
+	}
+
+}
