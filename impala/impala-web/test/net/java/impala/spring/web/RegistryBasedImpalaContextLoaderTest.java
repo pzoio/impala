028228e03c65dc7bb8af0d70b3cadf91780b4171
philzoio <philzoio@realsolve.co.uk>
2007-10-27 22:18:14 +0000

    Deprecated previous mechanism for BeanSet loading

--- impala/impala-web/test/net/java/impala/spring/web/RegistryBasedImpalaContextLoaderTest.java
+++ impala/impala-web/test/net/java/impala/spring/web/RegistryBasedImpalaContextLoaderTest.java
@@ -2,16 +2,15 @@ package net.java.impala.spring.web;
 
 import javax.servlet.ServletContext;
 
+import junit.framework.TestCase;
 import net.java.impala.location.PropertyClassLocationResolver;
 import net.java.impala.spring.plugin.ApplicationPluginLoader;
-import net.java.impala.spring.plugin.BeansetApplicationPluginLoader;
 import net.java.impala.spring.plugin.PluginLoaderRegistry;
 import net.java.impala.spring.plugin.PluginTypes;
 import net.java.impala.spring.plugin.WebPluginTypes;
+import net.java.impala.spring.plugin.XmlBeansetApplicationPluginLoader;
 
 import org.easymock.classextension.EasyMock;
-
-import junit.framework.TestCase;
 
 public class RegistryBasedImpalaContextLoaderTest extends TestCase {
 
@@ -22,7 +21,7 @@ public class RegistryBasedImpalaContextLoaderTest extends TestCase {
 
 		assertTrue(registry.getPluginLoader(PluginTypes.ROOT) instanceof WebParentPluginLoader);
 		assertTrue(registry.getPluginLoader(PluginTypes.APPLICATION) instanceof ApplicationPluginLoader);
-		assertTrue(registry.getPluginLoader(PluginTypes.APPLICATION_WITH_BEANSETS) instanceof BeansetApplicationPluginLoader);
+		assertTrue(registry.getPluginLoader(PluginTypes.APPLICATION_WITH_BEANSETS) instanceof XmlBeansetApplicationPluginLoader);
 		assertTrue(registry.getPluginLoader(WebPluginTypes.SERVLET) instanceof WebPluginLoader);
 	}
 
