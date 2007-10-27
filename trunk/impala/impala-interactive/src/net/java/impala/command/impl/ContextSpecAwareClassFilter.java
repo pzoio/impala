349e193bffbc08d581ba093738cf3e51a6746ef4
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2007-10-27 20:33:02 +0000

    Added error logging

--- trunk/impala/impala-interactive/src/net/java/impala/command/impl/ContextSpecAwareClassFilter.java
+++ trunk/impala/impala-interactive/src/net/java/impala/command/impl/ContextSpecAwareClassFilter.java
@@ -9,9 +9,13 @@ import net.java.impala.exception.ExecutionException;
 import net.java.impala.file.DefaultClassFilter;
 import net.java.impala.testrun.SpringContextSpecAware;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.util.Assert;
 
 public class ContextSpecAwareClassFilter extends DefaultClassFilter implements FileFilter {
+
+	private static final Log log = LogFactory.getLog(ContextSpecAwareClassFilter.class);
 
 	@Override
 	public void setRootPath(File file) {
@@ -25,7 +29,7 @@ public class ContextSpecAwareClassFilter extends DefaultClassFilter implements F
 	}
 
 	private String rootCanonicalPath;
-	
+
 	public ContextSpecAwareClassFilter() {
 	}
 
@@ -37,45 +41,50 @@ public class ContextSpecAwareClassFilter extends DefaultClassFilter implements F
 		if (pathname.isDirectory()) {
 			return true;
 		}
-		
+
 		if (rootCanonicalPath == null) {
 			throw new IllegalStateException("root canonical path not set");
 		}
-		
+
+		String canonicalPath = null;
 		try {
-			
-			String canonicalPath = pathname.getCanonicalPath();
-			String relativePath = canonicalPath.substring(rootCanonicalPath.length());
-			
-			relativePath = relativePath.replace(File.separator, ".");
-			relativePath = relativePath.replace("/", ".");
-			relativePath = relativePath.substring(0, relativePath.length() - ".class".length());
-			if (relativePath.startsWith(".")) {
-				relativePath = relativePath.substring(1);
-			}
-			
-			final Class<?> forName = Class.forName(relativePath);
-			
+			canonicalPath = pathname.getCanonicalPath();
+		}
+		catch (IOException e) {
+			log.error("Could not read canonical path for " + pathname, e);
+			return false;
+		}
+		String relativePath = canonicalPath.substring(rootCanonicalPath.length());
+
+		relativePath = relativePath.replace(File.separator, ".");
+		relativePath = relativePath.replace("/", ".");
+		relativePath = relativePath.substring(0, relativePath.length() - ".class".length());
+		if (relativePath.startsWith(".")) {
+			relativePath = relativePath.substring(1);
+		}
+
+		Class<?> forName = null;
+		try {
+			forName = Class.forName(relativePath);
 			if (forName.isInterface()) {
 				return false;
 			}
 
 			int mods = forName.getModifiers();
-		    if (Modifier.isAbstract(mods)) {
-		    	return false;
-		    }
-			
+			if (Modifier.isAbstract(mods)) {
+				return false;
+			}
+
 			if (SpringContextSpecAware.class.isAssignableFrom(forName)) {
 				return true;
 			}
 		}
-		catch (Exception e) {
-			//FIXME debug an error here
+		catch (ClassNotFoundException e) {
+			log.error("Unable to resolve class associated with path " + pathname, e);
 		}
-		
-		
+
 		return false;
-		
+
 	}
 
 }
