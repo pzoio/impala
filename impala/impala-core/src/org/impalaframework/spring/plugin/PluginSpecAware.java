06f05fb08cd849bd1d948699f23ba05bd2acb071
philzoio <philzoio@realsolve.co.uk>
2007-11-06 23:15:02 +0000

    Added PluginMetadataPostProcessor implementation and test

---
+++ impala/impala-core/src/org/impalaframework/spring/plugin/PluginSpecAware.java
@@ -0,0 +1,7 @@
+package org.impalaframework.spring.plugin;
+
+import org.impalaframework.plugin.spec.PluginSpec;
+
+public interface PluginSpecAware {
+	public void setPluginSpec(PluginSpec pluginSpec);
+}
