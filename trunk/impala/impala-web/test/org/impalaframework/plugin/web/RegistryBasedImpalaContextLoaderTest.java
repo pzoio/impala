3079b03cc064845e323009edc7c7121cae807bd8
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2007-11-28 22:41:35 +0000

    Extracted method to return bootstrap context locations Removed deprecated code

--- trunk/impala/impala-web/test/org/impalaframework/plugin/web/RegistryBasedImpalaContextLoaderTest.java
+++ trunk/impala/impala-web/test/org/impalaframework/plugin/web/RegistryBasedImpalaContextLoaderTest.java
@@ -1,32 +1,10 @@
 package org.impalaframework.plugin.web;
 
-import javax.servlet.ServletContext;
-
 import junit.framework.TestCase;
-
-import org.easymock.classextension.EasyMock;
-import org.impalaframework.plugin.loader.ApplicationPluginLoader;
-import org.impalaframework.plugin.loader.BeansetApplicationPluginLoader;
-import org.impalaframework.plugin.loader.PluginLoaderRegistry;
-import org.impalaframework.plugin.spec.PluginTypes;
-import org.impalaframework.plugin.web.ImpalaContextLoader;
-import org.impalaframework.plugin.web.WebParentPluginLoader;
-import org.impalaframework.plugin.web.WebRootPluginLoader;
-import org.impalaframework.plugin.web.WebPluginTypes;
-import org.impalaframework.resolver.PropertyClassLocationResolver;
 
 public class RegistryBasedImpalaContextLoaderTest extends TestCase {
 
-	@Deprecated
 	public final void testNewRegistry() {
-		ImpalaContextLoader loader = new ImpalaContextLoader();
-		PluginLoaderRegistry registry = loader.newRegistry(EasyMock.createMock(ServletContext.class),
-				new PropertyClassLocationResolver());
-
-		assertTrue(registry.getPluginLoader(PluginTypes.ROOT) instanceof WebParentPluginLoader);
-		assertTrue(registry.getPluginLoader(PluginTypes.APPLICATION) instanceof ApplicationPluginLoader);
-		assertTrue(registry.getPluginLoader(PluginTypes.APPLICATION_WITH_BEANSETS) instanceof BeansetApplicationPluginLoader);
-		assertTrue(registry.getPluginLoader(WebPluginTypes.WEB_ROOT) instanceof WebRootPluginLoader);
 	}
 
 }
