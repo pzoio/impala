1b7b3504416d0e36acea2121a80dd3a8a2aa3bff
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2007-12-05 21:02:15 +0000

    ConfigurableImpalaContextLoader: added mechanism for overriding parent contexts

--- trunk/impala/impala-web/test/org/impalaframework/plugin/web/ConfigurableImpalaContextLoaderTest.java
+++ trunk/impala/impala-web/test/org/impalaframework/plugin/web/ConfigurableImpalaContextLoaderTest.java
@@ -162,4 +162,71 @@ public class ConfigurableImpalaContextLoaderTest extends TestCase {
 			System.clearProperty(WebConstants.BOOTSTRAP_LOCATIONS_RESOURCE_PARAM);
 		}
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public final void testParentLocationsSetGetProperties() {
+		System.setProperty(WebConstants.BOOTSTRAP_PLUGINS_RESOURCE_PARAM,
+				"org/impalaframework/plugin/web/locations.properties");
+		try {
+			replay(servletContext);
+			String[] parentLocations = contextLoader.getParentLocations(servletContext);
+			System.out.println(Arrays.toString(parentLocations));
+			assertTrue(Arrays.equals(new String[]{"plugin1", "plugin2"}, parentLocations));
+			verify(servletContext);
+
+		}
+		finally {
+			System.clearProperty(WebConstants.BOOTSTRAP_LOCATIONS_RESOURCE_PARAM);
+		}
+	}
+
+	public final void testParentLocationsGetPropertiesNotFound() {
+		System.setProperty(WebConstants.BOOTSTRAP_PLUGINS_RESOURCE_PARAM, "a location which does not exist");
+		try {
+			expect(servletContext.getInitParameter("contextConfigLocation")).andReturn("location1 location2");
+			replay(servletContext);
+			String[] parentLocations = contextLoader.getParentLocations(servletContext);
+
+			System.out.println(Arrays.toString(parentLocations));
+			assertTrue(Arrays.equals(new String[]{"location1", "location2"}, parentLocations));
+			verify(servletContext);
+
+		}
+		finally {
+			System.clearProperty(WebConstants.BOOTSTRAP_LOCATIONS_RESOURCE_PARAM);
+		}
+	}
+
+	public final void testParentLocationsPropertyNotFound() {
+		System.setProperty(WebConstants.BOOTSTRAP_PLUGINS_RESOURCE_PARAM,
+				"org/impalaframework/plugin/web/unspecified_locations.properties");
+		try {
+			replay(servletContext);
+
+			try {
+				contextLoader.getParentLocations(servletContext);
+				fail();
+			}
+			catch (IllegalStateException e) {
+				assertEquals("Bootstrap location resource 'class path resource [org/impalaframework/plugin/web/unspecified_locations.properties]' does not contain property 'bootstrapPluginNames'" +
+						"", e.getMessage());
+			}
+			verify(servletContext);
+		}
+		finally {
+			System.clearProperty(WebConstants.BOOTSTRAP_LOCATIONS_RESOURCE_PARAM);
+		}
+	}	
+	
+	
 }
