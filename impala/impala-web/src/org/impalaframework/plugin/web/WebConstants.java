1802bc1e9414a587344290d9d798835f2c8a42f8
philzoio <philzoio@realsolve.co.uk>
2007-12-05 09:42:42 +0000

    Configurable Impala context loader added

--- impala/impala-web/src/org/impalaframework/plugin/web/WebConstants.java
+++ impala/impala-web/src/org/impalaframework/plugin/web/WebConstants.java
@@ -6,11 +6,19 @@ public interface WebConstants {
 
 	public static final String PLUGIN_NAMES_PARAM = "pluginNames";
 	
-	public static final String IMPALA_FACTORY_PARAM = WebApplicationContext.class.getName() + ".CONTEXT_HOLDER";
+	public static final String IMPALA_FACTORY_PARAM = WebApplicationContext.class.getName() + ".FACTORY_HOLDER";
 	
 	public static final String WEBAPP_LOCATION_PARAM = "webappConfigLocation";
 	
 	public static final String ROOT_WEB_PLUGIN_PARAM = "rootWebPlugin";
+
+	public static final String BOOTSTRAP_LOCATIONS_RESOURCE_PARAM = "bootstrapLocationsResource";
+
+	public static final String BOOTSTRAP_LOCATIONS_PROPERTY_PARAM = "bootstrapLocations";
+
+	public static final String BOOTSTRAP_PLUGIN_NAMES_PARAM = "bootstrapPluginNames";
+	
+	public static final String PLUGIN_LOCATIONS_RESOURCE_PARAM = "pluginLocationsResource";
 	
 	/** Default config location for the root context */
 	public static final String DEFAULT_CONFIG_LOCATION = "/WEB-INF/applicationContext.xml";
