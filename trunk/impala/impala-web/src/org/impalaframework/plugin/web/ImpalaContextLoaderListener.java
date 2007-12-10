da0c22a185e9b13fa82dc399fb5b62132c5d7096
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2007-12-10 23:09:22 +0000

    Modified ImpalaContextLoaderListener

--- trunk/impala/impala-web/src/org/impalaframework/plugin/web/ImpalaContextLoaderListener.java
+++ trunk/impala/impala-web/src/org/impalaframework/plugin/web/ImpalaContextLoaderListener.java
@@ -12,13 +12,81 @@
  * the License.
  */
 
+//FIXME add spring copyright notice
+
 package org.impalaframework.plugin.web;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+
+import org.springframework.util.ClassUtils;
 import org.springframework.web.context.ContextLoader;
 import org.springframework.web.context.ContextLoaderListener;
 
 public class ImpalaContextLoaderListener extends ContextLoaderListener {
-	protected ContextLoader createContextLoader() {
-		return new WebXmlBasedContextLoader();
+	
+	//FIXME test
+	
+	private ContextLoader contextLoader;
+	
+	private Class defaultContextLoaderClass = WebXmlBasedContextLoader.class;
+
+	/**
+	 * Initialize the root web application context.
+	 */
+	public void contextInitialized(ServletContextEvent event) {
+		ServletContext servletContext = event.getServletContext();
+		this.contextLoader = createContextLoader(servletContext);
+		this.contextLoader.initWebApplicationContext(servletContext);
 	}
+
+	/**
+	 * Create the ContextLoader to use. Can be overridden in subclasses.
+	 * @param servletContext 
+	 * @return the new ContextLoader
+	 */
+	protected ContextLoader createContextLoader(ServletContext servletContext) {
+		String contextLoaderClassName = servletContext.getInitParameter(WebConstants.CONTEXT_LOADER_CLASS_NAME);
+		
+		Class<? extends ContextLoader> contextLoaderClass = defaultContextLoaderClass;
+		
+		if (contextLoaderClassName != null) {
+			try {
+				contextLoaderClass = ClassUtils.forName(contextLoaderClassName);
+			}
+			catch (Throwable e) {
+				// FIXME Auto-generated catch block
+				throw new RuntimeException(e);
+			}
+		}
+		
+		ContextLoader contextLoader = null;
+		try {
+			contextLoader = contextLoaderClass.newInstance();
+		}
+		catch (Exception e) {
+			// FIXME Auto-generated catch block
+			throw new RuntimeException(e);
+		}
+		
+		return contextLoader;
+	}
+
+	/**
+	 * Return the ContextLoader used by this listener.
+	 * @return the current ContextLoader
+	 */
+	public ContextLoader getContextLoader() {
+		return this.contextLoader;
+	}
+
+	/**
+	 * Close the root web application context.
+	 */
+	public void contextDestroyed(ServletContextEvent event) {
+		if (this.contextLoader != null) {
+			this.contextLoader.closeWebApplicationContext(event.getServletContext());
+		}
+	}
+
 }
