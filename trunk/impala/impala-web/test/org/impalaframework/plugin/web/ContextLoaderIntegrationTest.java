60f08c13d5a6566e7f46beedd321852c2febd830
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2007-12-13 22:43:44 +0000

    Added pluginSpecBuilder as servletContext attribute

--- trunk/impala/impala-web/test/org/impalaframework/plugin/web/ContextLoaderIntegrationTest.java
+++ trunk/impala/impala-web/test/org/impalaframework/plugin/web/ContextLoaderIntegrationTest.java
@@ -5,6 +5,7 @@ import static org.easymock.EasyMock.*;
 import javax.servlet.ServletContext;
 
 import org.impalaframework.plugin.bootstrap.BootstrapBeanFactory;
+import org.impalaframework.plugin.builder.SingleStringPluginSpecBuilder;
 import org.springframework.web.context.ContextLoader;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
@@ -28,6 +29,7 @@ public class ContextLoaderIntegrationTest extends TestCase {
 		expect(servletContext.getInitParameter(ContextLoader.CONFIG_LOCATION_PARAM)).andReturn("parentTestContext.xml");
 		expect(servletContext.getInitParameter(WebConstants.PLUGIN_NAMES_PARAM)).andReturn("impala-sample-dynamic-plugin1");
 		servletContext.setAttribute(eq(WebConstants.IMPALA_FACTORY_PARAM), isA(BootstrapBeanFactory.class));
+		servletContext.setAttribute(eq(WebConstants.PLUGIN_SPEC_BUILDER_PARAM), isA(SingleStringPluginSpecBuilder.class));
 		
 		replay(servletContext);
 
@@ -51,7 +53,7 @@ public class ContextLoaderIntegrationTest extends TestCase {
 		expect(servletContext.getInitParameter(WebConstants.BOOTSTRAP_LOCATIONS_RESOURCE_PARAM)).andReturn("org/impalaframework/plugin/web/bootstrap_locations.properties");
 		expect(servletContext.getInitParameter(WebConstants.BOOTSTRAP_PLUGINS_RESOURCE_PARAM)).andReturn("org/impalaframework/plugin/web/plugin_locations.properties");
 		expect(servletContext.getInitParameter(WebConstants.BOOTSTRAP_PLUGINS_RESOURCE_PARAM)).andReturn("org/impalaframework/plugin/web/plugin_locations.properties");
-		servletContext.setAttribute(eq(WebConstants.IMPALA_FACTORY_PARAM), isA(BootstrapBeanFactory.class));
+		servletContext.setAttribute(eq(WebConstants.IMPALA_FACTORY_PARAM), isA(BootstrapBeanFactory.class));		servletContext.setAttribute(eq(WebConstants.PLUGIN_SPEC_BUILDER_PARAM), isA(SingleStringPluginSpecBuilder.class));
 		
 		replay(servletContext);
 
@@ -65,7 +67,8 @@ public class ContextLoaderIntegrationTest extends TestCase {
 	
 	public void testExternalXmlBasedContextLoader() throws Exception {
 		expect(servletContext.getInitParameter(WebConstants.BOOTSTRAP_PLUGINS_RESOURCE_PARAM)).andReturn("xmlspec/xmlspec.xml");
-		servletContext.setAttribute(eq(WebConstants.IMPALA_FACTORY_PARAM), isA(BootstrapBeanFactory.class));
+		servletContext.setAttribute(eq(WebConstants.IMPALA_FACTORY_PARAM), isA(BootstrapBeanFactory.class));		
+		servletContext.setAttribute(eq(WebConstants.PLUGIN_SPEC_BUILDER_PARAM), isA(WebXmlPluginSpecBuilder.class));
 		
 		replay(servletContext);
 
@@ -85,5 +88,5 @@ public class ContextLoaderIntegrationTest extends TestCase {
 		assertNotNull(context);
 		assertTrue(context instanceof GenericWebApplicationContext);
 		verify(servletContext);
-	}		
+	}
 }
