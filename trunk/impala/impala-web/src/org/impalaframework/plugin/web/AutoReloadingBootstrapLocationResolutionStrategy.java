d2c2aa3f08170e59e805b07a133593daa09ab13a
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2007-12-16 22:59:55 +0000

    Created alternative bootstrap locations

--- trunk/impala/impala-web/src/org/impalaframework/plugin/web/AutoReloadingBootstrapLocationResolutionStrategy.java
+++ trunk/impala/impala-web/src/org/impalaframework/plugin/web/AutoReloadingBootstrapLocationResolutionStrategy.java
@@ -2,14 +2,16 @@ package org.impalaframework.plugin.web;
 
 import javax.servlet.ServletContext;
 
-public class DefaultBootstrapLocationResolutionStrategy {
+public class AutoReloadingBootstrapLocationResolutionStrategy {
 	protected String[] getBootstrapContextLocations(ServletContext servletContext) {
 		String[] locations = new String[] { 
 				"META-INF/impala-bootstrap.xml",
-				"META-INF/impala-web-bootstrap.xml"
+				"META-INF/impala-web-bootstrap.xml",
+				"META-INF/impala-jmx-bootstrap.xml",				
+				"META-INF/impala-web-listener-bootstrap.xml" 
 				};
 		
-		//FIXME test impala-web-jmx-bootstrap
+		//FIXME test
 		return locations;
 	}
 }
