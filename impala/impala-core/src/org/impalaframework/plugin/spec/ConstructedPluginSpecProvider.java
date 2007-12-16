ff058bf48419c9938584f3f014c9eb29c142d4fb
philzoio <philzoio@realsolve.co.uk>
2007-12-16 21:58:21 +0000

    Added ConstructedPluginSpecProvider

--- impala/impala-core/src/org/impalaframework/plugin/spec/ConstructedPluginSpecProvider.java
+++ impala/impala-core/src/org/impalaframework/plugin/spec/ConstructedPluginSpecProvider.java
@@ -1,12 +1,12 @@
 package org.impalaframework.plugin.spec;
 
+
 public class ConstructedPluginSpecProvider implements PluginSpecProvider {
 
 	private ParentSpec pluginSpec;
 	
 	public ConstructedPluginSpecProvider(ParentSpec pluginSpec) {
 		super();
-		//FIXME assert, test
 		this.pluginSpec = pluginSpec;
 	}
 
