325f41160a501d0553d9c9c779c3974b41ce73c1
philzoio <philzoio@realsolve.co.uk>
2007-12-17 10:36:36 +0000

    Comments

--- impala/impala-core/src/org/impalaframework/plugin/operation/PluginOperation.java
+++ impala/impala-core/src/org/impalaframework/plugin/operation/PluginOperation.java
@@ -1,5 +1,9 @@
 package org.impalaframework.plugin.operation;
 
 public interface PluginOperation {
+	
+	//FIXME take Map of arguments, and return boolean, Map of results, for partially
+	//constrained interface
+	
 	public boolean execute();
 }
