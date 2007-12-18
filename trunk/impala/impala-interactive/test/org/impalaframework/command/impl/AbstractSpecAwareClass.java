09bdfb482a3efc3a42a9565d8a870e07ba824cc8
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2007-12-18 09:32:52 +0000

    Renamed package

--- trunk/impala/impala-interactive/test/org/impalaframework/command/impl/AbstractSpecAwareClass.java
+++ trunk/impala/impala-interactive/test/org/impalaframework/command/impl/AbstractSpecAwareClass.java
@@ -1,7 +1,7 @@
 package org.impalaframework.command.impl;
 
-import org.impalaframework.module.spec.RootModuleDefinition;
-import org.impalaframework.module.spec.ModuleDefinitionSource;
+import org.impalaframework.module.definition.ModuleDefinitionSource;
+import org.impalaframework.module.definition.RootModuleDefinition;
 
 
 public abstract class AbstractSpecAwareClass implements ModuleDefinitionSource {
