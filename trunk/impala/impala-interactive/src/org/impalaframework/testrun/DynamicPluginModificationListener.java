cd0d2d263e79199e83fb7980a5f28442e585d2ee
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2007-12-18 17:24:19 +0000

    Class renames

--- trunk/impala/impala-interactive/src/org/impalaframework/testrun/DynamicPluginModificationListener.java
+++ trunk/impala/impala-interactive/src/org/impalaframework/testrun/DynamicPluginModificationListener.java
@@ -2,14 +2,14 @@ package org.impalaframework.testrun;
 
 import java.util.Set;
 
-import org.impalaframework.module.monitor.BasePluginModificationListener;
-import org.impalaframework.module.monitor.PluginModificationEvent;
-import org.impalaframework.module.monitor.PluginModificationListener;
+import org.impalaframework.module.monitor.BaseModuleChangeListener;
+import org.impalaframework.module.monitor.ModuleChangeEvent;
+import org.impalaframework.module.monitor.ModuleChangeListener;
 
 
-public class DynamicPluginModificationListener extends BasePluginModificationListener implements PluginModificationListener {
+public class DynamicPluginModificationListener extends BaseModuleChangeListener implements ModuleChangeListener {
 
-	public void pluginModified(PluginModificationEvent event) {
+	public void pluginModified(ModuleChangeEvent event) {
 		Set<String> modified = getModifiedPlugins(event);
 		
 		for (String pluginName : modified) {
