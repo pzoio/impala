f6fb0c14ad702876621b36ae0060973e222f2d1c
philzoio <philzoio@realsolve.co.uk>
2007-12-18 20:37:33 +0000

    Renamed package

--- impala/impala-web/test/org/impalaframework/module/web/WebPluginModificationListenerTest.java
+++ impala/impala-web/test/org/impalaframework/module/web/WebPluginModificationListenerTest.java
@@ -14,7 +14,7 @@ import junit.framework.TestCase;
 
 import org.impalaframework.module.bootstrap.ModuleManagementSource;
 import org.impalaframework.module.definition.SimpleRootModuleDefinition;
-import org.impalaframework.module.manager.ModuleStateHolder;
+import org.impalaframework.module.holder.ModuleStateHolder;
 import org.impalaframework.module.modification.ModificationExtractorType;
 import org.impalaframework.module.modification.ModuleModificationExtractor;
 import org.impalaframework.module.modification.ModuleModificationExtractorRegistry;
