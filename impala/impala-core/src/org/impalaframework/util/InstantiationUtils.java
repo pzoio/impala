c9e5633069b466ebeaf609b2fba9d9add37bc58a
philzoio <philzoio@realsolve.co.uk>
2007-12-22 22:22:00 +0000

    Added InstantiationUtils tests

--- impala/impala-core/src/org/impalaframework/util/InstantiationUtils.java
+++ impala/impala-core/src/org/impalaframework/util/InstantiationUtils.java
@@ -1,29 +1,27 @@
 package org.impalaframework.util;
 
-
 public class InstantiationUtils {
-	
+
 	@SuppressWarnings("unchecked")
 	public static <T extends Object> T instantiate(String className) {
 		Class<T> clazz = null;
-		
-			//FIXME better exception catching
-		
-			try {
-				clazz = org.springframework.util.ClassUtils.forName(className);
-			}
-			catch (Throwable e) {
-				throw new IllegalStateException("Unable to find class of type class " + className);
-			}
-		
+		try {
+			clazz = org.springframework.util.ClassUtils.forName(className);
+		}
+		catch (ClassNotFoundException e) {
+			throw new IllegalStateException("Unable to find class of type '" + className + "'");
+		}
+
 		T instance = null;
 		try {
-			instance = clazz.newInstance();
+			instance = (T) clazz.newInstance();
 			return instance;
 		}
 		catch (Exception e) {
-			throw new IllegalStateException("Error instantiating class of type " + className + ": " + e.getMessage(), e);
+			// FIXME better exception catching
+			String message = "Error instantiating class of type '" + className + "': " + e.getMessage();
+			throw new IllegalStateException(message, e);
 		}
 	}
-	
+
 }
--- impala/impala-core/test/org/impalaframework/util/InstantiationUtilsTest.java
+++ impala/impala-core/test/org/impalaframework/util/InstantiationUtilsTest.java
@@ -5,10 +5,46 @@ import junit.framework.TestCase;
 public class InstantiationUtilsTest extends TestCase {
 
 	public final void testInstantiate() {
-		Object instantiate = InstantiationUtils.instantiate(InstantiationUtilsTest.class.getName());
+		InstantiationUtilsTest instantiate = InstantiationUtils.instantiate(InstantiationUtilsTest.class.getName());
 		assertEquals(this.getClass(), instantiate.getClass());
-		
-		//FIXME more test
+	}
+	
+	public final void testWrongClass() {
+		try {
+			@SuppressWarnings("unused")
+			String instantiate = InstantiationUtils.instantiate(InstantiationUtilsTest.class.getName());
+		}
+		catch (ClassCastException e) {
+			assertEquals("org.impalaframework.util.InstantiationUtilsTest cannot be cast to java.lang.String", e.getMessage());
+		}
+	}
+	
+	public final void testClassNotFound() {
+		try {
+			@SuppressWarnings("unused")
+			String instantiate = InstantiationUtils.instantiate("unknown");
+		}
+		catch (IllegalStateException e) {
+			assertEquals("Unable to find class of type 'unknown'", e.getMessage());
+		}
+	}
+	
+	public final void testClassWithPrivateConstructor() {
+		try {
+			@SuppressWarnings("unused")
+			String instantiate = InstantiationUtils.instantiate(ClassWithPrivateConstructor.class.getName());
+		}
+		catch (IllegalStateException e) {
+			assertEquals("Error instantiating class of type 'org.impalaframework.util.ClassWithPrivateConstructor': Class org.impalaframework.util.InstantiationUtils can not access a member of class org.impalaframework.util.ClassWithPrivateConstructor with modifiers \"private\"", e.getMessage());
+		}
 	}
 
+}
+
+class ClassWithPrivateConstructor {
+
+	private ClassWithPrivateConstructor() {
+		super();
+	}
+	
 }
