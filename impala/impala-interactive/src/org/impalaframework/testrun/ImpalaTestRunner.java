449bd1b61d327b99deffa4f47c7a737560c4eab8
philzoio <philzoio@realsolve.co.uk>
2007-12-22 22:10:20 +0000

    Using FacadeConstants.facade.class.name system property

--- impala/impala-interactive/src/org/impalaframework/testrun/ImpalaTestRunner.java
+++ impala/impala-interactive/src/org/impalaframework/testrun/ImpalaTestRunner.java
@@ -38,6 +38,8 @@ import org.impalaframework.command.impl.ClassFindCommand;
 import org.impalaframework.command.impl.ModuleDefinitionAwareClassFilter;
 import org.impalaframework.command.impl.SearchClassCommand;
 import org.impalaframework.command.impl.SelectMethodCommand;
+import org.impalaframework.facade.FacadeConstants;
+import org.impalaframework.facade.ParentReloadingOperationsFacade;
 import org.impalaframework.module.definition.ModuleDefinitionSource;
 import org.impalaframework.module.definition.RootModuleDefinition;
 import org.impalaframework.resolver.ModuleLocationResolver;
@@ -94,7 +96,11 @@ public class ImpalaTestRunner {
 	 */
 	public void start(Class testClass) {
 
-		DynamicContextHolder.init(true);
+		// only set this if not set
+		if (System.getProperty(FacadeConstants.FACADE_CLASS_NAME) == null) {
+			System.setProperty(FacadeConstants.FACADE_CLASS_NAME, ParentReloadingOperationsFacade.class.getName());
+		}
+		DynamicContextHolder.init();
 
 		PluginDataHolder holder = new PluginDataHolder();
 		holder.testClass = testClass;
