a9baa663ab2854988f90ca471c90b4a6e32cfa8a
philzoio <philzoio@realsolve.co.uk>
2007-12-23 21:03:14 +0000

    Fixed broken test

--- impala/impala-web/test/org/impalaframework/web/module/WebModuleChangeListenerTest.java
+++ impala/impala-web/test/org/impalaframework/web/module/WebModuleChangeListenerTest.java
@@ -2,28 +2,25 @@ package org.impalaframework.web.module;
 
 import static org.easymock.EasyMock.createMock;
 import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.isA;
 import static org.easymock.EasyMock.replay;
 import static org.easymock.EasyMock.verify;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import javax.servlet.ServletContext;
 
 import junit.framework.TestCase;
 
 import org.impalaframework.module.bootstrap.ModuleManagementFactory;
-import org.impalaframework.module.definition.SimpleRootModuleDefinition;
-import org.impalaframework.module.holder.ModuleStateHolder;
-import org.impalaframework.module.modification.ModificationExtractorType;
-import org.impalaframework.module.modification.ModificationExtractor;
-import org.impalaframework.module.modification.ModificationExtractorRegistry;
-import org.impalaframework.module.modification.ModuleStateChange;
-import org.impalaframework.module.modification.TransitionSet;
 import org.impalaframework.module.monitor.ModuleChangeEvent;
 import org.impalaframework.module.monitor.ModuleChangeInfo;
+import org.impalaframework.module.operation.ModuleOperation;
+import org.impalaframework.module.operation.ModuleOperationConstants;
+import org.impalaframework.module.operation.ModuleOperationInput;
+import org.impalaframework.module.operation.ModuleOperationRegistry;
+import org.impalaframework.module.operation.ModuleOperationResult;
 import org.impalaframework.web.WebConstants;
-import org.impalaframework.web.module.WebModuleChangeListener;
 
 public class WebModuleChangeListenerTest extends TestCase {
 
@@ -48,51 +45,37 @@ public class WebModuleChangeListenerTest extends TestCase {
 
 		event = new ModuleChangeEvent(info);
 
-	}
-
-	public final void testNonEmpty() throws Exception {
+	}	
+	
+	public final void testEventsNonEmpty() throws Exception {
 
 		ArrayList<ModuleChangeInfo> info = new ArrayList<ModuleChangeInfo>();
 
 		info.add(new ModuleChangeInfo("p1"));
 
 		ModuleChangeEvent event = new ModuleChangeEvent(info);
-
 		ServletContext servletContext = createMock(ServletContext.class);
-		ModuleManagementFactory bootstrapFactory = createMock(ModuleManagementFactory.class);
-		ModuleStateHolder moduleStateHolder = createMock(ModuleStateHolder.class);
-
 		final WebModuleChangeListener listener = new WebModuleChangeListener(servletContext);
-		SimpleRootModuleDefinition simpleRootModuleDefinition = new SimpleRootModuleDefinition("p1");
-		
-		ModificationExtractor modificationCalculator = createMock(ModificationExtractor.class);
-		ModificationExtractorRegistry registry = new ModificationExtractorRegistry();
-		registry.addModificationCalculationType(ModificationExtractorType.STRICT, modificationCalculator);
+		ModuleManagementFactory bootstrapFactory = createMock(ModuleManagementFactory.class);
+		ModuleOperationRegistry moduleOperationRegistry = createMock(ModuleOperationRegistry.class);
+		ModuleOperation moduleOperation = createMock(ModuleOperation.class);
 
-		List<ModuleStateChange> transitions = new ArrayList<ModuleStateChange>();
-		TransitionSet transitionSet = new TransitionSet(transitions, simpleRootModuleDefinition);
-		
-		// set expectations
 		expect(servletContext.getAttribute(WebConstants.IMPALA_FACTORY_ATTRIBUTE)).andReturn(bootstrapFactory);
-		expect(bootstrapFactory.getModuleStateHolder()).andReturn(moduleStateHolder);
-		expect(moduleStateHolder.getRootModuleDefinition()).andReturn(simpleRootModuleDefinition);
-		expect(bootstrapFactory.getModuleStateHolder()).andReturn(moduleStateHolder);
-		expect(moduleStateHolder.cloneRootModuleDefinition()).andReturn(simpleRootModuleDefinition);
-		expect(bootstrapFactory.getModificationExtractorRegistry()).andReturn(registry);
-		expect(modificationCalculator.reload(simpleRootModuleDefinition, simpleRootModuleDefinition, "p1")).andReturn(transitionSet);
-		moduleStateHolder.processTransitions(transitionSet);
-
+		expect(bootstrapFactory.getModuleOperationRegistry()).andReturn(moduleOperationRegistry);
+		expect(moduleOperationRegistry.getOperation(ModuleOperationConstants.ReloadNamedModuleOperation)).andReturn(moduleOperation);
+		expect(moduleOperation.execute(isA(ModuleOperationInput.class))).andReturn(ModuleOperationResult.TRUE);
+		
 		replay(servletContext);
 		replay(bootstrapFactory);
-		replay(moduleStateHolder);
-		replay(modificationCalculator);
-
+		replay(moduleOperationRegistry);
+		replay(moduleOperation);
+		
 		listener.moduleContentsModified(event);
 
 		verify(servletContext);
 		verify(bootstrapFactory);
-		verify(moduleStateHolder);
-		verify(modificationCalculator);
+		verify(moduleOperationRegistry);
+		verify(moduleOperation);
 
 	}
 }
