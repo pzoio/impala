b6c1d58aad55fc90e1e137b41208766a8f09ad3b
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2007-12-27 22:35:21 +0000

    Changed ModuleLocationResolver to use Resource interface

--- trunk/impala/impala-web/src/org/impalaframework/web/module/ServletModuleLoader.java
+++ trunk/impala/impala-web/src/org/impalaframework/web/module/ServletModuleLoader.java
@@ -1,10 +1,7 @@
 package org.impalaframework.web.module;
 
-import java.io.File;
-
 import org.impalaframework.module.definition.ModuleDefinition;
 import org.impalaframework.resolver.ModuleLocationResolver;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 
 public class ServletModuleLoader extends WebRootModuleLoader {
@@ -15,8 +12,8 @@ public class ServletModuleLoader extends WebRootModuleLoader {
 
 	@Override
 	public Resource[] getSpringConfigResources(ModuleDefinition moduleDefinition, ClassLoader classLoader) {
-		File springLocation = this.getClassLocationResolver().getApplicationModuleSpringLocation(moduleDefinition.getName());
-		return new Resource[] { new FileSystemResource(springLocation) };
+		return new Resource[] { this.getClassLocationResolver().getApplicationModuleSpringLocation(
+				moduleDefinition.getName()) };
 	}
 
 }
