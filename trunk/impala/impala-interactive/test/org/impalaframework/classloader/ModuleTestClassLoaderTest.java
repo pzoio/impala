621e7b4717540ec720a91e2953e1cf8aa5fde055
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2008-01-05 21:59:27 +0000

    Class moves. Adding generic typing

--- trunk/impala/impala-interactive/test/org/impalaframework/classloader/ModuleTestClassLoaderTest.java
+++ trunk/impala/impala-interactive/test/org/impalaframework/classloader/ModuleTestClassLoaderTest.java
@@ -3,8 +3,6 @@ package org.impalaframework.classloader;
 import java.io.File;
 import java.util.Map;
 
-import org.impalaframework.classloader.ModuleTestClassLoader;
-
 import junit.framework.TestCase;
 
 public class ModuleTestClassLoaderTest extends TestCase {
@@ -13,10 +11,10 @@ public class ModuleTestClassLoaderTest extends TestCase {
 		ModuleTestClassLoader tcl = new ModuleTestClassLoader(new File[] {new File("../impala-interactive/bin")}, "org.impalaframework.testrun.AJUnitTest");
 
 		// check that this class loader loads the named class
-		Class cls = Class.forName("org.impalaframework.testrun.AJUnitTest", false, tcl);
+		Class<?> cls = Class.forName("org.impalaframework.testrun.AJUnitTest", false, tcl);
 		assertSame(cls.getClassLoader(), tcl);
 
-		Map<String, Class> loadedClasses = tcl.getLoadedClasses();
+		Map<String, Class<?>> loadedClasses = tcl.getLoadedClasses();
 		assertEquals(1, loadedClasses.size());
 		assertNotNull(loadedClasses.get("org.impalaframework.testrun.AJUnitTest"));
 
