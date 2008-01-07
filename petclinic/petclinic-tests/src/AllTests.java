ebbef5a2022b43b919d3483cec926c6f188c7452
philzoio <philzoio@realsolve.co.uk>
2008-01-07 21:25:20 +0000

    Initial commit

---
+++ petclinic/petclinic-tests/src/AllTests.java
@@ -0,0 +1,12 @@
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+
+public class AllTests extends TestCase {
+	public static Test suite() {
+		TestSuite suite = new TestSuite();
+		//suite.addTest(AllAdapterTests.suite());
+		return suite;
+	}
+}
