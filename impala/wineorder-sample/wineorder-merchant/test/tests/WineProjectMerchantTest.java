742881d06f5353e4bcd116cc539887d3c1d88682
philzoio <philzoio@realsolve.co.uk>
2008-01-17 22:35:01 +0000

    Applying InteractiveTestRunner

--- impala/wineorder-sample/wineorder-merchant/test/tests/WineProjectMerchantTest.java
+++ impala/wineorder-sample/wineorder-merchant/test/tests/WineProjectMerchantTest.java
@@ -22,7 +22,7 @@ import org.impalaframework.module.builder.SimpleModuleDefinitionSource;
 import org.impalaframework.module.definition.RootModuleDefinition;
 import org.impalaframework.module.definition.SimpleBeansetModuleDefinition;
 import org.impalaframework.testrun.DynamicContextHolder;
-import org.impalaframework.testrun.ImpalaTestRunner;
+import org.impalaframework.testrun.InteractiveTestRunner;
 
 import classes.Wine;
 
@@ -30,7 +30,7 @@ public class WineProjectMerchantTest extends BaseProjectWineMerchantTest {
 
 	public static void main(String[] args) {
 		System.setProperty("impala.parent.project", "wineorder");
-		ImpalaTestRunner.run(WineProjectMerchantTest.class);
+		InteractiveTestRunner.run(WineProjectMerchantTest.class);
 	}
 
 	public void testVintage() {
