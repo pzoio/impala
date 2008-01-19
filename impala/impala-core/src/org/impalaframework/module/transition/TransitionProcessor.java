5c44f3325dfe0a33a234f02be0e08b6111958006
philzoio <philzoio@realsolve.co.uk>
2008-01-19 21:47:37 +0000

    No longer passing in existing root module definition in TransitionProcessor interface

--- impala/impala-core/src/org/impalaframework/module/transition/TransitionProcessor.java
+++ impala/impala-core/src/org/impalaframework/module/transition/TransitionProcessor.java
@@ -5,6 +5,5 @@ import org.impalaframework.module.definition.RootModuleDefinition;
 import org.impalaframework.module.holder.ModuleStateHolder;
 
 public interface TransitionProcessor {
-	//FIXME reconsider this interface
 	public boolean process(ModuleStateHolder moduleStateHolder, RootModuleDefinition newRootDefinition, ModuleDefinition currentModuleDefinition);
 }
