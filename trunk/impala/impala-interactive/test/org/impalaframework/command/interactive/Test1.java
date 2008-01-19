f8c1e88172124c53ea30036ef25023bac2815a37
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2008-01-19 20:33:06 +0000

    Added RunTestCommandTest

--- trunk/impala/impala-interactive/test/org/impalaframework/command/interactive/Test1.java
+++ trunk/impala/impala-interactive/test/org/impalaframework/command/interactive/Test1.java
@@ -1,15 +1,22 @@
 package org.impalaframework.command.interactive;
 
+import junit.framework.TestCase;
+
 import org.impalaframework.module.builder.SimpleModuleDefinitionSource;
 import org.impalaframework.module.definition.ModuleDefinitionSource;
 import org.impalaframework.module.definition.RootModuleDefinition;
+import org.impalaframework.testrun.DynamicContextHolder;
 
-public class Test1 implements ModuleDefinitionSource {
+public class Test1 extends TestCase implements ModuleDefinitionSource {
 	
 	public static final String plugin1 = "impala-sample-dynamic-plugin1";
 
 	ModuleDefinitionSource source = new SimpleModuleDefinitionSource("parentTestContext.xml", new String[] { plugin1 });
 
+	public void testMyMethod() throws Exception {
+		System.out.println("Running test method with " + DynamicContextHolder.get());
+	}
+	
 	public RootModuleDefinition getModuleDefinition() {
 		return source.getModuleDefinition();
 	}
