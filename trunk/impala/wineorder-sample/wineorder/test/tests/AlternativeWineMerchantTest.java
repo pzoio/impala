47426b863b4f020bffe1ecaec294c9d6591d8dba
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2008-01-20 14:19:58 +0000

    Variable and method renames

--- trunk/impala/wineorder-sample/wineorder/test/tests/AlternativeWineMerchantTest.java
+++ trunk/impala/wineorder-sample/wineorder/test/tests/AlternativeWineMerchantTest.java
@@ -26,14 +26,14 @@ public class AlternativeWineMerchantTest extends WineMerchantTest {
 	}
 
 	public RootModuleDefinition getModuleDefinition() {
-		SimpleModuleDefinitionSource spec = new SimpleModuleDefinitionSource(new String[] { "parent-context.xml", "merchant-context.xml" }, 
+		SimpleModuleDefinitionSource definition = new SimpleModuleDefinitionSource(new String[] { "parent-context.xml", "merchant-context.xml" }, 
 						new String[] {
 						"wineorder-hibernate", "wineorder-dao" });
 		
-		RootModuleDefinition parent = spec.getModuleDefinition();
+		RootModuleDefinition parent = definition.getModuleDefinition();
 		new SimpleBeansetModuleDefinition(parent, "wineorder-merchant", "alternative: myImports");
 		
-		return spec.getModuleDefinition();
+		return definition.getModuleDefinition();
 	}
 
 }
