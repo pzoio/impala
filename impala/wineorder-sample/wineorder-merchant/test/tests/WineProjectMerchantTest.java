211bf8a047f3aa757625afc4ef3c00e25c44b349
philzoio <philzoio@realsolve.co.uk>
2008-01-20 21:45:17 +0000

    Renamed root projects property

--- impala/wineorder-sample/wineorder-merchant/test/tests/WineProjectMerchantTest.java
+++ impala/wineorder-sample/wineorder-merchant/test/tests/WineProjectMerchantTest.java
@@ -29,7 +29,7 @@ import classes.Wine;
 public class WineProjectMerchantTest extends BaseProjectWineMerchantTest {
 
 	public static void main(String[] args) {
-		System.setProperty("impala.parent.project", "wineorder");
+		System.setProperty("impala.root.projects", "wineorder");
 		InteractiveTestRunner.run(WineProjectMerchantTest.class);
 	}
 
