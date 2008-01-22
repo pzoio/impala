188e9d8b24c29f247890b713b449115af43bc9e9
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2008-01-22 21:10:11 +0000

    Implemented JarModuleLocationResolver with test

--- trunk/impala/impala-core/src/org/impalaframework/resolver/JarModuleLocationResolver.java
+++ trunk/impala/impala-core/src/org/impalaframework/resolver/JarModuleLocationResolver.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.util.Collections;
 import java.util.List;
 
+import org.impalaframework.exception.ConfigurationException;
 import org.impalaframework.util.PathUtils;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
@@ -12,29 +13,29 @@ public class JarModuleLocationResolver extends BaseModuleLocationResolver {
 
 	public List<Resource> getApplicationModuleClassLocations(String moduleName) {
 		// FIXME test
-
 		File workspaceRoot = getRootDirectory();
 
 		// return a classpath resource representing from a jar
 		String path = PathUtils.getPath(workspaceRoot.getAbsolutePath(), moduleName + ".jar");
 		Resource resource = new FileSystemResource(path);
-		List<Resource> singletonList = Collections.singletonList(resource);
-		return singletonList;
+		return Collections.singletonList(resource);
 	}
 
 	@Override
 	public File getRootDirectory() {
 		File rootDirectory = super.getRootDirectory();
 
-		// FIXME check that this exists, unlike StandaloneModuleLocationResolver
+		if (rootDirectory == null) {
+			throw new ConfigurationException("Workspace root not set. Have you set the property '"
+					+ LocationConstants.WORKSPACE_ROOT_PROPERTY + "'?");
+		}
 
 		return rootDirectory;
 	}
 
 	public List<Resource> getModuleTestClassLocations(String moduleName) {
-		// return a classpath resource representing from a jar.
 		// not sure what to do with this as this is a test
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 }
