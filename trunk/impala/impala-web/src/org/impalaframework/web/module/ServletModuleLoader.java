8b7c5457e583848f992e374f803621905bd8b7b1
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2008-01-24 21:03:55 +0000

    Wiring in spring resource lookup for web module loaders

--- trunk/impala/impala-web/src/org/impalaframework/web/module/ServletModuleLoader.java
+++ trunk/impala/impala-web/src/org/impalaframework/web/module/ServletModuleLoader.java
@@ -1,19 +1,23 @@
 package org.impalaframework.web.module;
 
-import org.impalaframework.module.definition.ModuleDefinition;
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.impalaframework.resolver.ModuleLocationResolver;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
+import org.impalaframework.spring.resource.ClassPathResourceLoader;
+import org.impalaframework.spring.resource.ResourceLoader;
 
 public class ServletModuleLoader extends WebRootModuleLoader {
 
 	public ServletModuleLoader(ModuleLocationResolver moduleLocationResolver) {
 		super(moduleLocationResolver);
 	}
-
+	
 	@Override
-	public Resource[] getSpringConfigResources(ModuleDefinition moduleDefinition, ClassLoader classLoader) {
-		return new Resource[]{new ClassPathResource(moduleDefinition.getName() + "-context.xml")};
+	protected Collection<ResourceLoader> getSpringLocationResourceLoaders() {
+		Collection<ResourceLoader> resourceLoaders = new ArrayList<ResourceLoader>();
+		resourceLoaders.add(new ClassPathResourceLoader());
+		return resourceLoaders;
 	}
 
 }
