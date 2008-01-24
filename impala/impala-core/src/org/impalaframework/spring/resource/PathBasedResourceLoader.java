92e77b9905f78ea7dc08e4a2565af2434726cc46
philzoio <philzoio@realsolve.co.uk>
2008-01-24 19:51:58 +0000

    Made class abstract

--- impala/impala-core/src/org/impalaframework/spring/resource/PathBasedResourceLoader.java
+++ impala/impala-core/src/org/impalaframework/spring/resource/PathBasedResourceLoader.java
@@ -1,9 +1,8 @@
 package org.impalaframework.spring.resource;
 
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
-public class PathBasedResourceLoader implements ResourceLoader {
+public abstract class PathBasedResourceLoader implements ResourceLoader {
 	
 	private String prefix;
 
@@ -15,10 +14,9 @@ public class PathBasedResourceLoader implements ResourceLoader {
 		String prefix = (this.prefix != null ? this.prefix : "");
 		return getResourceForPath(prefix, location, classLoader);
 	}
+	
+	protected abstract Resource getResourceForPath(String prefix, String location, ClassLoader classLoader);
 
-	protected Resource getResourceForPath(String prefix, String location, ClassLoader classLoader) {
-		return new ClassPathResource(prefix + location, classLoader);
-	}
 
 	public void setPrefix(String prefix) {
 		this.prefix = prefix;
