6aa060e7cbbf57fb8cc70cc7e48928e4be359a9f
philzoio <philzoio@realsolve.co.uk>
2008-01-24 14:14:32 +0000

    Added SpringLocationsResourceLoader interface

---
+++ impala/impala-core/src/org/impalaframework/module/resource/ApplicationModuleSpringLocationsResourceLoader.java
@@ -0,0 +1,17 @@
+package org.impalaframework.module.resource;
+
+import org.impalaframework.module.definition.ModuleDefinition;
+import org.springframework.core.io.Resource;
+
+public class ApplicationModuleSpringLocationsResourceLoader implements SpringLocationsResourceLoader {
+	
+	public ApplicationModuleSpringLocationsResourceLoader() {
+		super();
+	}
+
+	public Resource[] getSpringLocations(ModuleDefinition moduleDefinition) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
