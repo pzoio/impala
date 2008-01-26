49c7c7bd288a78b15a0524a2cf6a5f31401b25a0
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2008-01-26 20:26:48 +0000

    Renamed interface ModuleContentChangeListener

--- trunk/impala/impala-interactive/src/org/impalaframework/testrun/DynamicModuleChangeListener.java
+++ trunk/impala/impala-interactive/src/org/impalaframework/testrun/DynamicModuleChangeListener.java
@@ -4,10 +4,10 @@ import java.util.Set;
 
 import org.impalaframework.module.monitor.BaseModuleChangeListener;
 import org.impalaframework.module.monitor.ModuleChangeEvent;
-import org.impalaframework.module.monitor.ModuleChangeListener;
+import org.impalaframework.module.monitor.ModuleContentChangeListener;
 
 
-public class DynamicModuleChangeListener extends BaseModuleChangeListener implements ModuleChangeListener {
+public class DynamicModuleChangeListener extends BaseModuleChangeListener implements ModuleContentChangeListener {
 
 	public void moduleContentsModified(ModuleChangeEvent event) {
 		Set<String> modified = getModifiedPlugins(event);
