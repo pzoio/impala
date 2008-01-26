f535bc3cc2207d219bb7f3d6b881a3d590989f1e
philzoio <philzoio@realsolve.co.uk>
2008-01-26 22:12:25 +0000

    Added call to initServletBean in ExternalLoadingImpalaServlet's listener

--- impala/impala-web/src/org/impalaframework/web/servlet/ExternalLoadingImpalaServlet.java
+++ impala/impala-web/src/org/impalaframework/web/servlet/ExternalLoadingImpalaServlet.java
@@ -2,7 +2,11 @@ package org.impalaframework.web.servlet;
 
 import org.impalaframework.exception.ConfigurationException;
 import org.impalaframework.module.bootstrap.ModuleManagementFactory;
+import org.impalaframework.module.holder.ModuleStateChangeListener;
+import org.impalaframework.module.holder.ModuleStateChangeNotifier;
 import org.impalaframework.module.holder.ModuleStateHolder;
+import org.impalaframework.module.modification.ModuleStateChange;
+import org.impalaframework.module.modification.Transition;
 import org.impalaframework.web.WebConstants;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -11,6 +15,8 @@ import org.springframework.web.context.WebApplicationContext;
 public class ExternalLoadingImpalaServlet extends BaseImpalaServlet {
 
 	private static final long serialVersionUID = 1L;
+	
+	private boolean initialized;
 
 	@Override
 	protected WebApplicationContext createWebApplicationContext() throws BeansException {
@@ -27,6 +33,32 @@ public class ExternalLoadingImpalaServlet extends BaseImpalaServlet {
 
 		String servletName = getServletName();
 		ModuleStateHolder moduleStateHolder = factory.getModuleStateHolder();
+		
+		if (!initialized) {
+			
+			ModuleStateChangeNotifier moduleStateChangeNotifier = factory.getModuleStateChangeNotifier();
+			moduleStateChangeNotifier.addListener(new ModuleStateChangeListener() {
+
+				public void moduleStateChanged(ModuleStateHolder moduleStateHolder, ModuleStateChange change) {
+					try {
+						initServletBean();
+					}
+					catch (Exception e) {
+						logger.error("Unable to reinitialize servlet " + getServletName(), e);
+					}
+				}
+
+				public String getModuleName() {
+					return getServletName();
+				}
+
+				public Transition getTransition() {
+					return Transition.UNLOADED_TO_LOADED;
+				}
+				
+			});
+			this.initialized = true;
+		}
 
 		ConfigurableApplicationContext context = moduleStateHolder.getModule(servletName);
 		if (context != null) {
