7c0805c97329d96ef89bd2f91f2b3d41dbd7fa6a
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2008-01-28 18:20:35 +0000

    Added optional warning when dummy is called

---
+++ trunk/impala/impala-core/test/org/impalaframework/spring/module/ContributionEndpointInterceptorTest.java
@@ -0,0 +1,15 @@
+package org.impalaframework.spring.module;
+
+import junit.framework.TestCase;
+
+public class ContributionEndpointInterceptorTest extends TestCase {
+
+	public final void testInvoke() {
+		//FIXME implement test
+	}
+
+	public final void testInvokeDummy() {
+		//FIXME implement test
+	}
+
+}
