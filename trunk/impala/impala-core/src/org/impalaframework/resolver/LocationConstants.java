5077b4589ad606b547e007131174f7dfc2b9aecd
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2008-01-29 21:13:16 +0000

    Added ManualJarDeployer test

--- trunk/impala/impala-core/src/org/impalaframework/resolver/LocationConstants.java
+++ trunk/impala/impala-core/src/org/impalaframework/resolver/LocationConstants.java
@@ -6,5 +6,6 @@ public interface LocationConstants {
 	public static final String MODULE_TEST_DIR_PROPERTY = "impala.module.test.dir";
 	public static final String ROOT_PROJECTS_PROPERTY = "impala.root.projects";
 	public static final String WORKSPACE_ROOT_PROPERTY = "workspace.root";
+	public static final String APPLICATION_VERSION = "application.version";
 
 }
