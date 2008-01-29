8ac70ee6b81296a27f3b79f73faecaeb17237c8f
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2008-01-29 20:48:58 +0000

    Added Jar operations facade

---
+++ trunk/impala/impala-core/src/org/impalaframework/facade/JarOperationsFacade.java
@@ -0,0 +1,16 @@
+package org.impalaframework.facade;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JarOperationsFacade extends DefaultOperationsFacade {
+
+	@Override
+	protected List<String> getBootstrapContextLocations() {
+		List<String> locations = new ArrayList<String>();
+		locations.add("META-INF/impala-bootstrap.xml");
+		locations.add("META-INF/impala-jar-bootstrap.xml");
+		return locations;
+	}
+
+}
