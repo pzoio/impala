6388e94020cad29b2dc1a87be50a3e3f17241395
philzoio <philzoio@realsolve.co.uk>
2008-02-01 20:24:50 +0000

    Implemented with tests

--- impala/impala-core/test/org/impalaframework/spring/config/OptionalPropertiesFactoryBeanTest.java
+++ impala/impala-core/test/org/impalaframework/spring/config/OptionalPropertiesFactoryBeanTest.java
@@ -10,15 +10,34 @@ import junit.framework.TestCase;
 
 public class OptionalPropertiesFactoryBeanTest extends TestCase {
 
-	public final void testSetLocationResource() throws IOException {
-		OptionalPropertiesFactoryBean factoryBean = new OptionalPropertiesFactoryBean();
-		
-		Resource[] resources = new Resource[2];
+	private OptionalPropertiesFactoryBean factoryBean;
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		factoryBean = new OptionalPropertiesFactoryBean();
+	}
+	
+	public final void testNonexistOnly() throws IOException {
+		Resource[] resources = new Resource[1];
 		resources[0] = new ClassPathResource("aresourcewhichdoesnotexist");
+		factoryBean.setLocations(resources);
 		factoryBean.afterPropertiesSet();
 		
 		Properties properties = (Properties) factoryBean.getObject();
 		assertEquals(0, properties.size());
+	}
+	
+	public final void testWithExisting() throws IOException {
+		Resource[] resources = new Resource[2];
+		resources[0] = new ClassPathResource("aresourcewhichdoesnotexist");
+		resources[1] = new ClassPathResource("beanset.properties");
+		factoryBean.setLocations(resources);
+		factoryBean.afterPropertiesSet();
+		
+		Properties properties = (Properties) factoryBean.getObject();
+		assertEquals(3, properties.size());
+		assertEquals("applicationContext-set1.xml", properties.get("set1"));
 	}
 
 }
