3ada497e7b26471993eb7e749db9e03f3dfeba74
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2008-02-03 20:06:16 +0000

    Reversed order to fix ant test breakage

--- trunk/impala/impala-core/test/org/impalaframework/spring/resource/CompositeResourceLoaderTest.java
+++ trunk/impala/impala-core/test/org/impalaframework/spring/resource/CompositeResourceLoaderTest.java
@@ -13,22 +13,24 @@ import org.springframework.util.ClassUtils;
 public class CompositeResourceLoaderTest extends TestCase {
 
 	private List<ResourceLoader> resourceLoaders;
+
 	private CompositeResourceLoader resourceLoader;
 
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
 		resourceLoaders = new ArrayList<ResourceLoader>();
-		resourceLoaders.add(new ClassPathResourceLoader());
 		resourceLoaders.add(new FileSystemResourceLoader());
+		resourceLoaders.add(new ClassPathResourceLoader());
 		resourceLoader = new CompositeResourceLoader(resourceLoaders);
 	}
-	
+
 	public final void testGetResourceOnFileSystem() {
-		Resource resource = resourceLoader.getResource("../impala-core/files/MyTestClass.jar", ClassUtils.getDefaultClassLoader());
+		Resource resource = resourceLoader.getResource("../impala-core/files/MyTestClass.jar", ClassUtils
+				.getDefaultClassLoader());
 		assertTrue(resource instanceof FileSystemResource);
 	}
-	
+
 	public final void testGetResourceOnClassPath() {
 		Resource resource = resourceLoader.getResource("beanset.properties", ClassUtils.getDefaultClassLoader());
 		assertTrue(resource instanceof ClassPathResource);
