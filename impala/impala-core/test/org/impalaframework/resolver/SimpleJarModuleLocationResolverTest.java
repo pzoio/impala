62bdaa4f8ca4705c075824b936f43fd924d64aec
philzoio <philzoio@realsolve.co.uk>
2008-02-04 20:57:19 +0000

    Wiring in applicationVersion

--- impala/impala-core/test/org/impalaframework/resolver/SimpleJarModuleLocationResolverTest.java
+++ impala/impala-core/test/org/impalaframework/resolver/SimpleJarModuleLocationResolverTest.java
@@ -16,14 +16,22 @@ public class SimpleJarModuleLocationResolverTest extends TestCase {
 		jarResolver = new SimpleJarModuleLocationResolver();
 		jarResolver.setRootProjectString("impala-core");
 		jarResolver.setWorkspaceRoot("../impala-core/files");
+		System.setProperty(LocationConstants.WORKSPACE_ROOT_PROPERTY, "../impala-core/files");
+		jarResolver.init();
 	}
 	
 	public void testGetApplicationModuleClassLocations() throws Exception {
-		System.setProperty(LocationConstants.WORKSPACE_ROOT_PROPERTY, "../impala-core/files");
-		jarResolver.init();
 		List<Resource> locations = jarResolver.getApplicationModuleClassLocations("MyTestClass");
 		assertEquals(1, locations.size());
 		assertNotNull(locations.get(0).getInputStream());
+		assertEquals("MyTestClass.jar", locations.get(0).getFilename());
+	}
+	
+	public void testWithApplicationVersion() throws Exception {
+		jarResolver.setApplicationVersion("1.0");
+		List<Resource> locations = jarResolver.getApplicationModuleClassLocations("MyTestClass");
+		assertEquals(1, locations.size());
+		assertEquals("MyTestClass-1.0.jar", locations.get(0).getFilename());
 	}
 
 }
