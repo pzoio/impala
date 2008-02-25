5da9b346be9ce5e95b31290d3a351dc10faa49e3
philzoio <philzoio@realsolve.co.uk>
2008-02-25 21:29:22 +0000

    Added isRootProject method

--- impala/impala-interactive/src/org/impalaframework/command/interactive/InteractiveCommandUtils.java
+++ impala/impala-interactive/src/org/impalaframework/command/interactive/InteractiveCommandUtils.java
@@ -2,7 +2,11 @@ package org.impalaframework.command.interactive;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.Arrays;
+import java.util.List;
 
+import org.impalaframework.exception.ConfigurationException;
+import org.impalaframework.resolver.LocationConstants;
 import org.impalaframework.util.MemoryUtils;
 import org.springframework.util.StopWatch;
 
@@ -23,6 +27,30 @@ public class InteractiveCommandUtils {
 		else {
 			System.out.println("No module found which matches the text '" + suppliedName + "'");
 		}
+	}
+	
+	public static boolean isRootProject(String directoryName) {
+		List<String> projectList = getProjectList();
+		if (projectList.contains(directoryName.trim())) {
+			return true;
+		}
+		return false;
+	}
+
+	private static List<String> getProjectList() {
+		String rootProjectsString = System.getProperty(LocationConstants.ROOT_PROJECTS_PROPERTY);
+		
+		if (rootProjectsString == null) {
+			throw new ConfigurationException("'" + LocationConstants.ROOT_PROJECTS_PROPERTY + "' has not been specified");
+		}
+		
+		String[] projects = rootProjectsString.split(",");
+		for (int i = 0; i < projects.length; i++) {
+			projects[i] = projects[i].trim();
+		}
+		
+		List<String> projectList = Arrays.asList(projects);
+		return projectList;
 	}
 	
 }
