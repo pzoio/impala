667e6e29692ac79754b482bbbc06a7423823fb2e
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2008-02-25 09:10:02 +0000

    Changed comments

--- trunk/impala/impala-interactive/src/org/impalaframework/command/interactive/LoadTestClassContextCommand.java
+++ trunk/impala/impala-interactive/src/org/impalaframework/command/interactive/LoadTestClassContextCommand.java
@@ -4,7 +4,9 @@ import org.impalaframework.command.framework.Command;
 import org.impalaframework.command.framework.CommandDefinition;
 import org.impalaframework.command.framework.CommandState;
 import org.impalaframework.command.framework.GlobalCommandState;
+import org.impalaframework.facade.DynamicContextHolder;
 import org.impalaframework.module.definition.ModuleDefinitionSource;
+import org.springframework.util.ClassUtils;
 
 public class LoadTestClassContextCommand implements Command {
 
@@ -14,25 +16,46 @@ public class LoadTestClassContextCommand implements Command {
 			System.out.println("No test class set.");
 			return false;
 		}
-		
+
 		String testClassName = property.toString();
-		loadTestClass(testClassName);
-		return true;
+		return loadTestClass(testClassName);
 	}
-	
-	private void loadTestClass(String testClassName) {
-		
+
+	private boolean loadTestClass(String testClassName) {
+
 		Class<?> c = null;
 		try {
-			c = Class.forName(testClassName);
+
+			// FIXME this only allows you to switch directories to one containing a loaded module
+
+			String directoryName = (String) GlobalCommandState.getInstance().getValue(
+					CommandStateConstants.DIRECTORY_NAME);
+			ClassLoader parent = null;
+
+			try {
+				if (directoryName != null) {
+					parent = DynamicContextHolder.getModuleContext(directoryName).getClassLoader();
+				}
+				else {
+					parent = DynamicContextHolder.get().getClassLoader();
+				}
+			}
+			catch (Exception e) {
+				//TODO more precise error message required here
+				System.out.println("Unable to load module corresponding with directory name " + (directoryName != null ? directoryName : "[not set]"));
+				parent = ClassUtils.getDefaultClassLoader();
+			}
+
+			c = Class.forName(testClassName, false, parent);
 			try {
 				Object o = c.newInstance();
 				if (o instanceof ModuleDefinitionSource) {
 					ModuleDefinitionSource p = (ModuleDefinitionSource) o;
 					GlobalCommandState.getInstance().addValue(CommandStateConstants.MODULE_DEFINITION_SOURCE, p);
 				}
-				
+
 				GlobalCommandState.getInstance().addValue(CommandStateConstants.TEST_CLASS, c);
+				return false;
 			}
 			catch (Throwable e) {
 				System.out.println("Unable to instantiate " + testClassName);
@@ -43,7 +66,7 @@ public class LoadTestClassContextCommand implements Command {
 			System.out.println("Unable to find test class " + testClassName);
 			InteractiveCommandUtils.printException(e);
 		}
-		
+		return true;
 	}
 
 	public CommandDefinition getCommandDefinition() {
