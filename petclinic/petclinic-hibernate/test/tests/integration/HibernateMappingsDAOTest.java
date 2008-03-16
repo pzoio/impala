ff5b1c2acdab96e26f530d1f21f217ffb51409a5
philzoio <philzoio@realsolve.co.uk>
2008-03-16 21:29:23 +0000

    Upgraded to latest impala version. Web app still broken

--- petclinic/petclinic-hibernate/test/tests/integration/HibernateMappingsDAOTest.java
+++ petclinic/petclinic-hibernate/test/tests/integration/HibernateMappingsDAOTest.java
@@ -3,28 +3,29 @@ package tests.integration;
 import junit.framework.TestCase;
 
 import org.hibernate.SessionFactory;
+import org.impalaframework.facade.Impala;
 import org.impalaframework.module.builder.SimpleModuleDefinitionSource;
 import org.impalaframework.module.definition.ModuleDefinitionSource;
 import org.impalaframework.module.definition.RootModuleDefinition;
-import org.impalaframework.testrun.DynamicContextHolder;
-import org.impalaframework.testrun.ImpalaTestRunner;
+import org.impalaframework.resolver.LocationConstants;
+import org.impalaframework.testrun.InteractiveTestRunner;
 
 public class HibernateMappingsDAOTest extends TestCase implements
 		ModuleDefinitionSource {
 
 	public static void main(String[] args) {
-		System.setProperty("impala.parent.project", "petclinic");
-		ImpalaTestRunner.run(HibernateMappingsDAOTest.class);
+		System.setProperty(LocationConstants.ROOT_PROJECTS_PROPERTY, "petclinic");
+		InteractiveTestRunner.run(HibernateMappingsDAOTest.class);
 	}
 
 	public void setUp() throws Exception {
 		super.setUp();
-		System.setProperty("impala.parent.project", "petclinic");
-		DynamicContextHolder.init(this);
+		System.setProperty(LocationConstants.ROOT_PROJECTS_PROPERTY, "petclinic");
+		Impala.init(this);
 	}
 	
 	public void testMappings() throws Exception {
-		System.out.println("Session factory: " + DynamicContextHolder.getBean("sessionFactory", SessionFactory.class));
+		System.out.println("Session factory: " + Impala.getBean("sessionFactory", SessionFactory.class));
 	}
 
 	public RootModuleDefinition getModuleDefinition() {
