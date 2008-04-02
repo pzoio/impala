fc9d105d37d7124a7142bc0937fa66bee30f42e1
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2008-04-02 10:40:57 +0000

    Renamed bootstrap.properties to impala.properties

--- trunk/impala/impala/scaffold/java/web/test/StartServer.java
+++ trunk/impala/impala/scaffold/java/web/test/StartServer.java
@@ -21,7 +21,7 @@ import org.impalaframework.web.WebConstants;
 public class StartServer {
 	public static void main(String[] args) {
 		System.setProperty("org.mortbay.log.class", "org.mortbay.log.StdErrLog");
-		System.setProperty(WebConstants.BOOTSTRAP_LOCATIONS_RESOURCE_PARAM, "bootstrap-embedded.properties");
+		System.setProperty(WebConstants.BOOTSTRAP_LOCATIONS_RESOURCE_PARAM, "impala-embedded.properties");
 		System.setProperty(LocationConstants.WORKSPACE_ROOT_PROPERTY, new File("../").getAbsolutePath());
 		StartJetty.main(new String[]{"8080", "../@web.project.name@/context", "/@web.project.name@"});
 	}
