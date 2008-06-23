b0751538bfa4aac44206dfacb965c728be3fd760
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2008-06-23 19:25:51 +0000

    Ticket 12: further asserts for InternalModuleDefinitionSourceTest

--- trunk/impala/impala-core/test/org/impalaframework/module/builder/InternalXmlModuleDefinitionSourceTest.java
+++ trunk/impala/impala-core/test/org/impalaframework/module/builder/InternalXmlModuleDefinitionSourceTest.java
@@ -1,10 +1,14 @@
 package org.impalaframework.module.builder;
 
+import java.util.Arrays;
+
+import junit.framework.TestCase;
+
+import org.impalaframework.module.definition.ModuleDefinition;
+import org.impalaframework.module.definition.ModuleTypes;
 import org.impalaframework.module.definition.RootModuleDefinition;
 import org.impalaframework.resolver.StandaloneModuleLocationResolver;
 import org.springframework.core.io.ClassPathResource;
-
-import junit.framework.TestCase;
 
 public class InternalXmlModuleDefinitionSourceTest extends TestCase {
 
@@ -20,7 +24,33 @@ public class InternalXmlModuleDefinitionSourceTest extends TestCase {
 	public void testGetModuleDefinition() {
 		RootModuleDefinition moduleDefinition = moduleDefinitionSource.getModuleDefinition();
 		System.out.println(moduleDefinition);
-		//FIXME add asserts
+		
+		assertEquals(Arrays.asList(new String[]{"parentTestContext.xml"}), moduleDefinition.getContextLocations());
+		assertEquals(2, moduleDefinition.getChildDefinitions().size());
+		assertEquals(Arrays.asList(new String[]{"impala-core"}), moduleDefinition.getRootProjectNames());
+		
+		ModuleDefinition definition1 = getDefinition(moduleDefinition, "sample-module1");
+		assertEquals(ModuleTypes.APPLICATION, definition1.getType());
+		assertEquals(Arrays.asList(new String[]{"sample-module1-context.xml"}), definition1.getContextLocations());
+		
+		ModuleDefinition definition2 = getDefinition(moduleDefinition, "sample-module2");
+		assertEquals(ModuleTypes.APPLICATION, definition2.getType());
+		assertEquals(Arrays.asList(new String[]{"sample-module2-context.xml"}), definition2.getContextLocations());
+		
+		ModuleDefinition definition3 = getDefinition(definition2, "sample-module3");
+		assertEquals(ModuleTypes.APPLICATION, definition3.getType());
+		assertEquals(Arrays.asList(new String[]{"sample-module3-context.xml"}), definition3.getContextLocations());
+		
+		ModuleDefinition definition4 = getDefinition(definition2, "sample-module4");
+		assertEquals(ModuleTypes.APPLICATION_WITH_BEANSETS, definition4.getType());
+		assertEquals(Arrays.asList(new String[]{"sample-module4-context.xml"}), definition4.getContextLocations());
+	}
+
+	private ModuleDefinition getDefinition(ModuleDefinition moduleDefinition,
+			String moduleName) {
+		ModuleDefinition def = moduleDefinition.getModule(moduleName);
+		assertNotNull(def);
+		return def;
 	}
 
 }
