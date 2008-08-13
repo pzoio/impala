00eed63070329894c02aacd9e8864d8d5c505f8e
philzoio <philzoio@realsolve.co.uk>
2008-08-13 20:39:00 +0000

    Issue 28: extracted ImpalaServletUtils class

---
+++ impala/impala-web/src/org/impalaframework/web/generic/FrameworkIntegrationServlet.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2007-2008 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.impalaframework.web.generic;
+
+import java.io.IOException;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.impalaframework.web.servlet.FrameworkServletContextCreator;
+import org.impalaframework.web.servlet.ImpalaServletUtils;
+import org.springframework.beans.BeansException;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.FrameworkServlet;
+
+/**
+ * Servlet base class which preforms similar function to <code>BaseImpalaServlet</code>
+ * except that it does not participate in the Spring MVC dispatch infrastructure, hence
+ * it subclasses directly from <code>FrameworkServlet</code>, and not from
+ * <code>DispatcherServlet</code>.
+ * @author Phil Zoio
+ */
+public abstract class FrameworkIntegrationServlet extends FrameworkServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	private final ReentrantReadWriteLock rwl = new ReentrantReadWriteLock();
+	private final Lock r = rwl.readLock();
+	private final Lock w = rwl.writeLock();
+
+	private FrameworkServletContextCreator helper;
+	
+	public FrameworkIntegrationServlet() {
+		super();
+		this.helper = new FrameworkServletContextCreator(this);
+	}
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		r.lock();
+		try {
+			//FIXME wire in connectivity
+			WebApplicationContext wac = this.getWebApplicationContext();
+			//look for servlet instance
+			
+			HttpServlet servlet = null;
+			
+			servlet.service(request, response);
+		}
+		finally {
+			r.unlock();
+		}
+	}
+
+	@Override
+	protected WebApplicationContext initWebApplicationContext() throws BeansException {
+		w.lock();
+		try {
+			return createContext();
+		}
+		finally {
+			w.unlock();
+		}
+	}
+
+	private WebApplicationContext createContext() {
+		WebApplicationContext wac = this.helper.createWebApplicationContext();
+		return ImpalaServletUtils.initWithContext(this, wac);
+	}
+}
---
+++ impala/impala-web/src/org/impalaframework/web/servlet/ImpalaServletUtils.java
@@ -0,0 +1,24 @@
+package org.impalaframework.web.servlet;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.FrameworkServlet;
+
+public class ImpalaServletUtils {
+
+	private static final Log logger = LogFactory.getLog(ImpalaServletUtils.class);
+	
+	public static WebApplicationContext initWithContext(FrameworkServlet servlet, WebApplicationContext wac) {
+
+		// Publish the context as a servlet context attribute.
+		String attrName = servlet.getServletContextAttributeName();
+		servlet.getServletContext().setAttribute(attrName, wac);
+		if (logger.isDebugEnabled()) {
+			logger.debug("Published WebApplicationContext of servlet '" + servlet.getServletName()
+					+ "' as ServletContext attribute with name [" + attrName + "]");
+		}
+
+		return wac;
+	}
+}
