aec1b2d6f7ba64ead98aa6679be06d3f38b4d60d
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2008-08-14 20:59:52 +0000

    Ticket 42: Copyright notices. Added default to StringPropertyValue

--- trunk/impala/impala-core/src/org/impalaframework/config/StringPropertyValue.java
+++ trunk/impala/impala-core/src/org/impalaframework/config/StringPropertyValue.java
@@ -1,19 +1,33 @@
+/*
+ * Copyright 2007-2008 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package org.impalaframework.config;
 
-import org.springframework.beans.factory.FactoryBean;
 
-public class StringPropertyValue extends BasePropertyValue implements FactoryBean {
+public class StringPropertyValue extends BasePropertyValue  {
 
-	public Object getObject() throws Exception {
-		return getValue();
+	private String defaultValue;
+
+	@Override
+	protected String getValue() {
+		String value = super.getValue();
+		if (value == null) return defaultValue;
+		return value;
 	}
 
-	public Class<?> getObjectType() {
-		return String.class;
-	}
-
-	public boolean isSingleton() {
-		return false;
+	public void setDefaultValue(String defaultValue) {
+		this.defaultValue = defaultValue;
 	}
 
 }
