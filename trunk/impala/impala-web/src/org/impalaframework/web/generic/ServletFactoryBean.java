369b1864a97e87bcdfdbe327dbe00f0b723869b5
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2008-09-10 11:17:39 +0000

    Ticket #49: defaulting servlet name to module name

--- trunk/impala/impala-web/src/org/impalaframework/web/generic/ServletFactoryBean.java
+++ trunk/impala/impala-web/src/org/impalaframework/web/generic/ServletFactoryBean.java
@@ -6,6 +6,8 @@ import java.util.Map;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServlet;
 
+import org.impalaframework.module.definition.ModuleDefinition;
+import org.impalaframework.spring.module.ModuleDefinitionAware;
 import org.impalaframework.util.InstantiationUtils;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.DisposableBean;
@@ -15,7 +17,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.context.ServletContextAware;
 
-public class ServletFactoryBean implements FactoryBean, ServletContextAware, InitializingBean, DisposableBean, ApplicationContextAware {
+public class ServletFactoryBean implements FactoryBean, ServletContextAware, InitializingBean, DisposableBean, ApplicationContextAware, ModuleDefinitionAware {
 
 	private ServletContext servletContext;
 	private Map<String,String> initParameters;
@@ -23,6 +25,7 @@ public class ServletFactoryBean implements FactoryBean, ServletContextAware, Ini
 	private String servletName;
 	private Class<?> servletClass;
 	private ApplicationContext applicationContext;
+	private ModuleDefinition moduleDefintion;
 
 	public Object getObject() throws Exception {
 		return servlet;
@@ -39,6 +42,10 @@ public class ServletFactoryBean implements FactoryBean, ServletContextAware, Ini
 	/* ***************** InitializingBean implementation **************** */
 	
 	public void afterPropertiesSet() throws Exception {
+		if (servletName == null) {
+			servletName = moduleDefintion.getName();
+		}
+		
 		servlet = InstantiationUtils.instantiate(servletClass.getName());
 		Map<String, String> emptyMap = Collections.emptyMap();
 		Map<String,String> parameterMap = (initParameters != null ? initParameters : emptyMap);
@@ -83,6 +90,10 @@ public class ServletFactoryBean implements FactoryBean, ServletContextAware, Ini
 
 	public void setServletClass(Class<?> servletClass) {
 		this.servletClass = servletClass;
+	}
+
+	public void setModuleDefinition(ModuleDefinition moduleDefinition) {
+		this.moduleDefintion = moduleDefinition;
 	}
 
 }
