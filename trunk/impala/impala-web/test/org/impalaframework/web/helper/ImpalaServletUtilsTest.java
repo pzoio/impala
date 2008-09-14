51e65f17d5203723c123d4b1b6153f2b0c4da5be
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2008-09-14 21:35:37 +0000

    Ticket #28: added test

---
+++ trunk/impala/impala-web/test/org/impalaframework/web/helper/ImpalaServletUtilsTest.java
@@ -0,0 +1,22 @@
+package org.impalaframework.web.helper;
+
+
+import junit.framework.TestCase;
+
+import org.easymock.classextension.EasyMock;
+import org.impalaframework.exception.ConfigurationException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.WebApplicationContext;
+
+public class ImpalaServletUtilsTest extends TestCase {
+
+	public void testCheckIsWebApplicationContext() {
+		ImpalaServletUtils.checkIsWebApplicationContext("myservlet", EasyMock.createMock(WebApplicationContext.class));
+		try {
+			ImpalaServletUtils.checkIsWebApplicationContext("myservlet", EasyMock.createMock(ApplicationContext.class));
+		} catch (ConfigurationException e) {
+			assertEquals("Servlet 'myservlet' is not backed by an application context of type org.springframework.web.context.WebApplicationContext: EasyMock for interface org.springframework.context.ApplicationContext", e.getMessage());
+		}
+	}
+
+}
