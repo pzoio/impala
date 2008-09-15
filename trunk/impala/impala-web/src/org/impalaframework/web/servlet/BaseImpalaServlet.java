a803a38043b52477b9089a38b8ab7213824f4106
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2008-09-15 20:24:54 +0000

    Ticket #51: removed FIXME

--- trunk/impala/impala-web/src/org/impalaframework/web/servlet/BaseImpalaServlet.java
+++ trunk/impala/impala-web/src/org/impalaframework/web/servlet/BaseImpalaServlet.java
@@ -67,7 +67,6 @@ public abstract class BaseImpalaServlet extends DispatcherServlet implements Htt
 			//However, it must be called
 			onRefresh(wac);
 			
-			//FIXME we should unpublish this if the application context closes
 			ImpalaServletUtils.publishWebApplicationContext(wac, this);
 			return wac;
 		}
