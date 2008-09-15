a1bbde8d9249098743b17ff514dcc417dbdd01fe
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2008-09-15 20:23:17 +0000

    Ticket #51: reformatting/method reordering

--- trunk/impala/impala-web/src/org/impalaframework/web/integration/InternalFrameworkIntegrationServletFactoryBean.java
+++ trunk/impala/impala-web/src/org/impalaframework/web/integration/InternalFrameworkIntegrationServletFactoryBean.java
@@ -5,9 +5,14 @@ import javax.servlet.http.HttpServlet;
 import org.impalaframework.exception.ConfigurationException;
 import org.springframework.util.Assert;
 
-//FIXME test
+/**
+ * <code>FactoryBean</code> whose purpose is solely to create an instance of <code>InternalFrameworkIntegrationServlet</code>
+ * @author Phil Zoio
+ */
 public class InternalFrameworkIntegrationServletFactoryBean extends
 		ServletFactoryBean {
+
+	private HttpServlet delegateServlet;
 	
 	public Class<?> getObjectType() {
 		return InternalFrameworkIntegrationServlet.class;
@@ -18,7 +23,6 @@ public class InternalFrameworkIntegrationServletFactoryBean extends
 		super.initServletProperties(servlet);
 		Assert.notNull(delegateServlet, "delegateServlet cannot be null");
 		
-		//FIXME test
 		if (!(servlet instanceof InternalFrameworkIntegrationServlet)) {
 			throw new ConfigurationException(servlet + " must be an instanceof " + InternalFrameworkIntegrationServlet.class.getName());
 		}
@@ -26,8 +30,8 @@ public class InternalFrameworkIntegrationServletFactoryBean extends
 		InternalFrameworkIntegrationServlet integrationServlet = (InternalFrameworkIntegrationServlet) servlet;
 		integrationServlet.setDelegateServlet(delegateServlet);
 	}
-
-	private HttpServlet delegateServlet;
+	
+	/* *************** Injected setters ***************** */
 
 	public void setDelegateServlet(HttpServlet servlet) {
 		this.delegateServlet = servlet;
