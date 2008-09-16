c4d0a72b9281c8b8069689723dfd5539bd3d2117
philzoio <philzoio@realsolve.co.uk>
2008-09-16 09:05:21 +0000

    Ticket #58: comments and tidying up

--- impala/impala-core/src/org/impalaframework/util/serialize/SerializationHelper.java
+++ impala/impala-core/src/org/impalaframework/util/serialize/SerializationHelper.java
@@ -41,11 +41,12 @@ import org.impalaframework.exception.ExecutionException;
  * <p>This class throws exceptions for invalid <code>null</code> inputs.
  * Each method documents its behaviour in more detail.</p>
  * 
- * <p>Based on the class SerializationHelper from Apache commons lang, with one change being 
- * that <code>ExecutionException</code> is thrown instead of <code>SerializationException</code></p>
+ * <p>Based on the class <code>SerializationUtils</code> from Apache commons lang.</p>
  * 
+ * <p>Throwing <code>ExecutionException</code> rather than <code>SerializationException</code> on error.</p>
  * <p>Added <code>SerializationStreamFactory</code> with the purpose of customising serialization/deserialization mechanism</p>
  *
+ * @see org.impalaframework.util.serializeDefaultSerializationStreamFactory
  * @author <a href="mailto:nissim@nksystems.com">Nissim Karpenstein</a>
  * @author <a href="mailto:janekdb@yahoo.co.uk">Janek Bogucki</a>
  * @author <a href="mailto:dlr@finemaltcoding.com">Daniel Rall</a>
@@ -53,63 +54,28 @@ import org.impalaframework.exception.ExecutionException;
  * @author Jeff Varszegi
  * @author Gary Gregory
  * @author Phil Zoio
- * @since 1.0
- * @version $Id: SerializationHelper.java 437554 2006-08-28 06:21:41Z bayard $
  */
 public class SerializationHelper {
     
 	private SerializationStreamFactory factory;
 	
     /**
-     * <p>SerializationHelper instances should NOT be constructed in standard programming.
-     * Instead, the class should be used as <code>SerializationHelper.clone(object)</code>.</p>
-     *
-     * <p>This constructor is public to permit tools that require a JavaBean instance
-     * to operate.</p>
-     * @since 2.0
+     * <p>Default constructor, which internally use an instance of <code>DefaultSerializationStreamFactory</code> </p>
      */
     public SerializationHelper() {
         super();
         factory = new DefaultSerializationStreamFactory();
     }
-    
 
-    // Clone
-    //-----------------------------------------------------------------------
-    /**
-     * <p>Deep clone an <code>Object</code> using serialization.</p>
-     *
-     * <p>This is many times slower than writing clone methods by hand
-     * on all objects in your object graph. However, for complex object
-     * graphs, or for those that don't support deep cloning this can
-     * be a simple alternative implementation. Of course all the objects
-     * must be <code>Serializable</code>.</p>
-     * 
-     * @param object  the <code>Serializable</code> object to clone
-     * @return the cloned object
-     * @throws SerializationException (runtime) if the serialization fails
-     */
+    public SerializationHelper(SerializationStreamFactory factory) {
+		super();
+		this.factory = factory;
+	}
+
     public Object clone(Serializable object) {
         return deserialize(serialize(object));
     }
     
-    // Serialize
-    //-----------------------------------------------------------------------
-    /**
-     * <p>Serializes an <code>Object</code> to the specified stream.</p>
-     *
-     * <p>The stream will be closed once the object is written.
-     * This avoids the need for a finally clause, and maybe also exception
-     * handling, in the application code.</p>
-     * 
-     * <p>The stream passed in is not buffered internally within this method.
-     * This is the responsibility of your application if desired.</p>
-     *
-     * @param obj  the object to serialize to bytes, may be null
-     * @param outputStream  the stream to write to, must not be null
-     * @throws IllegalArgumentException if <code>outputStream</code> is <code>null</code>
-     * @throws SerializationException (runtime) if the serialization fails
-     */
     public void serialize(Serializable obj, OutputStream outputStream) {
         if (outputStream == null) {
             throw new IllegalArgumentException("The OutputStream must not be null");
@@ -133,37 +99,14 @@ public class SerializationHelper {
         }
     }
 
-    /**
-     * <p>Serializes an <code>Object</code> to a byte array for
-     * storage/serialization.</p>
-     *
-     * @param obj  the object to serialize to bytes
-     * @return a byte[] with the converted Serializable
-     * @throws SerializationException (runtime) if the serialization fails
-     */
     public byte[] serialize(Serializable obj) {
         ByteArrayOutputStream baos = new ByteArrayOutputStream(512);
         serialize(obj, baos);
         return baos.toByteArray();
     }
 
-    // Deserialize
-    //-----------------------------------------------------------------------
-    /**
-     * <p>Deserializes an <code>Object</code> from the specified stream.</p>
-     *
-     * <p>The stream will be closed once the object is written. This
-     * avoids the need for a finally clause, and maybe also exception
-     * handling, in the application code.</p>
-     * 
-     * <p>The stream passed in is not buffered internally within this method.
-     * This is the responsibility of your application if desired.</p>
-     *
-     * @param inputStream  the serialized object input stream, must not be null
-     * @return the deserialized object
-     * @throws IllegalArgumentException if <code>inputStream</code> is <code>null</code>
-     * @throws SerializationException (runtime) if the serialization fails
-     */
+    /* **************  Deserialize **************** */
+ 
     public Object deserialize(InputStream inputStream) {
         if (inputStream == null) {
             throw new IllegalArgumentException("The InputStream must not be null");
@@ -189,14 +132,6 @@ public class SerializationHelper {
         }
     }
 
-    /**
-     * <p>Deserializes a single <code>Object</code> from an array of bytes.</p>
-     *
-     * @param objectData  the serialized object, must not be null
-     * @return the deserialized object
-     * @throws IllegalArgumentException if <code>objectData</code> is <code>null</code>
-     * @throws SerializationException (runtime) if the serialization fails
-     */
     public Object deserialize(byte[] objectData) {
         if (objectData == null) {
             throw new IllegalArgumentException("The byte[] must not be null");
