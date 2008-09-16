a09e059e81b4c93f2a897fb37c4767c3004f0985
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2008-09-16 19:48:25 +0000

    No bug: added copyright statement

---
+++ trunk/impala/impala-web/src/org/impalaframework/web/servlet/wrapper/ModuleServletWrapperFactory.java
@@ -0,0 +1,11 @@
+package org.impalaframework.web.servlet.wrapper;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class ModuleServletWrapperFactory {
+	
+	public HttpServletRequest wrapRequest(HttpServletRequest request) {
+		return request;
+	}
+
+}
