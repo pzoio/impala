974e260a9a4f51e0a11087de74c2d1c01d81c7aa
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2008-09-18 11:22:14 +0000

    Ticket #51: Added utility for retrieving ModuleManagementFactory

--- trunk/impala/impala-web/src/org/impalaframework/web/loader/BaseImpalaContextLoader.java
+++ trunk/impala/impala-web/src/org/impalaframework/web/loader/BaseImpalaContextLoader.java
@@ -27,6 +27,7 @@ import org.impalaframework.module.operation.ModuleOperationInput;
 import org.impalaframework.util.ObjectUtils;
 import org.impalaframework.web.WebConstants;
 import org.impalaframework.web.bootstrap.DefaultBootstrapLocationResolutionStrategy;
+import org.impalaframework.web.helper.ImpalaServletUtils;
 import org.impalaframework.web.module.ServletModuleDefinitionSource;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -40,26 +41,47 @@ import org.springframework.web.context.ContextLoader;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 
+/**
+ * Extension of <code>ContextLoader</code> which implements base functionality for initializing Impala within a Spring web
+ * application. This class is abstract. It defines the abstract method <code>getModuleDefinitionSource</code>, which subclasses must
+ * implement. The implementation of this method provides the strategy for loading metadata which specifies the structure
+ * of the modules to be loaded as part of this web application. 
+ * @author Phil Zoio
+ */
 public abstract class BaseImpalaContextLoader extends ContextLoader implements ServletModuleDefinitionSource {
 
 	private static final Log logger = LogFactory.getLog(BaseImpalaContextLoader.class);
 
+	/* ************************* Overridden superclass methods ******************** */
+
+	@Override
+	public WebApplicationContext initWebApplicationContext(
+			ServletContext servletContext) throws IllegalStateException,
+			BeansException {
+		return super.initWebApplicationContext(servletContext);
+	}
+	
+	/**
+	 * Creates the <code>WebApplicationContext</code> by bootstrapping Impala, retrieving the <code>ModuleDefinitionSource</code>
+	 * for loading the module metadata. It then instantiates the application context and returns it for further processing by
+	 * <code>ContextLoader</code>.
+	 */
 	@Override
 	protected WebApplicationContext createWebApplicationContext(ServletContext servletContext, ApplicationContext parent)
 			throws BeansException {
 
-		ModuleManagementFactory factory = createBootStrapFactory(servletContext);
+		ModuleManagementFactory factory = createModuleManagementFactory(servletContext);
 
 		// load the parent context, which is web-independent
 		ModuleDefinitionSource moduleDefinitionSource = getModuleDefinitionSource(servletContext, factory);
-		
-		ModuleOperationInput input = new ModuleOperationInput(moduleDefinitionSource, null, null);
-		ModuleOperation operation = factory.getModuleOperationRegistry().getOperation(ModuleOperationConstants.UpdateRootModuleOperation);		
-		operation.execute(input);
 
 		// add items to servlet context
 		servletContext.setAttribute(WebConstants.MODULE_DEFINITION_SOURCE_ATTRIBUTE, moduleDefinitionSource);
 		servletContext.setAttribute(WebConstants.IMPALA_FACTORY_ATTRIBUTE, factory);
+		
+		ModuleOperationInput input = new ModuleOperationInput(moduleDefinitionSource, null, null);
+		ModuleOperation operation = factory.getModuleOperationRegistry().getOperation(ModuleOperationConstants.UpdateRootModuleOperation);		
+		operation.execute(input);
 
 		ConfigurableApplicationContext context = factory.getModuleStateHolder().getRootModuleContext();
 
@@ -76,29 +98,16 @@ public abstract class BaseImpalaContextLoader extends ContextLoader implements S
 		return (WebApplicationContext) context;
 	}
 
-	protected ModuleManagementFactory createBootStrapFactory(ServletContext servletContext) {
-		String[] locations = getBootstrapContextLocations(servletContext);
-		logger.info("Loading bootstrap context from locations " + Arrays.toString(locations));
-
-		final DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
-		final GenericWebApplicationContext applicationContext = new GenericWebApplicationContext(beanFactory);
-		applicationContext.setServletContext(servletContext);
-
-		XmlBeanDefinitionReader definitionReader = new XmlBeanDefinitionReader(beanFactory);
-		for (int i = 0; i < locations.length; i++) {
-			definitionReader.loadBeanDefinitions(new ClassPathResource(locations[i]));
-		}
-		applicationContext.refresh();
-
-		return ObjectUtils.cast(applicationContext.getBean("moduleManagementFactory"), ModuleManagementFactory.class);
-	}
-
+	/**
+	 * Overrides <code>ContextLoader</code> superclass. First, the modules are shut down. 
+	 * The superclass <code>closeWebApplicationContext</code> is then called.
+	 * Finally Impala is shut down, in the form of the <code>ModuleManagementFactory.close()</code>
+	 */
 	@Override
 	public void closeWebApplicationContext(ServletContext servletContext) {
 
 		// the superclass closes the modules
-		ModuleManagementFactory factory = (ModuleManagementFactory) servletContext
-				.getAttribute(WebConstants.IMPALA_FACTORY_ATTRIBUTE);
+		ModuleManagementFactory factory = ImpalaServletUtils.getModuleManagementFactory(servletContext);
 
 		if (factory != null) {
 
@@ -116,11 +125,34 @@ public abstract class BaseImpalaContextLoader extends ContextLoader implements S
 			factory.close();
 		}
 	}
+	
+	/* ************************* Internal helper methods ******************** */
+
+	/**
+	 * Instantiates Impala in the form of a <code>ModuleManagementFactory</code> instance.
+	 */
+	protected ModuleManagementFactory createModuleManagementFactory(ServletContext servletContext) {
+		String[] locations = getBootstrapContextLocations(servletContext);
+		logger.info("Loading bootstrap context from locations " + Arrays.toString(locations));
+
+		final DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+		final GenericWebApplicationContext applicationContext = new GenericWebApplicationContext(beanFactory);
+		applicationContext.setServletContext(servletContext);
+
+		XmlBeanDefinitionReader definitionReader = new XmlBeanDefinitionReader(beanFactory);
+		for (int i = 0; i < locations.length; i++) {
+			definitionReader.loadBeanDefinitions(new ClassPathResource(locations[i]));
+		}
+		applicationContext.refresh();
+
+		return ObjectUtils.cast(applicationContext.getBean("moduleManagementFactory"), ModuleManagementFactory.class);
+	}
 
 	public String[] getBootstrapContextLocations(ServletContext servletContext) {
 		return new DefaultBootstrapLocationResolutionStrategy().getBootstrapContextLocations(servletContext);
 	}
 
 	public abstract ModuleDefinitionSource getModuleDefinitionSource(ServletContext servletContext, ModuleManagementFactory factory);
+
 
 }
