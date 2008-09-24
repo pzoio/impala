f9961ad4ef8a1ea0adc934bf5592d2dfaae68578
philzoio <philzoio@realsolve.co.uk>
2008-09-24 20:06:02 +0000

    No bug: Added check for debug mode

--- impala/impala-interactive/src/org/impalaframework/command/interactive/CommandStateConstants.java
+++ impala/impala-interactive/src/org/impalaframework/command/interactive/CommandStateConstants.java
@@ -16,12 +16,13 @@ package org.impalaframework.command.interactive;
 
 public interface CommandStateConstants {
 
-	public static final String TEST_CLASS = "testClass";
-	public static final String COMMAND_TEXT = "commandText";
-	public static final String LAST_COMMAND = "lastCommand";
-	public static final String TEST_CLASS_NAME = "testClassName";
-	public static final String MODULE_DEFINITION_SOURCE = "moduleDefinitionSource";
-	public static final String DIRECTORY_NAME = "directoryName";
-	public static final String TEST_METHOD_NAME = "testMethodName";
+	String DEBUG_MODE = "debugMode";
+	String TEST_CLASS = "testClass";
+	String COMMAND_TEXT = "commandText";
+	String LAST_COMMAND = "lastCommand";
+	String TEST_CLASS_NAME = "testClassName";
+	String MODULE_DEFINITION_SOURCE = "moduleDefinitionSource";
+	String DIRECTORY_NAME = "directoryName";
+	String TEST_METHOD_NAME = "testMethodName";
 
 }
