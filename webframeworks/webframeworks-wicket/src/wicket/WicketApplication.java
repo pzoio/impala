364d9ee64a83121553f03d114a2498bd7193d1a3
philzoio <philzoio@realsolve.co.uk>
2008-09-28 21:27:26 +0000

    Ticket #53: added wicket example to webframeworks sample

---
+++ webframeworks/webframeworks-wicket/src/wicket/WicketApplication.java
@@ -0,0 +1,28 @@
+package wicket;
+
+import org.apache.wicket.settings.IPageSettings;
+
+/**
+ * Application object for your web application. If you want to run this application without deploying, run the Start class.
+ * 
+ * @see wicket.myproject.Start#main(String[])
+ */
+public class WicketApplication extends org.apache.wicket.protocol.http.WebApplication
+{    
+    /**
+     * Constructor
+     */
+	public WicketApplication()
+	{
+		mountBookmarkablePage("home", HomePage.class);
+	}
+	
+	/**
+	 * @see wicket.Application#getHomePage()
+	 */
+	public Class<?> getHomePage()
+	{
+		return HomePage.class;
+	}
+
+}
