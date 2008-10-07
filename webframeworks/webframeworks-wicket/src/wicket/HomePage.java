cd9172113af631f0b70fd4dc34a5efc28cc2479d
philzoio <philzoio@realsolve.co.uk>
2008-10-07 20:17:42 +0000

    Ticket #53: changes to templates in webframeworks example

--- webframeworks/webframeworks-wicket/src/wicket/HomePage.java
+++ webframeworks/webframeworks-wicket/src/wicket/HomePage.java
@@ -4,33 +4,13 @@ import org.apache.wicket.PageParameters;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 
-/**
- * Homepage
- */
 public class HomePage extends WebPage {
 
 	private static final long serialVersionUID = 1L;
 
-    /**
-	 * Constructor that is invoked when page is invoked without a session.
-	 * 
-	 * @param parameters
-	 *            Page parameters
-	 */
     public HomePage(final PageParameters parameters) {
 
-        // Add the simplest type of label
-        add(new Label("message", "If you see this message then wicket is properly configured and running. Modify this message in HomePage, then" +
-        		" reload the module 'wicket'. See your changes reflected."));
-
-        // TODO Add your page's components here
+        add(new Label("message", "Hello Phil"));
     }
-
-	@Override
-	protected void onAfterRender() {
-		super.onAfterRender();
-		System.out.println("Just rendered the page");
-	}
-    
     
 }
