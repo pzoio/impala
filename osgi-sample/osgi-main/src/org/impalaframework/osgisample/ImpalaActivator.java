00709b276dacf9dffaecf71a4e0005b4758068e2
philzoio <philzoio@realsolve.co.uk>
2008-10-27 22:24:13 +0000

    Ticket #94: resource loading + impala updates

--- osgi-sample/osgi-main/src/org/impalaframework/osgisample/ImpalaActivator.java
+++ osgi-sample/osgi-main/src/org/impalaframework/osgisample/ImpalaActivator.java
@@ -4,10 +4,13 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 
-import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.osgi.io.OsgiBundleResource;
 import org.springframework.util.FileCopyUtils;
+import org.springframework.web.context.support.GenericWebApplicationContext;
 
 public class ImpalaActivator implements BundleActivator {
 
@@ -20,6 +23,14 @@ public class ImpalaActivator implements BundleActivator {
 			System.out.println(stream);
 			
 			String name = "";
+			
+			final DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+			final GenericWebApplicationContext applicationContext = new GenericWebApplicationContext(beanFactory);
+			//applicationContext.setServletContext(servletContext);
+
+			XmlBeanDefinitionReader definitionReader = new XmlBeanDefinitionReader(beanFactory);
+			definitionReader.loadBeanDefinitions(new OsgiBundleResource(bundleContext.getBundle(), "META-INF/impala-bootstrap.xml"));
+			applicationContext.refresh();
 		}
 	}
 
