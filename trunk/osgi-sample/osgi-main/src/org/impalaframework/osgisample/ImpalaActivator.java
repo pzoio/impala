d41c209340080edea3e6c283f046554f9a9aa89d
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2008-10-27 22:08:58 +0000

    Ticket #94: resource loading

--- trunk/osgi-sample/osgi-main/src/org/impalaframework/osgisample/ImpalaActivator.java
+++ trunk/osgi-sample/osgi-main/src/org/impalaframework/osgisample/ImpalaActivator.java
@@ -4,6 +4,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.springframework.util.FileCopyUtils;
@@ -17,8 +18,12 @@ public class ImpalaActivator implements BundleActivator {
 			InputStream stream = resource.openStream();
 			FileCopyUtils.copyToString(new InputStreamReader(stream));
 			System.out.println(stream);
+			
+			String name = "";
 		}
 	}
+
+
 
 	public void stop(BundleContext bundleContext) throws Exception {
 	}
