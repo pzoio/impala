2f54caa2962819429f03b550ed57dae07011c550
philzoio <philzoio@realsolve.co.uk>
2008-10-27 22:08:58 +0000

    Ticket #94: resource loading

--- osgi-sample/osgi-service/src/org/impalaframework/osgiservice/ResourceActivator.java
+++ osgi-sample/osgi-service/src/org/impalaframework/osgiservice/ResourceActivator.java
@@ -4,8 +4,10 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.springframework.osgi.io.OsgiBundleResource;
 import org.springframework.util.FileCopyUtils;
 
 public class ResourceActivator implements BundleActivator {
@@ -15,10 +17,24 @@ public class ResourceActivator implements BundleActivator {
 		if (resource != null) {
 			InputStream stream = resource.openStream();
 			FileCopyUtils.copyToString(new InputStreamReader(stream));
-			System.out.println(stream);
+			System.out.println("Found resource for META-INF/main.txt: " + stream);
 		}
+
+		Bundle b = getBundleWithName(bundleContext, "org.impalaframework.osgisample");
+		OsgiBundleResource br = new OsgiBundleResource(b, "META-INF/main.txt");
+		System.out.println("Found via bundle: " + br.exists());
 	}
 
 	public void stop(BundleContext bundleContext) throws Exception {	
+	}	
+	
+	private Bundle getBundleWithName(BundleContext bundleContext, String name) {
+		Bundle[] bundles = bundleContext.getBundles();
+		for (Bundle bundle : bundles) {
+			if (name.equals(bundle.getSymbolicName())) {
+				return bundle;
+			}
+		}
+		return null;
 	}
 }
