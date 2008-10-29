ebfededd3bad8b2ba651300ea22913414c6aeca8
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2008-10-29 21:05:24 +0000

    Ticket #94: loading Impala application context via OSGi

--- trunk/osgi-sample/osgi-main/src/org/impalaframework/osgisample/ImpalaActivator.java
+++ trunk/osgi-sample/osgi-main/src/org/impalaframework/osgisample/ImpalaActivator.java
@@ -1,42 +1,70 @@
 package org.impalaframework.osgisample;
 
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
-import org.springframework.osgi.io.OsgiBundleResource;
-import org.springframework.util.FileCopyUtils;
-import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.osgi.util.BundleDelegatingClassLoader;
 
 public class ImpalaActivator implements BundleActivator {
 
 	public void start(BundleContext bundleContext) throws Exception {
-		URL resource = bundleContext.getBundle().getResource("META-INF/impala-bootstrap.xml");
+
+		URL[] resource = findResources(bundleContext, new String[] {
+				"META-INF/impala-bootstrap.xml",
+				"META-INF/impala-web-bootstrap.xml" });
 		
 		if (resource != null) {
-			InputStream stream = resource.openStream();
-			FileCopyUtils.copyToString(new InputStreamReader(stream));
-			System.out.println(stream);
+			Thread currentThread = Thread.currentThread();
+			ClassLoader oldTCCL = currentThread.getContextClassLoader();
 			
-			String name = "";
-			
-			final DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
-			final GenericWebApplicationContext applicationContext = new GenericWebApplicationContext(beanFactory);
-			//applicationContext.setServletContext(servletContext);
-
-			XmlBeanDefinitionReader definitionReader = new XmlBeanDefinitionReader(beanFactory);
-			definitionReader.loadBeanDefinitions(new OsgiBundleResource(bundleContext.getBundle(), "META-INF/impala-bootstrap.xml"));
-			applicationContext.refresh();
+			try
+			{
+				ClassLoader classLoader = BundleDelegatingClassLoader.createBundleClassLoaderFor(bundleContext.getBundle());
+				currentThread.setContextClassLoader(classLoader);
+				
+				final OSGiApplicationContext applicationContext = new OSGiApplicationContext(resource);		
+				applicationContext.setBundleContext(bundleContext);
+				applicationContext.refresh();
+			}
+			finally {
+				currentThread.setContextClassLoader(oldTCCL);
+			}
+		} else {
+			System.out.println("Could not find impala-bootstrap.xml resource");
 		}
+			
 	}
 
+	private URL[] findResources(BundleContext bundleContext, String[] names) {
+		List<URL> urls = new ArrayList<URL>();
+		for (int i = 0; i < names.length; i++) {
+			URL findResource = findResource(bundleContext, names[i]);
+			if (findResource != null) {
+				urls.add(findResource);
+			} else {
+				//TODO log
+			}
+		}
+		return urls.toArray(new URL[0]);
+	}
 
+	private URL findResource(BundleContext bundleContext, String name) {
+		URL resource = null;
+		//find the resources
+		Bundle[] bundles = bundleContext.getBundles();
+		for (Bundle bundle : bundles) {
+			resource = bundle.getResource(name);
+			if (resource != null) {
+				return resource;
+			}
+		}
+		return null;
+	}
 
 	public void stop(BundleContext bundleContext) throws Exception {
 	}
 }
-
