c70c68cbc08b488a5413b2731374166aa70f465a
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2008-10-29 21:34:57 +0000

    Ticket #94: added osgi-module1

---
+++ trunk/osgi-sample/osgi-module1/src/org/impalaframework/osgimodule1/ResourceActivator.java
@@ -0,0 +1,15 @@
+package org.impalaframework.osgimodule1;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+
+public class ResourceActivator implements BundleActivator {
+	public void start(BundleContext bundleContext) throws Exception {
+		System.out.println("Started bundle " + bundleContext.getBundle().getSymbolicName());
+	}
+
+	public void stop(BundleContext bundleContext) throws Exception {	
+		System.out.println("Stopping bundle " + bundleContext.getBundle().getSymbolicName());
+	}
+}
