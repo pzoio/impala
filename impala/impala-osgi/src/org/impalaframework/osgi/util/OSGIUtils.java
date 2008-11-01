3886e8b3d21e2c45e33109486c7ff5e151021c1d
philzoio <philzoio@realsolve.co.uk>
2008-11-01 21:10:07 +0000

    Ticket #98: initial OSGi implementation

---
+++ impala/impala-osgi/src/org/impalaframework/osgi/util/OSGIUtils.java
@@ -0,0 +1,55 @@
+package org.impalaframework.osgi.util;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.List;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+
+public class OSGIUtils {
+	
+	//FIXME test
+	public static URL[] findResources(BundleContext bundleContext, String[] names) {
+		List<URL> urls = new ArrayList<URL>();
+		for (int i = 0; i < names.length; i++) {
+			URL findResource = findResource(bundleContext, names[i]);
+			if (findResource != null) {
+				urls.add(findResource);
+			} else {
+				//TODO log
+			}
+		}
+		return urls.toArray(new URL[0]);
+	}
+
+	//FIXME test
+	public static URL findResource(BundleContext bundleContext, String name) {
+		URL resource = null;
+		//find the resources
+		Bundle[] bundles = bundleContext.getBundles();
+		for (Bundle bundle : bundles) {
+			resource = bundle.getResource(name);
+			if (resource != null) {
+				return resource;
+			}
+		}
+		return null;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static Bundle findBundle(BundleContext bundleContext, String name) {
+		String resource = null;
+		//find the resources
+		Bundle[] bundles = bundleContext.getBundles();
+		for (Bundle bundle : bundles) {
+			final Dictionary headers = bundle.getHeaders();
+			resource = (String) headers.get(org.osgi.framework.Constants.BUNDLE_NAME);
+			if (name.equals(resource)) {
+				return bundle;
+			}
+		}
+		return null;
+	}
+}
