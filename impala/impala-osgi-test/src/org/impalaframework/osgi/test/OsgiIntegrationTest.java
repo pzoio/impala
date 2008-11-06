cf63a53ec2ad24e85d02e2e4efb36d11500f83c3
philzoio <philzoio@realsolve.co.uk>
2008-11-06 19:57:56 +0000

    Ticket #98: added copyright notice

--- impala/impala-osgi-test/src/org/impalaframework/osgi/test/OsgiIntegrationTest.java
+++ impala/impala-osgi-test/src/org/impalaframework/osgi/test/OsgiIntegrationTest.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright 2007-2008 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package org.impalaframework.osgi.test;
 
 import java.io.File;
@@ -34,13 +48,6 @@ public abstract class OsgiIntegrationTest extends AbstractConfigurableBundleCrea
 	
 	/* ********************** Test bundle names ********************* */
 	
-	@Override
-	public void runBare() throws Throwable {
-		super.runBare();
-		//FIXME should we delegate to Osgi runBare or not depending on whether this is an OSGi test or not
-	}
-
-
 	@Override
 	protected Resource[] getTestBundles() {
 		File osgiDirectory = new File("../osgi-repository/osgi");
