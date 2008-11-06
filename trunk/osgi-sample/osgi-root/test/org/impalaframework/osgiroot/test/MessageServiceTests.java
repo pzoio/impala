295947a3cf22e960ed5052a1c4143665d3697b3d
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2008-11-06 15:25:50 +0000

    Ticket #98: using ModuleDefinitionSource in test to fire up OSGi bundles

--- trunk/osgi-sample/osgi-root/test/org/impalaframework/osgiroot/test/MessageServiceTests.java
+++ trunk/osgi-sample/osgi-root/test/org/impalaframework/osgiroot/test/MessageServiceTests.java
@@ -1,12 +1,80 @@
 package org.impalaframework.osgiroot.test;
 
+import java.io.File;
+import java.io.FileFilter;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.impalaframework.definition.source.TestDefinitionSource;
+import org.impalaframework.facade.Impala;
+import org.impalaframework.module.definition.ModuleDefinitionSource;
+import org.impalaframework.module.definition.RootModuleDefinition;
 import org.impalaframework.osgi.test.OsgiIntegrationTest;
 import org.impalaframework.osgiroot.MessageService;
+import org.impalaframework.util.ReflectionUtils;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 
 public class MessageServiceTests extends OsgiIntegrationTest {
+	
+	protected void postProcessBundleContext(BundleContext context) throws Exception {
+		Impala.init();
+		RootModuleDefinition definition = getModuleDefinition();
+		
+		ServiceReference serviceReference = context.getServiceReference(ModuleDefinitionSource.class.getName());
+		
+		Object service = context.getService(serviceReference);
+		System.out.println(service);
+		
+		Method method = ReflectionUtils.findMethod(service.getClass(), "inject", new Class[]{Object.class});
+		ReflectionUtils.invokeMethod(method, service, new Object[]{definition});
+		
+		//now fire up extender bundle 
+		
+		File distDirectory = new File("../osgi-repository/dist");
+		File[] distBundles = distDirectory.listFiles(new FileFilter(){
 
+			public boolean accept(File pathname) {
+				if (pathname.getName().contains("sources")) return false;
+				return pathname.getName().contains("extender");
+			} 
+			
+		});
+		
+		File mainDirectory = new File("../osgi-repository/main");
+		File[] mainBundles = mainDirectory.listFiles(new FileFilter(){
+
+			public boolean accept(File pathname) {
+				if (pathname.getName().contains("sources")) return false;
+				return pathname.getName().contains("extender");
+			} 
+			
+		});
+		
+		List<Resource> arrayList = new ArrayList<Resource>();
+		addResources(arrayList, distBundles);
+		Resource[] addResources = addResources(arrayList, mainBundles);
+		
+		for (Resource resource : addResources) {
+			Bundle bundle = installBundle(context, resource);
+			startBundle(bundle);
+		}
+		
+		super.postProcessBundleContext(context);
+	}
+	
+	private Resource[] addResources(List<Resource> resources, File[] listFiles) {
+		for (int i = 0; i < listFiles.length; i++) {
+			resources.add(new FileSystemResource(listFiles[i]));
+		}
+		return resources.toArray(new Resource[0]);
+	}
+	
 	public void testMessageService() throws Exception {
 		
 		ServiceReference[] serviceReference = bundleContext.getServiceReferences(ApplicationContext.class.getName(), "(org.springframework.context.service.name=org.impalaframework.osgiroot)");	
@@ -14,6 +82,10 @@ public class MessageServiceTests extends OsgiIntegrationTest {
 		ApplicationContext context = (ApplicationContext) service;
 		MessageService messageService = (MessageService) context.getBean("messageService");
 		System.out.println(messageService.getMessage());
+	}
+
+	public RootModuleDefinition getModuleDefinition() {
+		return new TestDefinitionSource("osgi-root", "osgi-module1").getModuleDefinition();
 	}
 	
 }
---
+++ trunk/osgi-sample/osgi-root/test/org/impalaframework/osgiroot/test/TestReload.java
@@ -0,0 +1,25 @@
+package org.impalaframework.osgiroot.test;
+
+import org.impalaframework.definition.source.TestDefinitionSource;
+import org.impalaframework.facade.OperationsFacade;
+import org.impalaframework.module.definition.RootModuleDefinition;
+import org.impalaframework.osgi.test.OsgiIntegrationTest;
+import org.osgi.framework.ServiceReference;
+
+public class TestReload extends OsgiIntegrationTest {
+
+	public void testOsgiEnvironment() throws Exception {
+		System.out.println("Starting reload of --------------------- ");
+		
+		ServiceReference serviceReference = bundleContext.getServiceReference(OperationsFacade.class.getName());
+		OperationsFacade facade = (OperationsFacade) bundleContext.getService(serviceReference);
+		facade.reload("osgi-root");
+		
+		System.out.println("Finished reloading module --------------------- ");
+	}
+
+	public RootModuleDefinition getModuleDefinition() {
+		return new TestDefinitionSource("osgi-root", "osgi-module1").getModuleDefinition();
+	}
+	
+}
