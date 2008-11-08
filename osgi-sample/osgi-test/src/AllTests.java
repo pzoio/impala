6818f23402b91d57ff4fb5ed10bf9bceea3abf79
philzoio <philzoio@realsolve.co.uk>
2008-11-08 21:11:25 +0000

    Ticket #98: Impala update

--- osgi-sample/osgi-test/src/AllTests.java
+++ osgi-sample/osgi-test/src/AllTests.java
@@ -1,3 +1,7 @@
+import org.impalaframework.osgiroot.test.MessageServiceTests;
+import org.impalaframework.osgiroot.test.TestOSGiContext;
+import org.impalaframework.osgiroot.test.TestReload;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -6,6 +10,9 @@ import junit.framework.TestSuite;
 public class AllTests extends TestCase {
 	public static Test suite() {
 		TestSuite suite = new TestSuite(); 
+		suite.addTestSuite(MessageServiceTests.class);
+		suite.addTestSuite(TestOSGiContext.class);
+		suite.addTestSuite(TestReload.class);
 		return suite;
 	}
 }
