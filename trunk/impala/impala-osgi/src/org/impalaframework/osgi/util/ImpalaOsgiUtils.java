5b3c40a35c4b6a5f2f7f7b9e0177ff8eaf82de6a
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2008-11-11 19:53:38 +0000

    Ticket #21: added ImpalaOsgiUtils test

--- trunk/impala/impala-osgi/src/org/impalaframework/osgi/util/ImpalaOsgiUtils.java
+++ trunk/impala/impala-osgi/src/org/impalaframework/osgi/util/ImpalaOsgiUtils.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright 2007-2008 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package org.impalaframework.osgi.util;
 
 import org.impalaframework.facade.InternalOperationsFacade;
@@ -15,13 +29,13 @@ public abstract class ImpalaOsgiUtils {
 	 * Will be null if Impala has not been initialised, typically via an Impala {@link BundleActivator} instance.
 	 */
 	public static ModuleManagementFacade getManagementFacade(BundleContext context) {
-		//FIXME add test
+		
 		InternalOperationsFacade facade = ImpalaOsgiUtils.getOperationsFacade(context);
 		return facade.getModuleManagementFacade();
 	}
 
 	public static InternalOperationsFacade getOperationsFacade(BundleContext context) {
-		//FIXME add test
+		
 		ServiceReference serviceReference = context.getServiceReference(OperationsFacade.class.getName());
 		InternalOperationsFacade facade = ObjectUtils.cast(context.getService(serviceReference), InternalOperationsFacade.class);
 		return facade;
