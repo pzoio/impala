552a5ca741a733178e11c633f095f997adf74d86
philzoio <philzoio@realsolve.co.uk>
2008-11-18 10:58:00 +0000

    Ticket #21: added DependencyRegistryTest. Enhancements to GraphModificationExtractor test

---
+++ impala/impala-sandbox/test/org/impalaframework/classloader/graph/DependencyRegistryTest.java
@@ -0,0 +1,149 @@
+package org.impalaframework.classloader.graph;
+
+import static org.impalaframework.classloader.graph.GraphTestUtils.assertContainsOnly;
+import static org.impalaframework.classloader.graph.GraphTestUtils.expectedAsList;
+import static org.impalaframework.classloader.graph.GraphTestUtils.findDefintion;
+import static org.impalaframework.classloader.graph.GraphTestUtils.newDefinition;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import junit.framework.TestCase;
+
+import org.impalaframework.module.definition.ModuleDefinition;
+import org.impalaframework.module.definition.ModuleDefinitionUtils;
+import org.impalaframework.module.definition.graph.SimpleGraphModuleDefinition;
+import org.impalaframework.module.definition.graph.SimpleGraphRootModuleDefinition;
+
+
+public class DependencyRegistryTest extends TestCase {
+	
+	private SimpleGraphRootModuleDefinition rootDefinition;
+	private DependencyRegistry registry;
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		rootDefinition = definitionSet1();
+		registry = new DependencyRegistry(rootDefinition);
+	}
+	
+	public void testGetDirectDependees() throws Exception {
+		assertDirectDependee("a", "root");
+		assertDirectDependee("b", "root,e");
+		assertDirectDependee("c", "f");
+		assertDirectDependee("d", "e,b");
+		assertDirectDependee("root", "e");
+		assertDirectDependee("e", "f,g");
+		assertDirectDependee("f", "g");
+		assertDirectDependee("g", null);
+	}
+	
+	public void testGetDependencies() throws Exception {
+		assertDependencies("a", "a");
+		assertDependencies("b", "d,b");
+		assertDependencies("c", "c");
+		assertDependencies("d", "d");
+		assertDependencies("root", "a,d,b,root");
+		assertDependencies("e", "a,d,b,root,e");
+		assertDependencies("f", "a,d,c,b,root,e,f");
+		assertDependencies("g", "a,d,c,b,root,e,f,g");
+	}
+	
+	public void testGetDependees() throws Exception {
+		assertDependees("a", "a,root,e,f,g");
+		assertDependees("b", "b,root,e,f,g");
+		assertDependees("c", "c,f,g");
+		assertDependees("d", "d,b,root,e,f,g");
+		assertDependees("root", "root,e,f,g");
+		assertDependees("e", "e,f,g");
+		assertDependees("f", "f,g");
+		assertDependees("g", "g");
+	}
+	
+	public void testAllModules() throws Exception {
+		Collection<ModuleDefinition> allModules = registry.getAllModules();
+		assertModules("d,a,c,b,root,e,f,g", allModules);
+	}
+	
+	public void testAddH() throws Exception {
+		registry.addModule("root", new SimpleGraphModuleDefinition("h", Arrays.asList("a")));
+		Collection<ModuleDefinition> allModules = registry.getAllModules();
+		assertModules("d,a,c,b,root,h,e,f,g", allModules);
+		assertDependees("root", "root,h,e,f,g");
+		assertDependencies("h", "a,d,b,root,h");
+	}
+	
+	public void testAddI() throws Exception {
+		registry.addModule("c", new SimpleGraphModuleDefinition("i", Arrays.asList("g")));
+		Collection<ModuleDefinition> allModules = registry.getAllModules();
+		assertModules("d,a,c,b,root,e,f,g,i", allModules);
+		assertDependees("root", "root,e,f,g,i");
+		assertDependees("a", "a,root,e,f,g,i");
+		//FIXME: this should be consistent with overall order as it contains same modules
+		assertDependencies("i", "c,a,d,b,root,e,f,g,i");
+	}
+
+	private SimpleGraphRootModuleDefinition definitionSet1() {
+		List<ModuleDefinition> definitions = new ArrayList<ModuleDefinition>();
+		
+		//a has no parent or dependencies
+		ModuleDefinition a = newDefinition(definitions, null, "a", null);
+		
+		//b depends on d but has no parent
+		ModuleDefinition b = newDefinition(definitions, null, "b", "d");
+		
+		//c has no parent or dependencies
+		ModuleDefinition c = newDefinition(definitions, null, "c", null);
+		
+		//d has no parent or dependencies
+		ModuleDefinition d = newDefinition(definitions, null, "d", null);
+		
+		//root has siblings a to d, and depends on a and b
+		SimpleGraphRootModuleDefinition root = new SimpleGraphRootModuleDefinition("root", 
+				Collections.singletonList("root.xml"), 
+				Arrays.asList("a", "b"), 
+				Arrays.asList(a, b, c, d));
+		
+		//e has parent root, and depends on b an d
+		ModuleDefinition e = newDefinition(definitions, root, "e", "b,d");
+		
+		//has parent e, and depends on c
+		newDefinition(definitions, e, "f", "c");
+
+		//has parent e, depends on f
+		newDefinition(definitions, e, "g", "f");
+		return root;
+	}	
+
+	private void assertDependencies(String moduleName, String expected) {
+		List<ModuleDefinition> orderedModuleDependencies = registry.getOrderedModuleDependencies(moduleName);
+		assertModules(expected, orderedModuleDependencies);
+	}
+
+	private void assertDependees(String moduleName, String expected) {
+		List<ModuleDefinition> orderedModuleDependencies = registry.getOrderedModuleDependees(moduleName);
+		assertModules(expected, orderedModuleDependencies);
+	}
+	
+	private void assertDirectDependee(
+			String module,
+			String expected) {
+		
+		ModuleDefinition findDefinition = findDefintion(rootDefinition,	module);
+		Collection<ModuleDefinition> directDependees = registry.getDirectDependees(findDefinition);
+		System.out.println(directDependees);
+		assertContainsOnly(directDependees, expected);
+	}
+	
+	private void assertModules(String expected,	Collection<ModuleDefinition> moduleDefinitions) {
+		List<String> actualNames = ModuleDefinitionUtils.getModuleNamesFromCollection(moduleDefinitions);
+		List<String> expectedNames = expectedAsList(expected);
+		assertEquals(expectedNames, actualNames);
+	}
+
+	
+}
