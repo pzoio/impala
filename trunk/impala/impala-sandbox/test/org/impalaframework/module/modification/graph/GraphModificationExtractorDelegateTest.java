5fefe407cf34743bf728c4b6a25720eb1c87c77f
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2008-11-19 10:45:40 +0000

    Ticket #21: added GraphModificationExtractorDelegate test

---
+++ trunk/impala/impala-sandbox/test/org/impalaframework/module/modification/graph/GraphModificationExtractorDelegateTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2007-2008 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.impalaframework.module.modification.graph;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import junit.framework.TestCase;
+
+import org.impalaframework.module.TransitionSet;
+import org.impalaframework.module.definition.ModuleDefinition;
+import org.impalaframework.module.definition.graph.SimpleGraphRootModuleDefinition;
+
+public class GraphModificationExtractorDelegateTest extends TestCase {
+
+	public void testGetTransitions() {
+		GraphModificationExtractorDelegate delegate = new GraphModificationExtractorDelegate();
+		SimpleGraphRootModuleDefinition rootDefinitionOld = rootDefinition("orig");
+		SimpleGraphRootModuleDefinition rootDefinitionNew = rootDefinition("new");
+		TransitionSet transitions = delegate.getTransitions(rootDefinitionOld, rootDefinitionNew);
+		assertEquals(2, transitions.getModuleTransitions().size());
+		
+		assertNotNull(delegate.getNewDependencyRegistry());
+		assertNotNull(delegate.getOriginalDependencyRegistry());
+	}
+
+	private SimpleGraphRootModuleDefinition rootDefinition(String name) {
+		SimpleGraphRootModuleDefinition rootDefinition = new SimpleGraphRootModuleDefinition(name, 
+				Arrays.asList("context.xml"), 
+				new ArrayList<String>(), 
+				new ArrayList<ModuleDefinition>());
+		return rootDefinition;
+	}
+
+}
