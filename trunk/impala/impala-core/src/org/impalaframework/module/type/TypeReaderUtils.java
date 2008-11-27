7e4a5e0ddbbb1e01c232798ab06bc1f7fda75952
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2008-11-27 10:02:47 +0000

    Javadocs, copyright notice

--- trunk/impala/impala-core/src/org/impalaframework/module/type/TypeReaderUtils.java
+++ trunk/impala/impala-core/src/org/impalaframework/module/type/TypeReaderUtils.java
@@ -12,6 +12,20 @@
  * the License.
  */
 
+/*
+ * Copyright 2007-2008 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package org.impalaframework.module.type;
 
 import java.util.ArrayList;
@@ -19,11 +33,16 @@ import java.util.List;
 import java.util.Properties;
 
 import org.impalaframework.module.ModuleElementNames;
+import org.impalaframework.module.TypeReader;
 import org.springframework.util.Assert;
 import org.springframework.util.StringUtils;
 import org.springframework.util.xml.DomUtils;
 import org.w3c.dom.Element;
 
+/**
+ * Class for utility methods used by {@link TypeReader} implementations
+ * @author Phil Zoio
+ */
 public class TypeReaderUtils {
 
 	/**
