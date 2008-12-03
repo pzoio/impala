5bce856d55c9c4ceb23ffa2c07b40ab15b8ab0e8
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2008-12-03 09:34:37 +0000

    Ticket #21: added ClassRetriever interface as well as URLClassRetriever implementation

---
+++ trunk/impala/impala-core/src/org/impalaframework/classloader/URLClassRetriever.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2007-2008 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.impalaframework.classloader;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Arrays;
+
+import org.impalaframework.util.URLUtils;
+import org.springframework.util.Assert;
+import org.springframework.util.FileCopyUtils;
+
+/**
+ * Implementation of {@link ClassRetriever} which delegates to the
+ * {@link URLClassLoader#findResource(String)} method for location both the
+ * bytes for classes as well as resources. Works both for both directories and
+ * jar files located on the file system. Does not explicitly support any other
+ * kinds of URLs (e.g. classes loaded from remote locations)
+ * 
+ * @author Phil Zoio
+ */
+public class URLClassRetriever implements ClassRetriever {
+	
+	private URL[] urls;
+	private URLClassLoader urlClassLoader;
+	
+	public URLClassRetriever(File[] files) {
+		super();
+		Assert.notNull(files, "files cannot be null");
+		this.urls = URLUtils.createUrls(files);
+		this.urlClassLoader = new URLClassLoader(urls);
+	}
+
+	/**
+	 * Returns the bytes for a particular named class
+	 */
+	public byte[] getClassBytes(String className) {
+		
+		String resourceName = className.replace('.','/') + ".class";
+		URL resource = urlClassLoader.findResource(resourceName);
+		if (resource != null) {
+			InputStream stream = null;
+			try {
+				stream = resource.openConnection().getInputStream();
+				byte[] bytes = FileCopyUtils.copyToByteArray(stream);
+				return bytes;
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					if (stream != null) stream.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * Returns a URL representing a particular resource
+	 */
+	public URL findResource(String resourceName) {
+		return urlClassLoader.findResource(resourceName);
+	}
+	
+	@Override
+	public String toString() {
+		//FIXME implement properly
+		String string = super.toString();
+		StringBuffer buffer = new StringBuffer(string);
+		buffer.append(Arrays.toString(urls));
+		return string;
+	}
+	
+}
