9cf57bf8ab82a6e358032633015723ff48cbf9c5
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2008-12-03 17:18:37 +0000

    Ticket #98: enabling multiple workspace roots

--- trunk/osgi-sample/osgi-root/test/org/impalaframework/osgiroot/test/MessageServiceTest.java
+++ trunk/osgi-sample/osgi-root/test/org/impalaframework/osgiroot/test/MessageServiceTest.java
@@ -1,5 +1,6 @@
 package org.impalaframework.osgiroot.test;
 
+import org.impalaframework.constants.LocationConstants;
 import org.impalaframework.definition.source.TestDefinitionSource;
 import org.impalaframework.module.definition.RootModuleDefinition;
 import org.impalaframework.osgi.test.OsgiIntegrationTest;
@@ -9,6 +10,13 @@ import org.springframework.context.ApplicationContext;
 
 public class MessageServiceTest extends OsgiIntegrationTest {
 	
+	public MessageServiceTest() {
+		super();
+		//FIXME figure out where to put these
+		System.setProperty(LocationConstants.WORKSPACE_ROOT_PROPERTY, "../,../osgi-repository/dist");
+		System.setProperty("application.version", "1.0");
+	}
+
 	public void testMessageService() throws Exception {
 		
 		ServiceReference[] serviceReference = bundleContext.getServiceReferences(ApplicationContext.class.getName(), "(org.springframework.context.service.name=org.impalaframework.osgiroot)");	
