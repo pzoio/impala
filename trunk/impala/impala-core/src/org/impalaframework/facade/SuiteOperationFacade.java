4aa615a4559d84c78b6db1d7c068a52b727bbf13
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2008-12-03 16:19:59 +0000

    Ticket #21: graph enabled by default

--- trunk/impala/impala-core/src/org/impalaframework/facade/SuiteOperationFacade.java
+++ trunk/impala/impala-core/src/org/impalaframework/facade/SuiteOperationFacade.java
@@ -36,6 +36,7 @@ public class SuiteOperationFacade extends BaseOperationsFacade {
 	protected List<String> getBootstrapContextLocations() {
 		List<String> locations = new ArrayList<String>();
 		locations.add("META-INF/impala-bootstrap.xml");
+		locations.add("META-INF/impala-graph-bootstrap.xml");
 		locations.add("META-INF/impala-parent-loader-bootstrap.xml");
 		return locations;
 	}
