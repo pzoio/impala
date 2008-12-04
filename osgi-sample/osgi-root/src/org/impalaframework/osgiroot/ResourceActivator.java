4be0f7f407ac09a411a2fc2c172cb992694119a9
philzoio <philzoio@realsolve.co.uk>
2008-12-04 16:01:02 +0000

    Ticket #98: tweaked logging

--- osgi-sample/osgi-root/src/org/impalaframework/osgiroot/ResourceActivator.java
+++ osgi-sample/osgi-root/src/org/impalaframework/osgiroot/ResourceActivator.java
@@ -1,16 +1,20 @@
 package org.impalaframework.osgiroot;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
 public class ResourceActivator implements BundleActivator {
 	
+	private static Log logger = LogFactory.getLog(ResourceActivator.class);
+
 	public void start(BundleContext bundleContext) throws Exception {
-		System.out.println("Started bundle " + bundleContext.getBundle().getSymbolicName());
+		logger.info("Started bundle " + bundleContext.getBundle().getSymbolicName());
 	}
 
 	public void stop(BundleContext bundleContext) throws Exception {	
-		System.out.println("Stopping bundle " + bundleContext.getBundle().getSymbolicName());
+		logger.info("Stopping bundle " + bundleContext.getBundle().getSymbolicName());
 	}
 	
 }
