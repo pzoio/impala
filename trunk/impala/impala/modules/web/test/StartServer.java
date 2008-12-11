92a83721062c01f5ee9e6fe448bbf3bf875a5136
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2008-12-11 14:25:45 +0000

    No bug: tweaks to scaffold generated code

--- trunk/impala/impala/modules/web/test/StartServer.java
+++ trunk/impala/impala/modules/web/test/StartServer.java
@@ -12,17 +12,12 @@
  * the License.
  */
 
-import java.io.File;
-
-import org.impalaframework.constants.LocationConstants;
 import org.impalaframework.web.StartJetty;
 import org.impalaframework.web.WebConstants;
 
 public class StartServer {
 	public static void main(String[] args) {
-		System.setProperty("org.mortbay.log.class", "org.mortbay.log.StdErrLog");
 		System.setProperty(WebConstants.BOOTSTRAP_LOCATIONS_RESOURCE_PARAM, "classpath:impala-embedded.properties");
-		System.setProperty(LocationConstants.WORKSPACE_ROOT_PROPERTY, new File("../").getAbsolutePath());
-		StartJetty.main(new String[]{"8080", "../@web.project.name@/context", "/@web.project.name@"});
+		StartJetty.main(new String[]{"8080", "../web/context", "/web"});
 	}
 }
