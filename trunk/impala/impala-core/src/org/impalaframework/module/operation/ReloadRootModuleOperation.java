3e16816c9cbcf07a2d61dfd8feff8ade899d1c96
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2008-12-18 17:26:32 +0000

    No bug - JavaDoc additions

--- trunk/impala/impala-core/src/org/impalaframework/module/operation/ReloadRootModuleOperation.java
+++ trunk/impala/impala-core/src/org/impalaframework/module/operation/ReloadRootModuleOperation.java
@@ -17,9 +17,14 @@ package org.impalaframework.module.operation;
 import org.impalaframework.module.definition.RootModuleDefinition;
 
 /**
- * Implementation of {@link ModuleOperation} which encapsulates mechanism for reloading the root module,
- * which is effectively all modules in the graph/hierarchy.
+ * Implementation of {@link ModuleOperation} which encapsulates mechanism for updating the root module,
+ * and potentially all modules in the graph/hierarchy. Unlike {@link ReloadModuleNamedLikeOperation} and 
+ * {@link ReloadNamedModuleOperation}, this operation will only reflect changes to the module graph/
+ * hierarchy if the module definitions themselves have changed. It won't for example, pick up changes to
+ * module classes in the absence of module definition changes.
  * 
+ * @see ReloadModuleNamedLikeOperation
+ * @see ReloadNamedModuleOperation
  * @author Phil Zoio
  */
 public class ReloadRootModuleOperation extends UpdateRootModuleOperation {
