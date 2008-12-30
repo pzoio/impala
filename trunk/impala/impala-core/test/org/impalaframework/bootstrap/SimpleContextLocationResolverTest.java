7f7dc7839752a4f9efc3dcbe0cf9de6e4bb1e614
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2008-12-30 21:01:46 +0000

    Issue 118: added explicitlySetLocations method

--- trunk/impala/impala-core/test/org/impalaframework/bootstrap/SimpleContextLocationResolverTest.java
+++ trunk/impala/impala-core/test/org/impalaframework/bootstrap/SimpleContextLocationResolverTest.java
@@ -26,6 +26,17 @@ public class SimpleContextLocationResolverTest extends TestCase {
 		contextLocations = new ArrayList<String>();
 	}
 
+	public void testDefaultExplicitlySetLocations() {
+		assertFalse(resolver.explicitlySetLocations(contextLocations, propertySource));
+		assertLocations();
+	}
+	
+	public void testExplicitlySetLocations() {
+		properties.setProperty("allLocations", "impala-location1,impala-location2 impala-location3");
+		assertTrue(resolver.explicitlySetLocations(contextLocations, propertySource));
+		assertLocations("location1", "location2", "location3");
+	}
+
 	public void testDefaultAddParentClassLoaderFirst() {
 		resolver.addParentClassLoaderFirst(contextLocations, propertySource);
 		assertLocations();
