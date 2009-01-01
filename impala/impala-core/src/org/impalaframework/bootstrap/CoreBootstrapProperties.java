453163dcf47402a92bc87f5804c42b6a20ad8f26
philzoio <philzoio@realsolve.co.uk>
2009-01-01 20:39:13 +0000

    Issue 118: moved bootstrap properties into own Constants files

---
+++ impala/impala-core/src/org/impalaframework/bootstrap/CoreBootstrapProperties.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2007-2008 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.impalaframework.bootstrap;
+
+public interface CoreBootstrapProperties {
+
+	public static final String EXTRA_LOCATIONS = "extraLocations";
+	public static final String MODULE_TYPE = "moduleType";
+	public static final String PARENT_CLASS_LOADER_FIRST = "parentClassLoaderFirst";
+	public static final String BOOTSTRAP_LOCATIONS = "bootstrapLocations";
+	public static final String ALL_LOCATIONS = "allLocations";
+
+}
