ef64595707a066075b52a549c5cf82c8cceb6769
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2009-01-23 09:18:15 +0000

    Issue 138 - moved Xml document loader to XMLDomUtils

--- trunk/impala/impala-core/src/org/impalaframework/module/source/XMLModulelDefinitionDocumentLoader.java
+++ trunk/impala/impala-core/src/org/impalaframework/module/source/XMLModulelDefinitionDocumentLoader.java
@@ -14,64 +14,23 @@
 
 package org.impalaframework.module.source;
 
-import java.io.IOException;
-import java.io.InputStream;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.impalaframework.exception.ConfigurationException;
-import org.impalaframework.xml.SimpleSaxErrorHandler;
-import org.springframework.beans.factory.xml.DefaultDocumentLoader;
-import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.impalaframework.util.XMLDomUtils;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.EncodedResource;
-import org.springframework.util.Assert;
 import org.w3c.dom.Document;
-import org.xml.sax.InputSource;
 
+/**
+ * Loads document by calling non-validating
+ * @author Phil Zoio
+ */
 public class XMLModulelDefinitionDocumentLoader {
 
-	private static final Log logger = LogFactory.getLog(XMLModulelDefinitionDocumentLoader.class);
-
-	Document loadDocument(Resource resource) {
-
-		Assert.notNull(resource);
-
-		EncodedResource encodedResource = new EncodedResource(resource, "UTF-8");
-
-		InputStream inputStream = null;
+	protected Document loadDocument(Resource resource) {
 		try {
-			// this will throw an IOException if the stream cannot be opened
-			inputStream = encodedResource.getResource().getInputStream();
+			return XMLDomUtils.loadDocument(resource);
+		} catch (Exception e) {
+			throw new ConfigurationException("Unable to load XML module definition document from resource " + resource, e);
 		}
-		catch (IOException e) {
-			throw new ConfigurationException(
-					"Could not load module definition, as unable to obtain input stream for resource "
-							+ encodedResource.getResource(), e);
-		}
-
-		Document document = null;
-
-		DefaultDocumentLoader loader = new DefaultDocumentLoader();
-		try {
-			InputSource inputSource = new InputSource(inputStream);
-			inputSource.setEncoding(encodedResource.getEncoding());
-			document = loader.loadDocument(inputSource, null, new SimpleSaxErrorHandler(logger),
-					XmlBeanDefinitionReader.VALIDATION_NONE, false);
-		}
-		catch (Exception e) {
-			throw new ConfigurationException("Unable to load XML module definition document from resource "
-					+ encodedResource.getResource(), e);
-		}
-		finally {
-			try {
-				if (inputStream != null)
-					inputStream.close();
-			}
-			catch (IOException e) {
-			}
-		}
-		return document;
 	}
 
 }
