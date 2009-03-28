c1d86882d42f1b3fd2566d750c214bc1124c3b62
philzoio <philzoio@realsolve.co.uk>
2009-03-28 23:50:25 +0000

    Issue 172 - removed hasTarget from ContributionEndpointTargetSource

--- impala/impala-core/test/org/impalaframework/service/registry/ServiceRegistryTargetSourceTest.java
+++ impala/impala-core/test/org/impalaframework/service/registry/ServiceRegistryTargetSourceTest.java
@@ -14,18 +14,15 @@ public class ServiceRegistryTargetSourceTest extends TestCase {
 		serviceRegistry = new ServiceRegistryImpl();
 		ServiceRegistryTargetSource targetSource = new ServiceRegistryTargetSource("mybean", serviceRegistry);
 		
-		assertFalse(targetSource.hasTarget());
 		assertNull(targetSource.getServiceRegistryReference());
 		
 		Object service = new Object();
 		serviceRegistry.addService("mybean", "moduleName", service, ClassUtils.getDefaultClassLoader());
 		
-		assertTrue(targetSource.hasTarget());
 		assertNotNull(targetSource.getServiceRegistryReference());
 		
 		serviceRegistry.remove(service);
 		
-		assertFalse(targetSource.hasTarget());
 		assertNull(targetSource.getServiceRegistryReference());
 	}
 
