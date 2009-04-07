a842defb86793bb1cd8b38d7b2dda34c6a140d11
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2009-04-07 07:18:29 +0000

    Issue 4 - method to get instance including FactoryBean

---
+++ trunk/impala/impala-core/src/org/impalaframework/service/registry/internal/ServiceRegistryUtils.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2007-2008 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.impalaframework.service.registry.internal;
+
+import org.impalaframework.exception.ExecutionException;
+import org.impalaframework.service.ServiceRegistryReference;
+import org.springframework.beans.factory.FactoryBean;
+
+public class ServiceRegistryUtils {
+
+	static Object getTargetInstance(final ServiceRegistryReference reference) {
+		Object target = reference.getBean();
+		if (target instanceof FactoryBean) {
+			FactoryBean factoryBean  = (FactoryBean) target;
+			try {
+				target = factoryBean.getObject();
+			} catch (Exception e) {
+				throw new ExecutionException("Unable to get underlying object from factory bean " + factoryBean + ": ", e);
+			}
+		}
+		return target;
+	}
+
+}
