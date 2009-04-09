5e88aa7be0ca749522e6dae05e54a1bfe644933e
philzoio <philzoio@realsolve.co.uk>
2009-04-09 20:01:17 +0000

    Issue 179 - documentation

--- impala/impala-core/src/org/impalaframework/module/ModuleState.java
+++ impala/impala-core/src/org/impalaframework/module/ModuleState.java
@@ -16,13 +16,44 @@ package org.impalaframework.module;
 
 public interface ModuleState {
 
+	/**
+	 * Module has been marked for loading or is in process of being loaded
+	 */
 	String LOADING = "LOADING";
+	
+	/**
+	 * Module is successfully loaded and active
+	 */
 	String LOADED = "LOADED";
+	
+	/**
+	 * Module is marked for unloading or is in process of being unloaded
+	 */
 	String UNLOADING = "UNLOADING";
+	
+	/**
+	 * Module has been successfully unloaded
+	 */
 	String UNLOADED = "UNLOADED";
+	
+	/**
+	 * State is not clear, as module unloading failed
+	 */
 	String UNKNOWN = "UNKNOWN";
+	
+	/**
+	 * Module marked as stale - eligible for reloading
+	 */
 	String STALE = "STALE";
+	
+	/**
+	 * Module load unsuccessful
+	 */
 	String ERROR = "ERROR";
+	
+	/**
+	 * Module that was marked LOADING was not loaded because loading of one of its dependencies failed.
+	 */
 	String DEPENDENCY_FAILED = "DEPENDENCY_FAILED";
 
 }
