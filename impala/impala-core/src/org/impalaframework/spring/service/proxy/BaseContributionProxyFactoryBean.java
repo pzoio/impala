d173aa69dc0b7ecd60149339eca73b72037b31c8
philzoio <philzoio@realsolve.co.uk>
2009-04-25 20:32:54 +0000

    Issue 4 - ContributionProxyFactoryBean split into BaseContributionProxyFactoryBean

---
+++ impala/impala-core/src/org/impalaframework/spring/service/proxy/BaseContributionProxyFactoryBean.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2007-2008 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.impalaframework.spring.service.proxy;
+
+import org.impalaframework.service.ContributionEndpoint;
+import org.impalaframework.service.ServiceRegistry;
+import org.impalaframework.service.reference.BasicServiceRegistryReference;
+import org.impalaframework.service.registry.ServiceRegistryAware;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.beans.factory.BeanClassLoaderAware;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.util.ClassUtils;
+
+/**
+ * Base implementation for <code>FactoryBean</code> which represents a service obtained from the service registry.
+ * Defines an abstract {@link #createProxyFactory()} methods which subclasses should implement to define exactly how 
+ * the {@link ProxyFactory} gets created. Implementations may for example hold a static reference to a bean obtained from the 
+ * service registry, or use some kind of dynamic lookup.
+ * 
+ * @see BasicServiceRegistryReference
+ * @author Phil Zoio
+ */
+public abstract class BaseContributionProxyFactoryBean implements FactoryBean, InitializingBean, ContributionEndpoint, ServiceRegistryAware, BeanClassLoaderAware {
+
+    private static final long serialVersionUID = 1L;
+    
+    private ServiceProxyFactoryCreator proxyFactoryCreator;
+
+    private ProxyFactory proxyFactory;
+
+    private ClassLoader beanClassLoader;
+    
+    private ServiceRegistry serviceRegistry;
+
+    /* *************** InitializingBean implementation method ************** */
+
+    public void afterPropertiesSet() throws Exception {
+        
+        if (this.proxyFactoryCreator == null) {
+            this.proxyFactoryCreator = new DefaultServiceProxyFactoryCreator();
+            this.proxyFactoryCreator.setServiceRegistry(this.serviceRegistry);
+        }
+        
+        ProxyFactory createDynamicProxyFactory = createProxyFactory();
+        this.proxyFactory = createDynamicProxyFactory;
+    }
+
+    /* *************** Protected methods ************** */
+    
+    protected abstract ProxyFactory createProxyFactory();
+    
+    protected ServiceProxyFactoryCreator getProxyFactoryCreator() {
+        return proxyFactoryCreator;
+    }
+    
+    /* *************** FactoryBean implementation methods ************** */
+
+
+    public Object getObject() throws Exception {
+        return proxyFactory.getProxy(beanClassLoader != null ? beanClassLoader : ClassUtils.getDefaultClassLoader());
+    }
+
+    @SuppressWarnings("unchecked")
+    public Class getObjectType() {
+        // no specific awareness of object type, so return null
+        return null;
+    }
+
+    public boolean isSingleton() {
+        // prototype currently not supported
+        return true;
+    }
+    
+    /* *************** ServiceRegistryAware implementation ************** */
+    
+    public void setServiceRegistry(ServiceRegistry serviceRegistry) {
+        this.serviceRegistry = serviceRegistry;
+    }
+
+    /* *************** dependency injection setters ************** */
+
+    public void setBeanClassLoader(ClassLoader classLoader) {
+        this.beanClassLoader = classLoader;
+    }
+
+    public void setProxyFactoryCreator(ServiceProxyFactoryCreator proxyFactoryCreator) {
+        this.proxyFactoryCreator = proxyFactoryCreator;
+    }
+    
+}
--- impala/impala-core/src/org/impalaframework/spring/service/proxy/ContributionProxyFactoryBean.java
+++ impala/impala-core/src/org/impalaframework/spring/service/proxy/ContributionProxyFactoryBean.java
@@ -14,34 +14,23 @@
 
 package org.impalaframework.spring.service.proxy;
 
-import org.impalaframework.service.ContributionEndpoint;
-import org.impalaframework.service.ServiceRegistry;
 import org.impalaframework.service.reference.BasicServiceRegistryReference;
-import org.impalaframework.service.registry.ServiceRegistryAware;
 import org.springframework.aop.framework.ProxyFactory;
-import org.springframework.beans.factory.BeanClassLoaderAware;
 import org.springframework.beans.factory.BeanNameAware;
-import org.springframework.beans.factory.FactoryBean;
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.util.ClassUtils;
 
 /**
- * <code>FactoryBean</code> which creates a proxy which has uses
- * <code>ServiceRegistryTargetSource</code> as a target source and
- * <code>ContributionEndpointInterceptor</code> as interceptor.
- * 
- * The ContributionProxyFactoryBean works under the assumption that the service was exported against a named key.
+ * The <code>ContributionProxyFactoryBean</code> works under the assumption that the service was exported against a named key.
  * By default, the key is assumed to be the same name as the {@link ContributionProxyFactoryBean}'s bean name.
  * However, this can be overridden.
+ * 
+ * The service is retrieved using dynamic lookup following each invocation on the proxy.
  * 
  * @see BasicServiceRegistryReference
  * @author Phil Zoio
  */
-public class ContributionProxyFactoryBean implements FactoryBean, BeanNameAware, InitializingBean, ContributionEndpoint, ServiceRegistryAware, BeanClassLoaderAware {
+public class ContributionProxyFactoryBean extends BaseContributionProxyFactoryBean implements BeanNameAware {
 
     private static final long serialVersionUID = 1L;
-    
-    private ServiceProxyFactoryCreator proxyFactoryCreator;
 
     private Class<?>[] proxyInterfaces;
 
@@ -49,50 +38,18 @@ public class ContributionProxyFactoryBean implements FactoryBean, BeanNameAware,
     
     private String exportedBeanName;
 
-    private ProxyFactory proxyFactory;
-
-    private ClassLoader beanClassLoader;
-    
-    private ServiceRegistry serviceRegistry;
-
     /* *************** BeanNameAware implementation method ************** */
 
     public void setBeanName(String name) {
         this.beanName = name;
     }
 
-    /* *************** InitializingBean implementation method ************** */
+    /* *************** Abstract superclass method implementation ************** */
 
-    public void afterPropertiesSet() throws Exception {
-        
-        if (this.proxyFactoryCreator == null) {
-            this.proxyFactoryCreator = new DefaultServiceProxyFactoryCreator();
-            this.proxyFactoryCreator.setServiceRegistry(this.serviceRegistry);
-        }
-        
+    protected ProxyFactory createProxyFactory() {
         String registryBeanName = (exportedBeanName != null ? exportedBeanName : beanName);
-        this.proxyFactory = proxyFactoryCreator.createDynamicProxyFactory(proxyInterfaces, registryBeanName);
-    }
-
-    /* *************** FactoryBean implementation methods ************** */
-
-    public Object getObject() throws Exception {
-        return proxyFactory.getProxy(beanClassLoader != null ? beanClassLoader : ClassUtils.getDefaultClassLoader());
-    }
-
-    @SuppressWarnings("unchecked")
-    public Class getObjectType() {
-        // no specific awareness of object type, so return null
-        return null;
-    }
-
-    public boolean isSingleton() {
-        // prototype currently not supported
-        return true;
-    }
-    
-    public void setServiceRegistry(ServiceRegistry serviceRegistry) {
-        this.serviceRegistry = serviceRegistry;
+        ProxyFactory createDynamicProxyFactory = getProxyFactoryCreator().createDynamicProxyFactory(proxyInterfaces, registryBeanName);
+        return createDynamicProxyFactory;
     }
 
     /* *************** dependency injection setters ************** */
@@ -101,16 +58,8 @@ public class ContributionProxyFactoryBean implements FactoryBean, BeanNameAware,
         this.proxyInterfaces = proxyInterfaces;
     }
 
-    public void setBeanClassLoader(ClassLoader classLoader) {
-        this.beanClassLoader = classLoader;
-    }
-
     public void setExportedBeanName(String exportedBeanName) {
         this.exportedBeanName = exportedBeanName;
-    }
-
-    public void setProxyFactoryCreator(ServiceProxyFactoryCreator proxyFactoryCreator) {
-        this.proxyFactoryCreator = proxyFactoryCreator;
     }
     
 }
