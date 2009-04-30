a131da53241e3aba745baefe817dd9308617c06f
philzoio <philzoio@realsolve.co.uk>
2009-04-30 16:59:10 +0000

    Issue 4 - updated BeanRetrievingServiceRegistryTargetSource to use exportedTypes

--- impala/impala-core/test/org/impalaframework/service/registry/BeanRetrievingServiceRegistryTargetSourceTest.java
+++ impala/impala-core/test/org/impalaframework/service/registry/BeanRetrievingServiceRegistryTargetSourceTest.java
@@ -1,5 +1,8 @@
 package org.impalaframework.service.registry;
 
+import java.util.Arrays;
+import java.util.List;
+
 import junit.framework.TestCase;
 
 import org.impalaframework.service.ServiceRegistryReference;
@@ -10,15 +13,36 @@ import org.springframework.util.ClassUtils;
 public class BeanRetrievingServiceRegistryTargetSourceTest extends TestCase {
 
     private ServiceRegistryImpl serviceRegistry;
-
-    public void testGetTarget() {
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
         serviceRegistry = new ServiceRegistryImpl();
+    }
+
+    public void testGetUsingBean() {
         BeanRetrievingServiceRegistryTargetSource targetSource = new BeanRetrievingServiceRegistryTargetSource(serviceRegistry, "mybean", new Class[]{ Object.class }, false);
         
         assertNull(targetSource.getServiceRegistryReference());
         
         Object service = new Object();
         final ServiceRegistryReference serviceReference = serviceRegistry.addService("mybean", "moduleName", service, ClassUtils.getDefaultClassLoader());
+        
+        assertNotNull(targetSource.getServiceRegistryReference());
+        
+        serviceRegistry.remove(serviceReference);
+        
+        assertNull(targetSource.getServiceRegistryReference());
+    }
+    
+    public void testGetUsingExportedType() {
+        BeanRetrievingServiceRegistryTargetSource targetSource = new BeanRetrievingServiceRegistryTargetSource(serviceRegistry, null, new Class[]{ Object.class }, true);
+        
+        assertNull(targetSource.getServiceRegistryReference());
+        
+        Object service = new Object();
+        List<Class<?>> asList = Arrays.asList(new Class<?>[]{Object.class});
+        final ServiceRegistryReference serviceReference = serviceRegistry.addService("mybean", "moduleName", service, asList, null, ClassUtils.getDefaultClassLoader());
         
         assertNotNull(targetSource.getServiceRegistryReference());
         
