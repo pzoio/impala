9d81961c25627e6e851e17e881ce082f00dd35a1
philzoio <philzoio@realsolve.co.uk>
2009-05-02 05:42:24 +0000

    Issue 4 - adapted MapStringUtils so that it will parse a richer object map from a String

--- impala/impala-core/src/org/impalaframework/util/MapStringUtils.java
+++ impala/impala-core/src/org/impalaframework/util/MapStringUtils.java
@@ -21,7 +21,7 @@ import org.springframework.util.Assert;
 import org.springframework.util.StringUtils;
 
 
-public class MapStringUtils {
+public abstract class MapStringUtils {
 
     /**
      * Generates map of String to String name value pairs, assuming that individual pairings are
@@ -38,20 +38,70 @@ public class MapStringUtils {
      * As in {@link #parsePropertiesFromString(String)}, but with allowing the user to specify the 
      * delimiters between pairings
      */
+    @SuppressWarnings("unchecked")
     public static Map<String, String> parsePropertiesFromString(String mapString, String delimiters) {
+        return parseMap(mapString, delimiters, stringConverter);
+    }
+    
+    /**
+     * Generates map of String to Object name value pairs, assuming that individual pairings are
+     * separated by a new line or comma, and that name to value pairs are separated by equals
+     * @param mapString the input String
+     * @return a String to Object map. Uses {@link ParseUtils#parseObject(String)} to convert 
+     * String values to map values.
+     * Uses {@link LinkedHashMap} so that the ordering of the key
+     * value pairs is maintained
+     */
+    public static Map<String,Object> parseMapFromString(String mapString) {
+        return parseMapFromString(mapString, ",\n");
+    }
+    
+    /**
+     * As in {@link #parseMapFromString(String)}, but with allowing the user to specify the 
+     * delimiters between pairings
+     */
+    @SuppressWarnings("unchecked")
+    public static Map<String, Object> parseMapFromString(String mapString, String delimiters) {
+        return parseMap(mapString, delimiters, objectConverter);
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    private static Map parseMap(String mapString,
+            String delimiters, ValueConverter valueConverter) {
         Assert.notNull(mapString, "map string cannot be null");
         Assert.notNull(delimiters, "delimiters cannot be null");
         String[] pairings = StringUtils.tokenizeToStringArray(mapString, delimiters);
-        
-        Map<String,String> map = new LinkedHashMap<String,String>();
+        Map map = new LinkedHashMap();
         for (String pair : pairings) {
             int equalsIndex = pair.indexOf('=');
             if (equalsIndex > 0) {
-                map.put(pair.substring(0, equalsIndex).trim(), pair.substring(equalsIndex+1).trim());
+                Object value = valueConverter.convertValue(pair.substring(equalsIndex+1).trim());
+                map.put(pair.substring(0, equalsIndex).trim(), value);
             }
         }
         
         return map;
     }
+    
+    private static interface ValueConverter {
+        Object convertValue(String text);
+    }
+    
+    private static ValueConverter stringConverter = new ValueConverter() {
+
+        public Object convertValue(String text) {
+            return text;
+        }
+        
+    };
+    
+    private static ValueConverter objectConverter = new ValueConverter() {
+
+        public Object convertValue(String text) {
+            return ParseUtils.parseObject(text);
+        }
+        
+    };
     
 }
