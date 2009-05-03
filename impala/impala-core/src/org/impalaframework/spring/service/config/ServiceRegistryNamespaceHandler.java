74ccb6baef714f92896c081cf7d2fe9e5e58a090
philzoio <philzoio@realsolve.co.uk>
2009-05-03 19:59:03 +0000

    Issue 192 - added schema with import attributes

--- impala/impala-core/src/org/impalaframework/spring/service/config/ServiceRegistryNamespaceHandler.java
+++ impala/impala-core/src/org/impalaframework/spring/service/config/ServiceRegistryNamespaceHandler.java
@@ -3,12 +3,16 @@ package org.impalaframework.spring.service.config;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.impalaframework.spring.service.exporter.ServiceRegistryExporter;
+import org.impalaframework.spring.service.proxy.FilteredServiceProxyFactoryBean;
+import org.impalaframework.spring.service.proxy.NamedServiceProxyFactoryBean;
+import org.impalaframework.spring.service.proxy.TypedServiceProxyFactoryBean;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.xml.AbstractSimpleBeanDefinitionParser;
 import org.springframework.beans.factory.xml.BeanDefinitionParser;
 import org.springframework.beans.factory.xml.NamespaceHandler;
 import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
 import org.springframework.beans.factory.xml.ParserContext;
+import org.springframework.util.StringUtils;
 import org.w3c.dom.Element;
 
 /**
@@ -31,13 +35,13 @@ public class ServiceRegistryNamespaceHandler extends NamespaceHandlerSupport {
         registerBeanDefinitionParser("import", new ImportBeanDefinitionParser());
         registerBeanDefinitionParser("list", new ListBeanDefinitionParser());
         registerBeanDefinitionParser("map", new MapBeanDefinitionParser());
-        registerBeanDefinitionParser("auto-export", new MapBeanDefinitionParser());
+        registerBeanDefinitionParser("auto-export", new AutoExportBeanDefinitionParser());
     }
 
     private static class ExportBeanDefinitionParser extends AbstractSimpleBeanDefinitionParser {
 
         /**
-         * Parses the <code>export</code> element. An example is below:
+         * Parses the <code>export</code> element. An example is below.
          * 
          * <pre class = "code">
          * &lt;service:export beanName = "beanToBeExported" 
@@ -52,13 +56,31 @@ public class ServiceRegistryNamespaceHandler extends NamespaceHandlerSupport {
         }
     }
     
-    private static class ImportBeanDefinitionParser implements BeanDefinitionParser {
-
-        public BeanDefinition parse(Element element, ParserContext parserContext) {
-            //FIXME provide implementation of this ...
-            return null;
+    private static class ImportBeanDefinitionParser extends AbstractSimpleBeanDefinitionParser {
+        
+        /**
+         * Parses the <code>export</code> element. An example is below.
+         * 
+         * <pre class = "code">
+         * &lt;service:import 
+         *         exportName = "optionalNameInExportRegistry"
+         *         exportTypes = "optionalCommaSeparatedListOfTypes"
+         *         filterExpression = "(name=value)"/&gt;
+         * </pre>
+         */
+        
+        @Override
+        protected Class<?> getBeanClass(Element element) {
+            if (StringUtils.hasText(element.getAttribute("filterExpression"))) {
+                return FilteredServiceProxyFactoryBean.class;
+            }
+            
+            if (StringUtils.hasText(element.getAttribute("exportName"))) {
+                return NamedServiceProxyFactoryBean.class;
+            }
+            return TypedServiceProxyFactoryBean.class;
         }
-
+        
     }
     
     private static class ListBeanDefinitionParser implements BeanDefinitionParser {
