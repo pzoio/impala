01da32cbdc8bf45c67e1956991e7abd8bc9438e3
philzoio <philzoio@realsolve.co.uk>
2009-05-07 08:31:12 +0000

    Issue 195 - added strong deprecation warnings in code

--- impala/impala-core/src/org/impalaframework/spring/service/exporter/ModuleContributionPostProcessor.java
+++ impala/impala-core/src/org/impalaframework/spring/service/exporter/ModuleContributionPostProcessor.java
@@ -14,6 +14,10 @@
 
 package org.impalaframework.spring.service.exporter;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.BeansException;
+
 
 /**
  * See {@link NamedServiceAutoExportPostProcessor}
@@ -23,4 +27,24 @@ package org.impalaframework.spring.service.exporter;
  */
 @Deprecated 
 public class ModuleContributionPostProcessor extends NamedServiceAutoExportPostProcessor {
+    
+    Log logger = LogFactory.getLog(ModuleContributionPostProcessor.class);
+    
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName)
+            throws BeansException {
+
+        System.out.println("*************** WARNING ***************");
+        String message = "You are using " + ModuleContributionPostProcessor.class.getName() 
+                + ". This class is deprecated and will be removed in the next release. Use "
+                + NamedServiceAutoExportPostProcessor.class.getName() 
+                + " or the 'auto-export' element from the Impala 'service' namespace";
+        System.out.println(message);
+        logger.warn(message);
+        System.out.println("*************** WARNING ***************");
+        
+        return super.postProcessAfterInitialization(bean, beanName);
+    }
+    
+    
 }
