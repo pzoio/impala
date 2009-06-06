8fbf4b0280e00b1d7121069b91b71212ddcf594e
philzoio <philzoio@realsolve.co.uk>
2009-06-06 20:27:56 +0000

    Issue 218 - implemented NamedFactoryBean based on BaseExistingBeanExposingFactoryBean

---
+++ impala/impala-core/src/org/impalaframework/spring/service/bean/NamedFactoryBean.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2007-2008 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.impalaframework.spring.service.bean;
+
+import org.impalaframework.service.ServiceBeanReference;
+import org.impalaframework.spring.service.SpringServiceBeanUtils;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.util.Assert;
+
+/**
+ * {@link FactoryBean} which simply injects bean obtained from parent bean
+ * factory.
+ * @author Phil Zoio
+ */
+public class NamedFactoryBean extends BaseExistingBeanExposingFactoryBean {
+
+    private String beanName;
+    
+    private ServiceBeanReference serviceBeanReference;
+
+    @Override
+    protected String getBeanNameToSearchFor() {
+        return beanName;
+    }
+
+    @Override
+    protected boolean getIncludeCurrentBeanFactory() {
+        return true;
+    }    
+    
+    public void afterPropertiesSet() throws Exception {
+        Assert.notNull(beanName, "beanName cannot be null");
+        
+        BeanFactory parentFactory = findBeanFactory();
+        serviceBeanReference = SpringServiceBeanUtils.newServiceBeanReference(parentFactory, getBeanNameToSearchFor());
+    }
+
+    public Object getObject() throws Exception {
+        return serviceBeanReference.getService();
+    }
+
+    public boolean isSingleton() {
+        return serviceBeanReference.isStatic();
+    }
+
+    public void setBeanName(String beanName) {
+        this.beanName = beanName;
+    }
+
+}
