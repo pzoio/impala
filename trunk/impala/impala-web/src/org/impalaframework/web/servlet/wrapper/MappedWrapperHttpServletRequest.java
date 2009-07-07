f2deb53837738979735156b728ae7f194ed9486c
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2009-07-07 18:42:09 +0000

    Issue 227 - JavaDocs

--- trunk/impala/impala-web/src/org/impalaframework/web/servlet/wrapper/MappedWrapperHttpServletRequest.java
+++ trunk/impala/impala-web/src/org/impalaframework/web/servlet/wrapper/MappedWrapperHttpServletRequest.java
@@ -7,12 +7,18 @@ import javax.servlet.http.HttpServletRequestWrapper;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+/**
+ * Extension of {@link HttpServletRequestWrapper} which provides implementations for 
+ * servlet path related methods. Note that if servlet path supplied in the constructor
+ * is not valid (in the sense that, added to context path, it should be a substring of the request
+ * URI), then the supplied servlet path value will be ignored.
+ * 
+ * @author Phil Zoio
+ */
 public class MappedWrapperHttpServletRequest extends
         HttpServletRequestWrapper {
     
     private static final Log logger = LogFactory.getLog(ModuleAwareWrapperHttpServletRequest.class);
-
-    //FIXME complete implementation and test
     
     private ServletContext servletContext;
     
@@ -37,6 +43,9 @@ public class MappedWrapperHttpServletRequest extends
         }
     }
 
+    /**
+     * If valid servlet path is provided, then this is returned. Otherwise, delegates to wrapped {@link #getServletPath()}
+     */
     @Override
     public String getServletPath() {
         if (servletPath != null) {
@@ -45,6 +54,9 @@ public class MappedWrapperHttpServletRequest extends
         return super.getServletPath();
     }
     
+    /**
+     * If valid servlet path is provided, the remainder of URI is returned. Otherwise, delegates to wrapped {@link #getPathInfo()}
+     */
     @Override
     public String getPathInfo() {
         if (pathInfo != null) {
@@ -52,7 +64,13 @@ public class MappedWrapperHttpServletRequest extends
         }
         return super.getPathInfo();
     }
-
+    
+    /**
+     * If valid servlet path is provided, then real path associated with path info is returned. 
+     * Otherwise, delegates to wrapped {@link #getPathInfo()}.
+     * 
+     * See {@link ServletContext#getRealPath(String)}
+     */
     @Override
     public String getPathTranslated() {
         if (pathInfo != null) {
