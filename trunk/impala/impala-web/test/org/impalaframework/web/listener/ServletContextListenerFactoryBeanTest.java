6357e5ffb3a3961378664b1c3818c92d3d2b2ed7
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2009-07-13 21:51:16 +0000

    Issue 231: added test for ServletContextListenerFactoryBean

--- trunk/impala/impala-web/test/org/impalaframework/web/listener/ServletContextListenerFactoryBeanTest.java
+++ trunk/impala/impala-web/test/org/impalaframework/web/listener/ServletContextListenerFactoryBeanTest.java
@@ -14,12 +14,69 @@
 
 package org.impalaframework.web.listener;
 
+import static org.easymock.EasyMock.*;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.impalaframework.util.ObjectUtils;
+import org.springframework.util.ClassUtils;
+
 import junit.framework.TestCase;
 
 public class ServletContextListenerFactoryBeanTest extends TestCase {
 
-    public void testAfterPropertiesSet() {
-        //FIXME implement test
+    private ServletContextListenerFactoryBean bean;
+
+    private ServletContext servletContext;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        bean = new ServletContextListenerFactoryBean();
+        servletContext = createMock(ServletContext.class);
+        bean.setServletContext(servletContext);
+        bean.setListenerClassName(TestListener.class.getName());
+        bean.setBeanClassLoader(ClassUtils.getDefaultClassLoader());
     }
 
+    public void testAfterPropertiesSet() throws Exception {
+        replay(servletContext);
+    
+        bean.afterPropertiesSet();
+        bean.destroy();
+        
+        ServletContextListener listener = bean.getListener();
+        assertTrue(listener instanceof TestListener);
+        TestListener testListener = ObjectUtils.cast(listener, TestListener.class);
+        assertEquals(1, testListener.getInitializedCount());
+        assertEquals(1, testListener.getDestroyedCount());
+        
+        verify(servletContext);
+    }
+
+}
+
+class TestListener implements ServletContextListener {
+
+    private int initializedCount;
+
+    private int destroyedCount;
+
+    public void contextInitialized(ServletContextEvent event) {
+        initializedCount++;
+    }
+
+    public void contextDestroyed(ServletContextEvent event) {
+        destroyedCount++;
+    }
+
+    public int getDestroyedCount() {
+        return destroyedCount;
+    }
+
+    public int getInitializedCount() {
+        return initializedCount;
+    }
 }
