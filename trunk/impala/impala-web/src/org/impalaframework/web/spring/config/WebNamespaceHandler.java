8adf2a841a1b7f3c4a74f6b4e3f44ade61371143
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2009-07-15 17:34:28 +0000

    Issue 203: support for setting servlet factory class

--- trunk/impala/impala-web/src/org/impalaframework/web/spring/config/WebNamespaceHandler.java
+++ trunk/impala/impala-web/src/org/impalaframework/web/spring/config/WebNamespaceHandler.java
@@ -2,10 +2,14 @@ package org.impalaframework.web.spring.config;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.impalaframework.exception.ExecutionException;
 import org.impalaframework.web.spring.integration.ServletFactoryBean;
+import org.springframework.beans.factory.BeanClassLoaderAware;
 import org.springframework.beans.factory.xml.AbstractSimpleBeanDefinitionParser;
 import org.springframework.beans.factory.xml.NamespaceHandler;
 import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+import org.springframework.util.ClassUtils;
+import org.springframework.util.StringUtils;
 import org.w3c.dom.Element;
 
 /**
@@ -14,9 +18,11 @@ import org.w3c.dom.Element;
  * 
  * @author Phil Zoio
  */
-public class WebNamespaceHandler extends NamespaceHandlerSupport {
+public class WebNamespaceHandler extends NamespaceHandlerSupport implements BeanClassLoaderAware {
 
     private static final Log logger = LogFactory.getLog(WebNamespaceHandler.class);
+    
+    private ClassLoader classLoader;
     
     public void init() {
         
@@ -27,16 +33,44 @@ public class WebNamespaceHandler extends NamespaceHandlerSupport {
         registerBeanDefinitionParser("servlet", new ServletBeanDefinitionParser());
     }
     
-    private static class ServletBeanDefinitionParser extends AbstractSimpleBeanDefinitionParser {
+    private class ServletBeanDefinitionParser extends AbstractSimpleBeanDefinitionParser {
 
         @Override
         protected Class<?> getBeanClass(Element element) {
+            String factoryClass = element.getAttribute("factoryClass");
+            if (StringUtils.hasText(factoryClass)) {
+                
+                try {
+                    ClassUtils.forName(factoryClass, classLoader);
+                }
+                catch (Throwable e) {
+                    throw new ExecutionException("Unable to load class: " + factoryClass + ": " + e.getMessage(), e);
+                }
+            }
             return ServletFactoryBean.class;
+        }
+        
+        @Override
+        protected boolean isEligibleAttribute(String attributeName) {
+            boolean equals = "factoryClass".equals(attributeName);
+            if (equals) {
+                return false;
+            }
+            return super.isEligibleAttribute(attributeName);
+        }
+        
+        @Override
+        protected String extractPropertyName(String attributeName) {
+            return super.extractPropertyName(attributeName);
         }
 
         @Override
         protected boolean shouldGenerateIdAsFallback() {
             return true;
         }
+    }
+
+    public void setBeanClassLoader(ClassLoader classLoader) {
+        this.classLoader = classLoader;
     }
 }
