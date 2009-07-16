253e1ee78317f73ba3db1231ea1c09d35086271f
philzoio <philzoio@realsolve.co.uk>
2009-07-16 14:49:50 +0000

    Issue 203: extracted ServletBeanDefinitionParser

---
+++ impala/impala-web/src/org/impalaframework/web/spring/config/ServletBeanDefinitionParser.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2007-2008 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.impalaframework.web.spring.config;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.impalaframework.exception.ExecutionException;
+import org.impalaframework.web.spring.integration.ServletFactoryBean;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.xml.AbstractSimpleBeanDefinitionParser;
+import org.springframework.beans.factory.xml.ParserContext;
+import org.springframework.util.ClassUtils;
+import org.springframework.util.StringUtils;
+import org.springframework.util.xml.DomUtils;
+import org.w3c.dom.Element;
+
+/**
+ * Bean definition parser for <i>servlet</i> element from <i>web</i> namespace.
+ * 
+ * @author Phil Zoio
+ */
+public class ServletBeanDefinitionParser extends AbstractSimpleBeanDefinitionParser {
+
+    private static final String INIT_PARAMS_ELEMENT =  "init-parameters";
+    private static final String PARAM_ELEMENT =  "param";
+   
+    private static final String FACTORY_CLASS_ATTRIBUTE =  "factoryClass";
+    private static final String NAME_ATTRIBUTE = "name";
+    private static final String VALUE_ATTRIBUTE = "value";
+    
+    private static final String INIT_PARAMS_PROPERTY=  "initParameters";    
+    
+    private ClassLoader classLoader;
+    
+    
+    public ServletBeanDefinitionParser(ClassLoader classLoader) {
+        super();
+        this.classLoader = classLoader;
+    }
+
+        @Override
+        protected Class<?> getBeanClass(Element element) {
+            String factoryClass = element.getAttribute(FACTORY_CLASS_ATTRIBUTE);
+            if (StringUtils.hasText(factoryClass)) {
+                
+                try {
+                    ClassUtils.forName(factoryClass, classLoader);
+                }
+                catch (Throwable e) {
+                    throw new ExecutionException("Unable to load class: " + factoryClass + ": " + e.getMessage(), e);
+                }
+            }
+            return ServletFactoryBean.class;
+        }
+        
+        @Override
+        protected boolean isEligibleAttribute(String attributeName) {
+            if (FACTORY_CLASS_ATTRIBUTE.equals(attributeName)) {
+                return false;
+            }
+            return super.isEligibleAttribute(attributeName);
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        protected void doParse(Element element, ParserContext parserContext,
+                BeanDefinitionBuilder builder) {
+            super.doParse(element, parserContext, builder);
+            Element initParams = DomUtils.getChildElementByTagName(element, INIT_PARAMS_ELEMENT);
+            if (initParams != null) {
+
+                Map<String,String> properties = new LinkedHashMap<String,String>();
+                List<Element> params = DomUtils.getChildElementsByTagName(initParams, PARAM_ELEMENT);
+                for (Element param : params) {
+                    String name = param.getAttribute(NAME_ATTRIBUTE);
+                    String value = param.getAttribute(VALUE_ATTRIBUTE);
+                    properties.put(name.trim(), value.trim());
+                }
+                
+                // Specific environment settings defined, overriding any shared properties.
+                builder.addPropertyValue(INIT_PARAMS_PROPERTY, properties);
+            }
+        }
+}
--- impala/impala-web/src/org/impalaframework/web/spring/config/WebNamespaceHandler.java
+++ impala/impala-web/src/org/impalaframework/web/spring/config/WebNamespaceHandler.java
@@ -1,23 +1,24 @@
-package org.impalaframework.web.spring.config;
+/*
+ * Copyright 2007-2008 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
 
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+package org.impalaframework.web.spring.config;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.impalaframework.exception.ExecutionException;
-import org.impalaframework.web.spring.integration.ServletFactoryBean;
 import org.springframework.beans.factory.BeanClassLoaderAware;
-import org.springframework.beans.factory.support.BeanDefinitionBuilder;
-import org.springframework.beans.factory.xml.AbstractSimpleBeanDefinitionParser;
 import org.springframework.beans.factory.xml.NamespaceHandler;
 import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
-import org.springframework.beans.factory.xml.ParserContext;
-import org.springframework.util.ClassUtils;
-import org.springframework.util.StringUtils;
-import org.springframework.util.xml.DomUtils;
-import org.w3c.dom.Element;
 
 /**
  * Implementation of {@link NamespaceHandler} interface for <code>web</code>
@@ -30,14 +31,6 @@ public class WebNamespaceHandler extends NamespaceHandlerSupport implements Bean
     private static final Log logger = LogFactory.getLog(WebNamespaceHandler.class);
     
     private static final String SERVLET_ELEMENT =  "servlet";
-    private static final String INIT_PARAMS_ELEMENT =  "init-parameters";
-    private static final String PARAM_ELEMENT =  "param";
-   
-    private static final String FACTORY_CLASS_ATTRIBUTE =  "factoryClass";
-    private static final String NAME_ATTRIBUTE = "name";
-    private static final String VALUE_ATTRIBUTE = "value";
-    
-    private static final String INIT_PARAMS_PROPERTY=  "initParameters";
     
     private ClassLoader classLoader;
     
@@ -47,54 +40,7 @@ public class WebNamespaceHandler extends NamespaceHandlerSupport implements Bean
             logger.debug("Setting up " + WebNamespaceHandler.class.getName());
         }
 
-        registerBeanDefinitionParser(SERVLET_ELEMENT, new ServletBeanDefinitionParser());
-    }
-    
-    private class ServletBeanDefinitionParser extends AbstractSimpleBeanDefinitionParser {
-
-        @Override
-        protected Class<?> getBeanClass(Element element) {
-            String factoryClass = element.getAttribute(FACTORY_CLASS_ATTRIBUTE);
-            if (StringUtils.hasText(factoryClass)) {
-                
-                try {
-                    ClassUtils.forName(factoryClass, classLoader);
-                }
-                catch (Throwable e) {
-                    throw new ExecutionException("Unable to load class: " + factoryClass + ": " + e.getMessage(), e);
-                }
-            }
-            return ServletFactoryBean.class;
-        }
-        
-        @Override
-        protected boolean isEligibleAttribute(String attributeName) {
-            if (FACTORY_CLASS_ATTRIBUTE.equals(attributeName)) {
-                return false;
-            }
-            return super.isEligibleAttribute(attributeName);
-        }
-
-        @SuppressWarnings("unchecked")
-        @Override
-        protected void doParse(Element element, ParserContext parserContext,
-                BeanDefinitionBuilder builder) {
-            super.doParse(element, parserContext, builder);
-            Element initParams = DomUtils.getChildElementByTagName(element, INIT_PARAMS_ELEMENT);
-            if (initParams != null) {
-
-                Map<String,String> properties = new LinkedHashMap<String,String>();
-                List<Element> params = DomUtils.getChildElementsByTagName(initParams, PARAM_ELEMENT);
-                for (Element param : params) {
-                    String name = param.getAttribute(NAME_ATTRIBUTE);
-                    String value = param.getAttribute(VALUE_ATTRIBUTE);
-                    properties.put(name.trim(), value.trim());
-                }
-                
-                // Specific environment settings defined, overriding any shared properties.
-                builder.addPropertyValue(INIT_PARAMS_PROPERTY, properties);
-            }
-        }
+        registerBeanDefinitionParser(SERVLET_ELEMENT, new ServletBeanDefinitionParser(classLoader));
     }
 
     public void setBeanClassLoader(ClassLoader classLoader) {
