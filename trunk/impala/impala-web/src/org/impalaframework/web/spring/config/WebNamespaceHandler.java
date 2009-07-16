5670b5fbb9dc8a21a75771e413bb3f740bad210e
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2009-07-16 14:03:01 +0000

    Issue 203: basic working schema and element parser for servlet bean definition

--- trunk/impala/impala-web/src/org/impalaframework/web/spring/config/WebNamespaceHandler.java
+++ trunk/impala/impala-web/src/org/impalaframework/web/spring/config/WebNamespaceHandler.java
@@ -1,15 +1,22 @@
 package org.impalaframework.web.spring.config;
 
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.impalaframework.exception.ExecutionException;
 import org.impalaframework.web.spring.integration.ServletFactoryBean;
 import org.springframework.beans.factory.BeanClassLoaderAware;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.xml.AbstractSimpleBeanDefinitionParser;
 import org.springframework.beans.factory.xml.NamespaceHandler;
 import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+import org.springframework.beans.factory.xml.ParserContext;
 import org.springframework.util.ClassUtils;
 import org.springframework.util.StringUtils;
+import org.springframework.util.xml.DomUtils;
 import org.w3c.dom.Element;
 
 /**
@@ -22,6 +29,16 @@ public class WebNamespaceHandler extends NamespaceHandlerSupport implements Bean
 
     private static final Log logger = LogFactory.getLog(WebNamespaceHandler.class);
     
+    private static final String SERVLET_ELEMENT =  "servlet";
+    private static final String INIT_PARAMS_ELEMENT =  "init-parameters";
+    private static final String PARAM_ELEMENT =  "param";
+   
+    private static final String FACTORY_CLASS_ATTRIBUTE =  "factoryClass";
+    private static final String NAME_ATTRIBUTE = "name";
+    private static final String VALUE_ATTRIBUTE = "value";
+    
+    private static final String INIT_PARAMS_PROPERTY=  "initParameters";
+    
     private ClassLoader classLoader;
     
     public void init() {
@@ -30,14 +47,14 @@ public class WebNamespaceHandler extends NamespaceHandlerSupport implements Bean
             logger.debug("Setting up " + WebNamespaceHandler.class.getName());
         }
 
-        registerBeanDefinitionParser("servlet", new ServletBeanDefinitionParser());
+        registerBeanDefinitionParser(SERVLET_ELEMENT, new ServletBeanDefinitionParser());
     }
     
     private class ServletBeanDefinitionParser extends AbstractSimpleBeanDefinitionParser {
 
         @Override
         protected Class<?> getBeanClass(Element element) {
-            String factoryClass = element.getAttribute("factoryClass");
+            String factoryClass = element.getAttribute(FACTORY_CLASS_ATTRIBUTE);
             if (StringUtils.hasText(factoryClass)) {
                 
                 try {
@@ -52,21 +69,31 @@ public class WebNamespaceHandler extends NamespaceHandlerSupport implements Bean
         
         @Override
         protected boolean isEligibleAttribute(String attributeName) {
-            boolean equals = "factoryClass".equals(attributeName);
-            if (equals) {
+            if (FACTORY_CLASS_ATTRIBUTE.equals(attributeName)) {
                 return false;
             }
             return super.isEligibleAttribute(attributeName);
         }
-        
-        @Override
-        protected String extractPropertyName(String attributeName) {
-            return super.extractPropertyName(attributeName);
-        }
 
+        @SuppressWarnings("unchecked")
         @Override
-        protected boolean shouldGenerateIdAsFallback() {
-            return true;
+        protected void doParse(Element element, ParserContext parserContext,
+                BeanDefinitionBuilder builder) {
+            super.doParse(element, parserContext, builder);
+            Element initParams = DomUtils.getChildElementByTagName(element, INIT_PARAMS_ELEMENT);
+            if (initParams != null) {
+
+                Map<String,String> properties = new LinkedHashMap<String,String>();
+                List<Element> params = DomUtils.getChildElementsByTagName(initParams, PARAM_ELEMENT);
+                for (Element param : params) {
+                    String name = param.getAttribute(NAME_ATTRIBUTE);
+                    String value = param.getAttribute(VALUE_ATTRIBUTE);
+                    properties.put(name.trim(), value.trim());
+                }
+                
+                // Specific environment settings defined, overriding any shared properties.
+                builder.addPropertyValue(INIT_PARAMS_PROPERTY, properties);
+            }
         }
     }
 
