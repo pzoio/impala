2276a5416811a9f72ee0ec53eb149d76a45521fa
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2009-07-18 07:55:15 +0000

    Issue 203: Extracted AbstractWebHandlerBeanDefinitionParser

--- trunk/impala/impala-web/src/org/impalaframework/web/spring/config/ServletBeanDefinitionParser.java
+++ trunk/impala/impala-web/src/org/impalaframework/web/spring/config/ServletBeanDefinitionParser.java
@@ -14,44 +14,18 @@
 
 package org.impalaframework.web.spring.config;
 
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.impalaframework.exception.ExecutionException;
-import org.impalaframework.util.CollectionStringUtils;
-import org.impalaframework.web.spring.integration.InternalFrameworkIntegrationServlet;
-import org.impalaframework.web.spring.integration.InternalFrameworkIntegrationServletFactoryBean;
 import org.impalaframework.web.spring.integration.ServletFactoryBean;
-import org.springframework.beans.MutablePropertyValues;
-import org.springframework.beans.factory.config.RuntimeBeanReference;
-import org.springframework.beans.factory.support.BeanDefinitionBuilder;
-import org.springframework.beans.factory.support.RootBeanDefinition;
-import org.springframework.beans.factory.xml.AbstractSimpleBeanDefinitionParser;
-import org.springframework.beans.factory.xml.ParserContext;
-import org.springframework.util.ClassUtils;
-import org.springframework.util.StringUtils;
-import org.springframework.util.xml.DomUtils;
-import org.w3c.dom.Element;
+
 
 /**
  * Bean definition parser for <i>servlet</i> element from <i>web</i> namespace.
  * 
  * @author Phil Zoio
  */
-public class ServletBeanDefinitionParser extends AbstractSimpleBeanDefinitionParser {
-
-    private static final String INIT_PARAMS_ELEMENT =  "init-parameters";
-    private static final String PARAM_ELEMENT =  "param";
-    private static final String PROPERTY_ELEMENT = "property";    
+public class ServletBeanDefinitionParser extends AbstractWebHandlerBeanDefinitionParser {
 
     private static final String DELEGATOR_SERVLET_NAME_ATTRIBUTE =  "delegatorServletName";
-    private static final String INIT_PARAMS_ATTRIBUTE =  "initParameters";
-    private static final String FACTORY_CLASS_ATTRIBUTE =  "factoryClass";
-    private static final String NAME_ATTRIBUTE = "name";
-    private static final String VALUE_ATTRIBUTE = "value";
-    
-    private static final String INIT_PARAMS_PROPERTY=  "initParameters";
+
     private static final String SERVLET_NAME_PROPERTY = "servletName";
     private static final String SERVLET_CLASS_PROPERTY = "servletClass";
     
@@ -61,113 +35,8 @@ public class ServletBeanDefinitionParser extends AbstractSimpleBeanDefinitionPar
         super();
     }
 
-    @Override
-    protected Class<?> getBeanClass(Element element) {
-        String factoryClass = element.getAttribute(FACTORY_CLASS_ATTRIBUTE);
-        if (StringUtils.hasText(factoryClass)) {
-            
-            try {
-                return ClassUtils.forName(factoryClass);
-            }
-            catch (Throwable e) {
-                throw new ExecutionException("Unable to load class: " + factoryClass + ": " + e.getMessage(), e);
-            }
-        }
+    protected Class<?> getDefaultFactoryBeanClass() {
         return ServletFactoryBean.class;
-    }
-    
-    @Override
-    protected boolean isEligibleAttribute(String attributeName) {
-        
-        if (FACTORY_CLASS_ATTRIBUTE.equals(attributeName) 
-            || INIT_PARAMS_ATTRIBUTE.equals(attributeName)
-            || getDelegatorHandlerNameAttribute().equals(attributeName)) {
-            return false;
-        }
-        return super.isEligibleAttribute(attributeName);
-    }
-
-    @Override
-    protected void doParse(Element element, 
-            ParserContext parserContext,
-            BeanDefinitionBuilder builder) {
-        
-        super.doParse(element, parserContext, builder);
-        
-        String handlerNameProperty = getHandlerNameProperty();
-        if (!StringUtils.hasText(element.getAttribute(handlerNameProperty))) {
-            builder.addPropertyValue(getHandlerNameProperty(), element.getAttribute(ID_ATTRIBUTE));
-        }
-
-        handleInitParameters(element, builder);
-        
-        handlePropertyElements(element, parserContext, builder);
-        
-        handleDelegatorServletAttribute(element, parserContext);
-    }
-    
-    void handleInitParameters(Element element, BeanDefinitionBuilder builder) {
-        
-        Map<String,String> initParameters = new LinkedHashMap<String,String>();
-        handleInitParametersElement(element, initParameters);
-        handleInitParamsAttribute(element, initParameters);
-        
-        // Specific environment settings defined, overriding any shared properties.
-        builder.addPropertyValue(INIT_PARAMS_PROPERTY, initParameters);
-    }
-
-    void handleDelegatorServletAttribute(Element element, ParserContext parserContext) {
-        
-        String delegatorServletName = element.getAttribute(getDelegatorHandlerNameAttribute());
-        
-        if (StringUtils.hasText(delegatorServletName)) {
-            String id = element.getAttribute(ID_ATTRIBUTE);
-            
-            RootBeanDefinition integrationServlet = new RootBeanDefinition(InternalFrameworkIntegrationServletFactoryBean.class);
-            MutablePropertyValues propertyValues = integrationServlet.getPropertyValues();
-            propertyValues.addPropertyValue(getHandlerNameProperty(), delegatorServletName);
-            propertyValues.addPropertyValue(getHandlerClassProperty(), InternalFrameworkIntegrationServlet.class.getName());
-            propertyValues.addPropertyValue(getDelegateHandlerProperty(), new RuntimeBeanReference(id));
-            
-            String beanName = parserContext.getReaderContext().generateBeanName(integrationServlet);
-            parserContext.getRegistry().registerBeanDefinition(beanName, integrationServlet);
-        }
-    }
-    
-    @SuppressWarnings("unchecked")
-    void handlePropertyElements(
-            Element element,
-            ParserContext parserContext, 
-            BeanDefinitionBuilder builder) {
-        
-        List<Element> properties = DomUtils.getChildElementsByTagName(element, PROPERTY_ELEMENT);
-        for (Element propertyElement : properties) {
-            parserContext.getDelegate().parsePropertyElement(propertyElement, builder.getRawBeanDefinition());
-        }
-    }
-
-    void handleInitParamsAttribute(Element element, Map<String, String> initParameters) {
-        
-        String initParamsAttribute = element.getAttribute(INIT_PARAMS_ATTRIBUTE);
-        if (StringUtils.hasText(initParamsAttribute)) {
-            Map<String, String> initParamsFromAttribute = CollectionStringUtils.parsePropertiesFromString(initParamsAttribute);
-            initParameters.putAll(initParamsFromAttribute);
-        }
-    }
-
-    @SuppressWarnings("unchecked")
-    void handleInitParametersElement(Element element, Map<String, String> initParameters) {
-        
-        Element initParams = DomUtils.getChildElementByTagName(element, INIT_PARAMS_ELEMENT);
-        if (initParams != null) {
-
-            List<Element> params = DomUtils.getChildElementsByTagName(initParams, PARAM_ELEMENT);
-            for (Element param : params) {
-                String name = param.getAttribute(NAME_ATTRIBUTE);
-                String value = param.getAttribute(VALUE_ATTRIBUTE);
-                initParameters.put(name.trim(), value.trim());
-            }
-        }
     }
 
     protected String getDelegateHandlerProperty() {
