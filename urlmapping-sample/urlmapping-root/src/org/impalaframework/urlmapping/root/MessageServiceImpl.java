cb972f5de2b7293d1111135c9e6b9ed7c8c8614f
philzoio <philzoio@realsolve.co.uk>
2009-09-05 21:31:11 +0000

    Issue 238 - added updatable message service

--- urlmapping-sample/urlmapping-root/src/org/impalaframework/urlmapping/root/MessageServiceImpl.java
+++ urlmapping-sample/urlmapping-root/src/org/impalaframework/urlmapping/root/MessageServiceImpl.java
@@ -1,9 +1,15 @@
 package org.impalaframework.urlmapping.root;
 
-public class MessageServiceImpl implements MessageService {
+public class MessageServiceImpl implements UpdatableMessageService {
 
+    private String message = "Hello world";
+    
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    
 	public String getMessage() {
-		return "Hello world";
+		return message;
 	}
 
 }
