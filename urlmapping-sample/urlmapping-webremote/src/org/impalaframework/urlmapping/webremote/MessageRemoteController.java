cf813758dd899e7662ed70fd5579911092756d07
philzoio <philzoio@realsolve.co.uk>
2009-09-05 22:31:47 +0000

    Issue 238 - added remoting call

---
+++ urlmapping-sample/urlmapping-webremote/src/org/impalaframework/urlmapping/webremote/MessageRemoteController.java
@@ -0,0 +1,30 @@
+package org.impalaframework.urlmapping.webremote;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.impalaframework.urlmapping.root.MessageService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@SuppressWarnings("unchecked")
+public class MessageRemoteController {
+    
+    private MessageService messageService;
+    
+    @RequestMapping("/viewMessage.xml")
+    public String viewMessage(Map model,
+            HttpServletResponse response) {
+        
+        response.setContentType("application/xml");
+        model.put("message", messageService.getMessage());
+        return "viewMessage";
+    }
+    
+    public void setMessageService(MessageService messageService) {
+        this.messageService = messageService;
+    }
+    
+}
