c83d1fad709fe9a56cf2fd0324440d97dd06abab
philzoio <philzoio@realsolve.co.uk>
2009-09-08 08:03:18 +0000

    Issue 238 - rearranging notes

--- urlmapping-sample/urlmapping-web/src/org/impalaframework/urlmapping/web/MessageController.java
+++ urlmapping-sample/urlmapping-web/src/org/impalaframework/urlmapping/web/MessageController.java
@@ -6,10 +6,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@SuppressWarnings("unchecked")
 public class MessageController {
     
-    @SuppressWarnings("unchecked")
     @RequestMapping("/notes.htm")
-    public void viewMessage(Map model) {
+    public void notes(Map model) {
+    }    
+    
+    @RequestMapping("/intro.htm")
+    public void intro(Map model) {
     }
 }
