c28dfcdeea2c8705368565095a6d9d9260f5548a
philzoio <philzoio@realsolve.co.uk>
2009-09-22 20:20:25 +0000

    No issue - fixed preventing build

--- impala/impala-core/test/org/impalaframework/classloader/graph/GraphBasedClassLoaderTest.java
+++ impala/impala-core/test/org/impalaframework/classloader/graph/GraphBasedClassLoaderTest.java
@@ -76,16 +76,15 @@ public class GraphBasedClassLoaderTest extends TestCase {
     public void testFindResources() throws Exception {
 
         //finds copy in impala-core/bin directory
-        ClassLoader eClassLoader = factory.newClassLoader(dependencyManager, eDefinition);
-        final ArrayList<URL> eList = Collections.list(eClassLoader.getResources("beanset.properties"));
-        assertEquals(1, eList.size());
-        System.out.println(eList.get(0));
+        GraphClassLoader eClassLoader = factory.newClassLoader(dependencyManager, eDefinition);
+        final ArrayList<URL> eList = Collections.list(eClassLoader.getLocalResources("beanset.properties"));
+        assertEquals(0, eList.size());
         
         //note that bClassLoader finds two copies, as there is also a copy in the module-b/bin directory
         //however does not find copy in impala-core directory
-        ClassLoader bClassLoader = factory.newClassLoader(dependencyManager, bDefinition);
-        final ArrayList<URL> bList = Collections.list(bClassLoader.getResources("beanset.properties"));
-        assertEquals(2, bList.size());
+        GraphClassLoader bClassLoader = factory.newClassLoader(dependencyManager, bDefinition);
+        final ArrayList<URL> bList = Collections.list(bClassLoader.getLocalResources("beanset.properties"));
+        assertEquals(1, bList.size());
         System.out.println(bList.get(0));
     }
     
