0a681331edf6258b71cbf92efa62bb8ddb7ecb35
philzoio <philzoio@realsolve.co.uk>
2009-10-01 20:14:17 +0000

    Issue 247 - got struts2 up and running

---
+++ webframeworks/webframeworks-struts2/src/org/impalaframework/webframeworks/struts2/HelloWorld.java
@@ -0,0 +1,23 @@
+package org.impalaframework.webframeworks.struts2;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HelloWorld extends ActionSupport {
+
+    public static final String MESSAGE = "Struts is up and running ...";
+
+    public String execute() throws Exception {
+        setMessage(MESSAGE);
+        return SUCCESS;
+    }
+
+    private String message;
+
+    public void setMessage(String message){
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
