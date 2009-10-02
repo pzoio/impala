c19cb6bd25b4fa1324287abfb5dfe432d9f6998b
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2009-10-02 11:27:29 +0000

    Issue 247 - wired in Spring into Struts2 example

--- trunk/webframeworks/webframeworks-struts2/src/org/impalaframework/webframeworks/struts2/HelloWorld.java
+++ trunk/webframeworks/webframeworks-struts2/src/org/impalaframework/webframeworks/struts2/HelloWorld.java
@@ -1,5 +1,7 @@
 package org.impalaframework.webframeworks.struts2;
 
+import interfaces.MessageService;
+
 import com.opensymphony.xwork2.ActionSupport;
 
 public class HelloWorld extends ActionSupport {
@@ -7,9 +9,22 @@ public class HelloWorld extends ActionSupport {
     private static final long serialVersionUID = 1L;
     
     public static final String MESSAGE = "Struts 2 is up and running now ...";
+    
+    private MessageService messageService;
+    
+    public HelloWorld() {
+        super();
+    }
+     
+    /**
+     * Autowired setter for MessageService
+     */
+    public void setMessageService(MessageService messageService) {
+        this.messageService = messageService;
+    }
 
     public String execute() throws Exception {
-        setMessage(MESSAGE);
+        setMessage(MESSAGE + " with message from Impala message service: " + messageService.getMessage());
         return SUCCESS;
     }
 
