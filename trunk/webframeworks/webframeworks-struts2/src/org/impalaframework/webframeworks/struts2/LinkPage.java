61355b2cffc51298dcd0f63107cb372b0dc6a01d
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2009-10-22 21:17:35 +0000

    Issue 247 - link to struts2 page

---
+++ trunk/webframeworks/webframeworks-struts2/src/org/impalaframework/webframeworks/struts2/LinkPage.java
@@ -0,0 +1,13 @@
+package org.impalaframework.webframeworks.struts2;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class LinkPage extends ActionSupport {
+
+    private static final long serialVersionUID = 1L;
+    
+    public LinkPage() {
+        super();
+    }
+    
+}
