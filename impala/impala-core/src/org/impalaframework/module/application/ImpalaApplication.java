c1405e6fabcf2dc6b331ab7447087d74cc03c537
philzoio <philzoio@realsolve.co.uk>
2009-10-31 19:47:58 +0000

    Issue 259 - added Application interface

---
+++ impala/impala-core/src/org/impalaframework/module/application/ImpalaApplication.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2007-2008 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.impalaframework.module.application;
+
+import org.impalaframework.module.spi.Application;
+import org.impalaframework.module.spi.ClassLoaderRegistry;
+import org.impalaframework.module.spi.ModuleStateHolder;
+import org.impalaframework.service.ServiceRegistry;
+
+/**
+ * Holds mutable application state held by the Impala runtime
+ * @author Phil Zoio
+ */
+public class ImpalaApplication implements Application {
+    
+    private final ClassLoaderRegistry classLoaderRegistry;
+    
+    private final ModuleStateHolder moduleStateHolder;
+    
+    private final ServiceRegistry serviceRegistry;
+
+    public ImpalaApplication(
+            ClassLoaderRegistry classLoaderRegistry,
+            ModuleStateHolder moduleStateHolder, 
+            ServiceRegistry serviceRegistry) {
+        
+        super();
+        this.classLoaderRegistry = classLoaderRegistry;
+        this.moduleStateHolder = moduleStateHolder;
+        this.serviceRegistry = serviceRegistry;
+    }
+
+    public ClassLoaderRegistry getClassLoaderRegistry() {
+        return classLoaderRegistry;
+    }
+
+    public ModuleStateHolder getModuleStateHolder() {
+        return moduleStateHolder;
+    }
+
+    public ServiceRegistry getServiceRegistry() {
+        return serviceRegistry;
+    }
+
+}
