7a1d2b402fa0b67a266c570437b42364c9ed9bae
philzoio <philzoio@realsolve.co.uk>
2009-12-15 16:22:33 +0000

    No bug - tidying up

--- springfaces-sample/springfaces-main/test/com/application/main/suite/AutomatedRootTests.java
+++ springfaces-sample/springfaces-main/test/com/application/main/suite/AutomatedRootTests.java
@@ -1,6 +1,5 @@
 package com.application.main.suite;
 
-import com.application.main.MessageIntegrationTest;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -8,7 +7,7 @@ import junit.framework.TestSuite;
 public class AutomatedRootTests extends TestCase {
     public static Test suite() {
         TestSuite suite = new TestSuite();
-        suite.addTestSuite(MessageIntegrationTest.class);
+        //suite.addTestSuite(MessageIntegrationTest.class);
         return suite;
     }
 }
