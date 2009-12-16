a1a2ceb69397b75823988567f09d3155ec4b5dc9
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2009-12-16 21:54:26 +0000

    Issue 262 - pointing to maven-host

--- trunk/maven-sample/maven-host/src/test/java/StartServer.java
+++ trunk/maven-sample/maven-host/src/test/java/StartServer.java
@@ -5,6 +5,6 @@ import org.impalaframework.web.WebConstants;
 public class StartServer {
     public static void main(String[] args) {
         System.setProperty(WebConstants.BOOTSTRAP_LOCATIONS_RESOURCE_PARAM, "classpath:impala-embedded.properties");
-        StartJetty.main(new String[]{"8080", "../maven-host/src/main/webapp", "/maven-web"});
+        StartJetty.main(new String[]{"8080", "../maven-host/src/main/webapp", "/maven-host"});
     }
 }
