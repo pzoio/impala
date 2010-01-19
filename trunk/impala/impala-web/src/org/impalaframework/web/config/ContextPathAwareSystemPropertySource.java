834ce28972f041a4f31a6bf96a82a2b173ec18dc
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2010-01-19 18:22:04 +0000

    Issue 264 - added ContextPathAwareSystemPropertySource

---
+++ trunk/impala/impala-web/src/org/impalaframework/web/config/ContextPathAwareSystemPropertySource.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2007-2008 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.impalaframework.web.config;
+
+import javax.servlet.ServletContext;
+
+import org.impalaframework.config.PropertySource;
+import org.springframework.util.Assert;
+
+/**
+ * Implementation of {@link PropertySource} which will attempt to look for properties
+ * prefixed using the context path, as determined using {@link ServletContext#getContextPath()}
+ * @author Phil Zoio
+ */
+public class ContextPathAwareSystemPropertySource implements PropertySource {
+
+    private final ServletContext servletContext;
+
+    public ContextPathAwareSystemPropertySource(ServletContext servletContext) {
+        super();
+        Assert.notNull(servletContext, "servletContext cannot be null");
+        this.servletContext = servletContext;
+    }
+
+    /**
+     * If current version of {@link ServletContext} is 2.5 or greater, 
+     * attempts to retrieve property with '%contextPath%.name', with %contextPath% resolved using
+     * {@link ServletContext#getContextPath()}. Otherwise returns null,
+     */
+    public String getValue(String name) {
+        if (servletContext.getMajorVersion() >= 2 && servletContext.getMinorVersion() >= 5) {
+            String contextPath = servletContext.getContextPath();
+            if (contextPath.startsWith("/")) {
+                contextPath = contextPath.substring(1);
+            }
+            return System.getProperty(contextPath + "." + name);
+        }
+        
+        return null;
+    }
+
+}
