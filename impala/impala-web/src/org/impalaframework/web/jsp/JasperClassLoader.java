bf39b0a5907a0e4d6059c6ace2b4e27ce5931aa6
philzoio <philzoio@realsolve.co.uk>
2010-02-05 18:22:58 +0000

    Issue 255 - start of implementation of JasperServletFactoryBean

---
+++ impala/impala-web/src/org/impalaframework/web/jsp/JasperClassLoader.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2007-2008 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.impalaframework.web.jsp;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.net.URLStreamHandlerFactory;
+import java.util.Enumeration;
+
+/**
+ * {@link URLClassLoader} extension for integration with Tomcat Jasper JSP engine
+ * @author Phil Zoio
+ */
+public class JasperClassLoader extends URLClassLoader {
+
+    public JasperClassLoader(URL[] urls, 
+            ClassLoader classLoader,
+            URLStreamHandlerFactory arg2) {
+        super(urls, classLoader, arg2);
+    }
+
+    public JasperClassLoader(URL[] urls, ClassLoader classLoader) {
+        super(urls, classLoader);
+    }
+
+    public JasperClassLoader(URL[] urls) {
+        super(urls);
+    }
+    
+    @Override
+    public URL getResource(String name) {
+        return super.getResource(name);
+    }
+    
+    @Override
+    public Enumeration<URL> getResources(String name) throws IOException {
+        return super.getResources(name);
+    }
+    
+    @Override
+    public Class<?> loadClass(String name) throws ClassNotFoundException {
+        return super.loadClass(name);
+    }
+
+}
--- impala/impala-web/test/org/impalaframework/web/test/AutomatedWebTests.java
+++ impala/impala-web/test/org/impalaframework/web/test/AutomatedWebTests.java
@@ -28,6 +28,7 @@ import org.impalaframework.web.integration.ModuleProxyFilterTest;
 import org.impalaframework.web.integration.ModuleProxyServletTest;
 import org.impalaframework.web.integration.PrefixTreeHolderTest;
 import org.impalaframework.web.integration.UrlPrefixRequestModuleMapperTest;
+import org.impalaframework.web.jsp.JasperServletFactoryBeanTest;
 import org.impalaframework.web.listener.ServletContextListenerFactoryBeanTest;
 import org.impalaframework.web.module.WebModuleUtilsTest;
 import org.impalaframework.web.module.jmx.WebModuleReloaderTest;
@@ -110,6 +111,7 @@ public class AutomatedWebTests {
         suite.addTestSuite(InternalFrameworkIntegrationServletTest.class);
         suite.addTestSuite(InternalFrameworkIntegrationServletFactoryBeanTest.class);
         suite.addTestSuite(InvocationChainTest.class);
+        suite.addTestSuite(JasperServletFactoryBeanTest.class);
         suite.addTestSuite(ModuleHttpServiceInvokerBuilderTest.class);
         suite.addTestSuite(MappedHttpServletRequestTest.class);
         suite.addTestSuite(PartitionedHttpSessionTest.class);
