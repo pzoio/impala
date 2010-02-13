97a4a721cda0bd53be1847e825e25cd0620df633
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2010-02-13 13:27:47 +0000

    Issue 255 - added test for ModuleJspServlet

--- trunk/impala/impala-web/src/org/impalaframework/web/jsp/ModuleJspServlet.java
+++ trunk/impala/impala-web/src/org/impalaframework/web/jsp/ModuleJspServlet.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright 2007-2008 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package org.impalaframework.web.jsp;
 
 import java.io.IOException;
@@ -9,6 +23,27 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.impalaframework.web.servlet.qualifier.WebAttributeQualifier;
+
+/**
+ * Simple servlet implementation which is able to wrap a JSP invocation. Setup
+ * require and use requires the following:
+ * <ul>
+ * <li>Ensure that the property 'partitioned.servlet.context' is set to true in
+ * <i>impala.properties</i>
+ * <li>Add a JSP servlet entry into the module itself, for example using an
+ * entry such as the following:<br/> 
+ * <code>&lt;bean id="jspServlet" class="org.impalaframework.web.jsp.JasperServletFactoryBean"&gt;<br/>
+        &lt;property name="servletName" value="jspServlet"/&gt;<br/>
+    &lt;/bean&gt;</code>
+ * </ul>
+ * Note that direct external invocation to JSPs hosted within modules via this
+ * servlet is not supported. Invocation is expected to be internally within the
+ * application, for example via {@link java.servet.RequestDispatcher}
+ * <code>forward()</code> or <code>include()</code> calls.
+ * 
+ * @author Phil Zoio
+ */
 public class ModuleJspServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
@@ -24,10 +59,21 @@ public class ModuleJspServlet extends HttpServlet {
     protected void service(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         
-        //FIXME issue 255 - added module qualifier prefix
-        final String prefix = (String) req.getAttribute("module_qualifier_prefix");
+        final String prefix = (String) req.getAttribute(WebAttributeQualifier.MODULE_QUALIFIER_PREFIX);
+        if (prefix == null) {
+            throw new IllegalStateException("Cannot resolve attribute '" + WebAttributeQualifier.MODULE_QUALIFIER_PREFIX + "'." +
+            		" This attribute needs to be set in order for ModuleJspServlet to be able to find the module registered JSP servlet to which the request will be forwarded." +
+            		" Possible causes: 1) you have not set the property 'partitioned.servlet.context=true' in impala.properties, or " +
+            		" 2) you have attempted to access the JSP directly rather than through a request or forward from a servlet or filter within the application.");
+        }
 
         final HttpServlet jspServlet = (HttpServlet) servletContext.getAttribute(prefix + JspConstants.JSP_SERVLET);
+        
+        if (jspServlet == null) {
+            throw new IllegalStateException("No JSP servlet registered in the module to which the request was directed." +
+            		" Your module configuration requires a " + JasperServletFactoryBean.class + " configuration entry, or equivalent.");
+        }
+        
         jspServlet.service(req, resp);
     }
     
