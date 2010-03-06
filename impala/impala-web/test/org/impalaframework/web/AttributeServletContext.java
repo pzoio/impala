c487eb9fb0ddb7b81b57426555d477a823299c81
philzoio <philzoio@realsolve.co.uk>
2010-03-06 12:12:07 +0000

    Issue 307 - copyright notice update

--- impala/impala-web/test/org/impalaframework/web/AttributeServletContext.java
+++ impala/impala-web/test/org/impalaframework/web/AttributeServletContext.java
@@ -15,7 +15,7 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
 /*
- * Copyright 2007-2008 the original author or authors.
+ * Copyright 2007-2010 the original author or authors.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
