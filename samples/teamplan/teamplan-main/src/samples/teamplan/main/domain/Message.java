db41f75ad4e44145bb9ca64a7c2875726fbe316c
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2010-05-22 15:35:02 +0000

    Added annotatated class

---
+++ samples/teamplan/teamplan-main/src/samples/teamplan/main/domain/Message.java
@@ -0,0 +1,59 @@
+package samples.teamplan.main.domain;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Message {
+
+    private Long id;
+    
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Message other = (Message) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        }
+        else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Message [id=" + id + ", message=" + message + "]";
+    }
+    
+}
