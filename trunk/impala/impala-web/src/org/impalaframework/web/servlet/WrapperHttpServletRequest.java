244d7ff437ac8f3281fec4b0b61c291be7475b5a
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2010-07-16 09:16:56 +0000

    Issue 343 - JavaDocs

--- trunk/impala/impala-web/src/org/impalaframework/web/servlet/WrapperHttpServletRequest.java
+++ trunk/impala/impala-web/src/org/impalaframework/web/servlet/WrapperHttpServletRequest.java
@@ -17,12 +17,14 @@ package org.impalaframework.web.servlet;
 import javax.servlet.http.HttpServletRequest;
 
 /**
- * {@link HttpServletRequest} interface extension exposing methods which are implemented by Impala'
- * internal request wrapper implementations
+ * {@link HttpServletRequest} which allows underlying {@link HttpServletRequest} instance to be exposed.
  * @author Phil Zoio
  */
 public interface WrapperHttpServletRequest extends HttpServletRequest {
 
+    /**
+     * Returns the first {@link HttpServletRequest} to be unwrapped which does not implement {@link WrapperHttpServletRequest}.
+     */
     public HttpServletRequest getWrappedHttpServletRequest();
     
 }
