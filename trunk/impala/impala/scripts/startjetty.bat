b6b7b1e5e895c60c39406216cdddeaeaaabd9689
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2010-09-08 09:14:55 +0000

    Issue 347 - corrected version of startjetty.bat

--- trunk/impala/impala/scripts/startjetty.bat
+++ trunk/impala/impala/scripts/startjetty.bat
@@ -11,12 +11,13 @@ if "%4" == "" goto noConfigdirSet
 set CONFIGDIR=%4
 :noConfigdirSet
 
-if "%CONFIGDIR" !==! "" goto configdirSet
+if "%CONFIGDIR%" neq "" goto configdirSet
 set CONFIGDIR=config
 :configdirSet
 
 @echo on
 java %JPDA% %SYSPROP% -jar launcher.jar StartJetty --addclasspath %CONFIGDIR% --addjardir jetty %1 war %2
+@echo off
 
 :usage
-echo startjetty [port] [contextpath] [systemproperty file (optional)]
+echo startjetty [port] [contextpath] [systemproperty file (optional)] [config directory (optional)]
