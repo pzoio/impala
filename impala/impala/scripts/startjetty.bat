a8fe55316d1132e2338cc8a28e19b1a152b0c199
philzoio <philzoio@realsolve.co.uk>
2010-09-08 08:52:38 +0000

    Issue 347 - windows version of start jetty batch file

--- impala/impala/scripts/startjetty.bat
+++ impala/impala/scripts/startjetty.bat
@@ -7,8 +7,16 @@ if "%3" == "" goto syspropfileSet
 set SYSPROP=-Dsysprop-resource=%3
 :syspropfileSet
 
+if "%4" == "" goto noConfigdirSet
+set CONFIGDIR=%4
+:noConfigdirSet
+
+if "%CONFIGDIR" !==! "" goto configdirSet
+set CONFIGDIR=config
+:configdirSet
+
 @echo on
-java %JPDA% %SYSPROP% -jar launcher.jar StartJetty --addclasspath config --addjardir jetty %1 war %2
+java %JPDA% %SYSPROP% -jar launcher.jar StartJetty --addclasspath %CONFIGDIR% --addjardir jetty %1 war %2
 
 :usage
 echo startjetty [port] [contextpath] [systemproperty file (optional)]
