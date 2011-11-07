d9de4fd528202802cb121e0b241be20d57517b98
philzoio@realsolve.co.uk <philzoio@realsolve.co.uk@9c9863d1-a93b-0410-a92d-6ffae5813f01>
2011-11-07 16:40:31 +0000

    issue 361 - implement Servlet module loading

--- trunk/impala/impala-web/src/org/impalaframework/web/resolver/ServletContextModuleLocationResolver.java
+++ trunk/impala/impala-web/src/org/impalaframework/web/resolver/ServletContextModuleLocationResolver.java
@@ -14,12 +14,19 @@
 
 package org.impalaframework.web.resolver;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 import javax.servlet.ServletContext;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.impalaframework.file.ExtensionFileFilter;
 import org.impalaframework.resolver.ModuleLocationResolver;
+import org.impalaframework.util.ResourceUtils;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.core.io.Resource;
 import org.springframework.util.Assert;
@@ -39,6 +46,8 @@ import org.springframework.web.context.support.ServletContextResource;
  */
 public class ServletContextModuleLocationResolver implements ModuleLocationResolver, ServletContextAware, InitializingBean {
 
+    private static final Log logger = LogFactory.getLog(ServletContextModuleLocationResolver.class);
+    
     private String applicationVersion;
 
     private String relativeModuleRootLocation = "/WEB-INF/modules";
@@ -64,6 +73,22 @@ public class ServletContextModuleLocationResolver implements ModuleLocationResol
     }
     
     public List<Resource> getModuleSpecificJarLocations(String moduleName) {
+        
+        //FIXME add test
+        String parent = relativeModuleRootLocation + "/lib/" + moduleName;
+        Resource parentResource = new ServletContextResource(servletContext, parent);
+        try {
+            File file = parentResource.getFile();
+            if (file.exists()) {
+                File[] files = file.listFiles(new ExtensionFileFilter(".jar"));
+                if (files != null && files.length > 0) {
+                    return Arrays.asList(ResourceUtils.getResources(files));
+                }
+            }
+        }
+        catch (IOException e) {
+            logger.warn("Unable to get file corresponding with ");
+        }
         return null;
     }
 
